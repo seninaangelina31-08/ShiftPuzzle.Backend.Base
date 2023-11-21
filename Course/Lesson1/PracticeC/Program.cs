8ae04b66a868e9c5a3c8b4828d66561099244576
game <_@example.com>
2023-11-20 23:38:00 -0800

    feat:(Добавил PracticeC)

---
+++ Course/Lesson1/PracticeC/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticeC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int number))
+        {
+            // Увеличим число на 5
+            int result = number + 5;
+
+            // Выведем результат
+            Console.WriteLine($"{userInput} + 5 = {result}");
+    }
+}
+}

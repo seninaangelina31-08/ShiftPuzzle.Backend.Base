d2ccb85c62443fa083c2c596e5c06eaef82ca357
Юрий <zaretskiy.y.a@gmail.com>
2023-11-21 18:43:42 +0300

    init commit

---
+++ Course/Lesson1/A/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace A;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        double b = 10.1;
+        string c = "YURA";
+        bool e = true;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(e);
+    }
+}

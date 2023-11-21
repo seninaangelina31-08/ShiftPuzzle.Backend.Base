4832529217860ffe0a768c7b1b64b030db997c7f
game <_@example.com>
2023-11-20 23:37:51 -0800

    feat:(Добавил PracticeB)

---
+++ Course/Lesson1/PracticeB/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PracticeB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Калькулятор");  
+    
+    Console.Write("Введите свое первое число: ");
+    int numberone = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Ваше число: {numberone}");
+    
+    Console.Write("Введите свое второе число: ");
+    int numbertwo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Ваше число: {numbertwo}");
+
+    int additions = numberone + numbertwo;
+    int subtraction = numberone - numbertwo;
+    int multiplication = numberone * numbertwo;
+    int division = numberone / numbertwo;
+    
+    Console.WriteLine($"Результат сложение: {additions}");
+    Console.WriteLine($"Результат вычитание: {subtraction}");
+    Console.WriteLine($"Результат умножение: {multiplication}");
+    Console.WriteLine($"Результат деление: {division}");
+    }
+}

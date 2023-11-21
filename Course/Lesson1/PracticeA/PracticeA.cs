09ae71aa4945e3e3dfa2696fe27b5488860fb84c
game <_@example.com>
2023-11-20 23:36:40 -0800

    feat:(Добавил PracticeA)

---
+++ Course/Lesson1/PracticeA/PracticeA.cs
@@ -0,0 +1,21 @@
+﻿namespace Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int integerValue = 42;
+
+        double doubleValue = 3.14;
+
+        string stringValue = "Hello";
+
+        bool booleanValue = true;
+
+        Console.WriteLine("Целочисленная переменная: " + integerValue);
+        Console.WriteLine("Вещественная переменная: " + doubleValue);
+        Console.WriteLine("Строковая переменная: " + stringValue);
+        Console.WriteLine("Логическая переменная: " + booleanValue);
+    }
+}

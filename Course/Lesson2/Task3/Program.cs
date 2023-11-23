1cfe4a176eab7157604dc2809578c2c4a77dbae8
MisterCat100 <mistercat0618@gmail.com>
2023-11-23 18:48:49 +0300

    feat(Task3): book with names and phone numbers.

--- Course/Lesson2/Task3/Program.cs
+++ Course/Lesson2/Task3/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите ваше имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите ваш телефон: ");
+        string phoneNumber = Console.ReadLine();
+        Console.WriteLine(name + " - " + phoneNumber);
     }
 }

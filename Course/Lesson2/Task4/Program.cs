89463941ffff41eb6138280a60a67d9387aa0934
MisterCat100 <mistercat0618@gmail.com>
2023-11-23 19:02:08 +0300

    feat(Task4): checking, is Enter entered.

--- Course/Lesson2/Task4/Program.cs
+++ Course/Lesson2/Task4/Program.cs
@@ -3,7 +3,19 @@
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    {   
+        Console.WriteLine("Введите текст: ");
+        string text = Console.ReadLine();
+
+        if (text == "")
+        {
+            Console.WriteLine("Скорее всего, вы нажали на Enter");
+
+        }
+        else 
+        {
+            Console.WriteLine("Вы ввели не Enter");
+
+        }
     }
 }

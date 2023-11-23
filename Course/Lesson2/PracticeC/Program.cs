1e4380aada9c47e204b7418c8bd3d59cb19ccd49
Валерий Манойлов <manojlovv@inbox.ru>
2023-11-23 21:52:17 +0300

    add: have been completed all Practices C->D

--- Course/Lesson2/PracticeC/Program.cs
+++ Course/Lesson2/PracticeC/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите имя:");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Введите номер телефона:");
+        int phone_num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя: " + name);
+        Console.WriteLine("Ваше имя: " + phone_num);
     }
 }
---
+++ Course/Lesson2/PracticeD/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticeD;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        while (True)
+        {
+            Console.WriteLine("Enter было нажато " + сounter + " раз");
+            string EnterKey = Console.ReadLine();
+            counter = counter + 1;
+        }
+    }
+}
+

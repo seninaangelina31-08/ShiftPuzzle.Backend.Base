32c8638d7273cf838a3cbf0ada262f9472714c9a
Ivanova Daria <ida30062006@gmail.com>
2023-11-23 18:31:34 +0300

    feature: prac2

---
+++ Course/Lesson2/Prac2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your birth year: ");
+
+int year = Convert.ToInt32(Console.ReadLine());
+
+int age = 2023 - year;
+
+Console.WriteLine("Your is " + age + " years old.");

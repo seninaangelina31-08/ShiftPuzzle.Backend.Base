8d1ecf469c524d457d8ed2732011f3b057c9e905
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 18:27:25 +0300

    feat: Classwork Task 1 is DONE!

---
+++ Course/Lesson2/ClassWork/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is you name: ");
+        Console.WriteLine("Hello, " + Console.ReadLine());
+    }
+}

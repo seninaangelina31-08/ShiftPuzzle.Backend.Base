08f7a4139772a061195636b423b3503665c6077f
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 20:23:53 +0300

    feat: homework task1

---
+++ Course/Lesson2/Homework/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ввод в формате: "ЦЫФРА_1 ОПЕРАТОР ЦЫФРА_2"
+        string[] expressionSplit = Console.ReadLine().Split();
+        Console.WriteLine(ExpressionExecution(expressionSplit));
+    }
+
+    static int ExpressionExecution(string[] expression)
+    {
+        int result = 0;
+        if(expression[1] == "+"){
+            result = Convert.ToInt32(expression[0]) + Convert.ToInt32(expression[2]);
+        }
+        else if(expression[1] == "-"){
+            result = Convert.ToInt32(expression[0]) - Convert.ToInt32(expression[2]);
+        }
+        else if(expression[1] == "*"){
+            result = Convert.ToInt32(expression[0]) * Convert.ToInt32(expression[2]);
+        }
+        else if(expression[1] == "/"){
+            result = Convert.ToInt32(expression[0]) / Convert.ToInt32(expression[2]);
+        }
+        return result;
+    }
+}

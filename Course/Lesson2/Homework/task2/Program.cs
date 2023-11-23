a7d639cd591bac14b997ee6d6a67b7f5f0917ca1
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 20:29:06 +0300

    feat: homefork task2

---
+++ Course/Lesson2/Homework/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace task2;
+class Program
+{
+     static void Main(string[] args)
+        {
+            Console.WriteLine("Угадайте число от 1 до 100, у вас 10 попыток : ");           
+            Random rnd = new Random();
+            int chislo = rnd.Next(1 , 100);
+            bool isFound = false;
+            for (int i = 0; i < 10; i++)
+            {             
+                int vvod = Convert.ToInt32(Console.ReadLine());
+                if (vvod < chislo)
+                {
+                    Console.WriteLine("Загаданное число больше, чем то, которое вы ввели");
+                    
+                } 
+                if (vvod > chislo)
+                {
+                    Console.WriteLine("Загаданное число меньше, чем то, которое вы ввели");
+                   
+                }
+                 else if (vvod == chislo)
+                {
+                    isFound = true;
+                    break;
+                }
+                Console.WriteLine(isFound ? $"Вы угадали! Загаданное число : {chislo}" : "Вы израсходовали все попытки!");
+            } 
+ 
+        }
+}

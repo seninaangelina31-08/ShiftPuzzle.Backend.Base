6d7e59fc577334dec3cd90a44d162b2074490d31
Валерий Манойлов <manojlovv@inbox.ru>
2023-11-23 18:23:26 +0300

    add: have been added Practices, complete Practice1

---
+++ Course/Lesson2/PracticeA/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticeA;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Добрый день! Введите свое имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, " + name + "!");
+    }
+}

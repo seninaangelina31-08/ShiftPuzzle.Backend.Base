b2b26a21ffbd257ae7601d114f3bda4514cdc6b7
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-11-23 23:56:51 +0200

    feat: homework tasks 1-2

---
+++ Course/Lesson2/Homw1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homw1;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число: "); 
+        int num1 = Convert.ToInt32(Console.ReadLine()); 
+
+        Console.WriteLine("Введите второе число: "); 
+        int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+        int sum = num1 + num2; 
+
+        Console.WriteLine("Сумма чисел " + num1 + " и " + num2 + " равна: " + sum ); 
+    }
+}
---
+++ Course/Lesson2/Homw2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Homw2;
+
+class Program
+{
+    static void Main(){
+        Console.WriteLine("Угадайте число от 1 до 100, у вас 10 попыток)"); 
+        Random rnd = new Random();
+        int num = rnd.Next(1, 100);
+        
+        for (int i = 0; i < 10; i++){
+            int numvod = Convert.ToInt32(Console.ReadLine());
+            if(i == 9){
+                Console.WriteLine("Вы не угадали, попробуйте снова!");   
+            }
+            else if (numvod < num){
+                Console.WriteLine("Загаданное число больше");
+            }
+
+            else if (numvod > num){
+                Console.WriteLine("Загаданное число меньше");
+            }
+
+            else if (numvod == num){
+                Console.WriteLine("Ура, вы угадали!");
+            }   
+        }                
+    }
+}

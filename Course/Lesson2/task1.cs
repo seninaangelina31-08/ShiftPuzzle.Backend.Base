b0b3ec655199c7d5f5f691577f72d178bf6ee6e1
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-11-23 18:30:35 +0200

    feat: task1

---
+++ Course/Lesson2/task1.cs
@@ -0,0 +1,11 @@
+namespace task1;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите своё имя:");
+        string name = Console.ReadLine(); 
+        
+        Console.WriteLine("Привет " + name);
+    }
+}

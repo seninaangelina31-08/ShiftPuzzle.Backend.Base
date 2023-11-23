47be0ba8ec1b886925e6e6ddc02772515468a4ea
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 19:50:12 +0300

    fest: task5

---
+++ Course/Lesson2/ClassWork/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("You need to write a rundom text\n");
+        string text = Console.ReadLine();
+        Console.WriteLine("\n" + text);
+    }
+}

e2486acae302005dd0eecf6a38588a3916fd552a
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 18:34:18 +0300

    feat: classwork task2 is DONE

---
+++ Course/Lesson2/ClassWork/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("what year were you born: ");
+        var currentYear = DateTime.Now.Year;
+        Console.WriteLine("You are " + (currentYear - Convert.ToInt32(Console.ReadLine())) + " year.");
+    }
+}

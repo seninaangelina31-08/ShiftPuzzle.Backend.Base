ae5b8f10b727b1fec2a85fee859b60bc768a6c42
Ivanova Daria <ida30062006@gmail.com>
2023-11-23 18:44:26 +0300

    feature: prac3

---
+++ Course/Lesson2/Prac3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+Console.WriteLine("Enter the name: ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter the number: ");
+
+string number = Console.ReadLine();
+
+
+Console.WriteLine(name + "`s telephone number is " + number);

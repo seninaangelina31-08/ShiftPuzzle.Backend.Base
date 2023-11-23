665c0eed8738ec14abe64cc9993223c7c9504935
Ivanova Daria <ida30062006@gmail.com>
2023-11-23 18:25:12 +0300

    feature: prac1

---
+++ Course/Lesson2/Prac1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите Ваше имя: ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hi, " + name + "!");

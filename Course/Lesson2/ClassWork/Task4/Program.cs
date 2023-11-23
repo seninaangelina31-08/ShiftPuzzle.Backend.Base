d95c961e1af5eba8ccc1f324a6933ecdb9144a87
“IvanSobolev” <i@bezlikiy.ru>
2023-11-23 19:45:16 +0300

    Feat: Task4 Enter click number

---
+++ Course/Lesson2/ClassWork/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberClickEnter = 0;
+        while(true)
+        {
+            Console.Write("\nYou click the Enter " + numberClickEnter + " times\n");
+            Console.ReadLine();
+            numberClickEnter++;
+        }
+    }
+}

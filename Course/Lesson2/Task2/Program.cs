36891dd9e0297686b5cf01603dd0d5679c5c54a2
MisterCat100 <mistercat0618@gmail.com>
2023-11-23 18:41:42 +0300

    feat(Task2): score age.

--- Course/Lesson2/Task2/Program.cs
+++ Course/Lesson2/Task2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите год вашего рождения: ");
+        int year = Convert.ToInt16(Console.ReadLine());
+        int correct_year = 2023;
+        Console.WriteLine("Вам " + (correct_year - year) + " лет.");
     }
 }

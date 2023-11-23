3bb95a72c87ad665b2b6aa429133eddf47a20a84
Валерий Манойлов <manojlovv@inbox.ru>
2023-11-23 18:31:04 +0300

    add: have been compele PracticeB

---
+++ Course/Lesson2/PracticeB/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticeB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите год рождения:");
+        int year = Convert.ToInt32(Console.ReadLine());
+        if (year < 2023){
+            Console.WriteLine("Ваш возраст: " + (2023 - year) +  " или " + (2023 - year + 1) + " лет");
+        }
+        else{
+            Console.WriteLine("Такого не может быть. Повторите попытку");
+        }
+    }
+}
---
+++ Course/Lesson2/PracticeC/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

bf0c2417294b84bd4bfa56852cbd18e2fc68d2cc
Ivanova Daria <ida30062006@gmail.com>
2023-11-24 18:21:00 +0300

    feature: all hw

---
+++ Course/Lesson2/Homework/hw1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = a + b;
+
+
+Console.WriteLine("The sum is " + c);
---
+++ Course/Lesson2/Homework/hw2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rnd = new Random();
+int random = rnd.Next();
+
+Console.WriteLine("Enter your number: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<random)
+{
+    Console.WriteLine("Your number is less");
+}
+
+else if (a>random)
+{
+    Console.WriteLine("Your number is more");
+}
+
+else 
+{
+    Console.WriteLine("Your number is right!!!!!");
+}
+

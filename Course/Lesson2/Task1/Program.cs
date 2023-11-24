158e63e482019132974d5cd8f9db15df3f1aa4f2
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-11-24 19:46:35 +0300

    Feat(Lesson 2): Task1

---
+++ Course/Lesson2/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свое имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, " + name + "!");
+
+    }
+}

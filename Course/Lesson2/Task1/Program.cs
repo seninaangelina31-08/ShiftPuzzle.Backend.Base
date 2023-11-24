cc391f076f6e389bae287238c34cc338ad9bc6f7
DABer1805 <DABer1805@yandex.ru>
2023-11-24 20:36:49 +0300

    feature: Add Task1 solution

---
+++ Course/Lesson2/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShiftPuzzle.Backend.Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет, как тебя зовут? ");
+            string user_name = Console.ReadLine() ?? "Человек";
+
+            Console.Write("Привет, " + user_name + "!");
+        }
+    }
+}

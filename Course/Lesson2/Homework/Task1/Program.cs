82a05423cd39334c6e51b87e26c9f0c11ff406ce
Laki <r.matigorov@yandex.ru>
2023-11-24 19:43:22 +0300

    feat(Homework): Homework completed

---
+++ Course/Lesson2/Homework/Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿int x, y;
+Console.WriteLine("Сумма двух чисел. Первое число: ");
+Console.WriteLine("Первое число: "); x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: "); y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(x + y); 
---
+++ Course/Lesson2/Homework/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int random = 12, inten;
+Console.WriteLine("Игра 'Угадай число!'");
+Console.WriteLine("Правила: Вводите числа, а мы вам будем подсказыать больше загаданое число или меньше.");
+Console.WriteLine("НАЧАЛИ!");
+while (true)
+{
+    inten = Convert.ToInt32(Console.ReadLine());
+    if (inten == random)
+    {
+        Console.WriteLine("УРА! Ты угадал! Вот тебе приз!(Подарок: Выход из вечного цикла)");
+        break;
+    }
+    else if (inten > random)
+    {
+        Console.WriteLine("Меньше.");
+    }
+    else
+    {
+        Console.WriteLine("Больше.");
+    }
+}

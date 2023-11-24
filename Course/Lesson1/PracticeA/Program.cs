5d1be6f54e8167ca2596178cd535640990a62643
Nikita Bykov <nikita.bykov.322@gmail.com>
2023-11-24 19:11:18 +0300

    dev: Разработка Lesson1

---
+++ Course/Lesson1/PracticeA/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticeA;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 19;
+        double pi = 3.14;
+        string str = "Строка";
+        bool logical = true;
+        Console.WriteLine(age);
+        Console.WriteLine(pi);
+        Console.WriteLine(str);
+        Console.WriteLine(logical);
+    }
+}
---
+++ Course/Lesson1/PracticeB/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticeB;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int a = Console.Read();
+        // int b = Console.Read();
+        // Console.WriteLine(a+b);
+        // Console.WriteLine(a-b);
+        // Console.WriteLine(a*b);
+        // Console.WriteLine(a/b);
+    }
+}

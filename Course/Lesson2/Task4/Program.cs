66ed222be435b53125cc0b4257053bd1a3cbd968
Laki <r.matigorov@yandex.ru>
2023-11-24 19:15:40 +0300

    feat(Task4): Task completed

---
+++ Course/Lesson2/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int count, i, t;
+count = 0;
+Console.WriteLine("Счётчик нажатий на Enter! Для того чтобы остановиться введи любой текст.");
+for (i = 0; i < 1; i = 0)
+{
+    if (Console.ReadLine() == ""){
+        count = count + 1;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine("Ты нажал на Enter: " + count + " раз(а).");

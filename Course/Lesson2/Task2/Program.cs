76e3ad787bbfaf51c6c05af504306ac949d96c1b
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-11-24 19:51:25 +0300

    Feat(Lesson 2): Task2

---
+++ Course/Lesson2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int current_year = 2023;
+        Console.WriteLine("Введите год своего рождения:");
+        int year = Convert.ToInt32(Console.ReadLine());
+        int age = current_year - year;
+        Console.WriteLine("Вам " + age + " лет!");
+
+    }
+}

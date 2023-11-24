7d661cafa2cb1a91806654eaa76ae819aae944a9
venimu <ven1mu@yandex.ru>
2023-11-24 17:05:25 +0300

    new file:   Course/Lesson1/Lesson1.csproj new file:   Course/Lesson1/Program.cs new file:   ShiftPuzzle.Backend.Base.sln

---
+++ Course/Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+
+namespace Lesson1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task A
+        int a;
+        bool b;
+        long l;
+        float f;
+
+        // Task B
+        int d = Int32.Parse (Console.ReadLine());
+        int c = Int32.Parse (Console.ReadLine()); 
+
+        Console.WriteLine(d+c);
+        Console.WriteLine(d-c);
+        Console.WriteLine(d*c);
+        Console.WriteLine(d/c);
+
+        //Task C
+        int s = int.Parse(Console.ReadLine());
+        Console.WriteLine(s+5);
+         
+    }
+}

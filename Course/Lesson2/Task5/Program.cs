340124a0482364305bd4af55d4fa62bf4b9c5676
DABer1805 <DABer1805@yandex.ru>
2023-11-24 20:42:33 +0300

    feature: Add Task4 solution

---
+++ Course/Lesson2/Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShiftPuzzle.Backend.Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи что-нибудь: ");
+            string user_text = Console.ReadLine() ?? "";
+            Console.Write("Ты ввёл: " + user_text);
+        }
+    }
+}

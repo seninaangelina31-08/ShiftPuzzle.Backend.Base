4b8f1f893a867cab21f260bf7340b58b24e08fbc
Юрий <zaretskiy.y.a@gmail.com>
2023-11-24 18:44:18 +0300

    feature homework

---
+++ Course/Lesson2/hm1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace hm1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The sum is " + (a + b));
+    }
+}
---
+++ Course/Lesson2/hm2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hm2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int random = rnd.Next(0, 100);
+        int a = Convert.ToInt32(Console.ReadLine());
+        while (a != random){
+            if (a > random){
+                Console.WriteLine("Меньше");
+                a = Convert.ToInt32(Console.ReadLine());
+            } else {
+                Console.WriteLine("Больше");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}

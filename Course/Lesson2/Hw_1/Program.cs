39a65174140765e4fe3ef9ad654efafb0ce1b4a7
Satlykovs <satlykovs@gmail.com>
2023-11-24 20:01:14 +0300

    feat: homework practice done in Lesson2 Hw_(1 - 2)

---
+++ Course/Lesson2/Hw_1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hw_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b;
+        string str = "0";
+        Console.WriteLine("Добро пожаловать в калькулятор!");
+        Console.Write("Введите ваше первое число: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите ваше второе число: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Сумма ваших двух чисел: " + (a + b));
+        
+    }
+}
---
+++ Course/Lesson2/Hw_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Hw_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {   Random random = new Random();
+        int secret_number = random.Next(1, 101);
+
+        int guess;
+        int attempts = 0;
+
+        Console.WriteLine("Угадайте число от 1 до 100.");
+        do
+        {
+            Console.Write("Введите вашу догадку: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            attempts += 1;
+
+            if (guess < secret_number)
+            {
+                Console.WriteLine("Загаданное число больше.");
+            }
+            else if (guess > secret_number)
+            {
+                Console.WriteLine("Загаданное число меньше.");
+            }
+            else
+            {
+                Console.WriteLine($"Поздравляем! Вы угадали число {secret_number} за {attempts} попыток.");
+            }
+        } while (guess != secret_number);
+
+        Console.ReadLine();
+    }
+}
+

0e85d0a45d2b8b11fd4708f659297c0da2437905
Laki <r.matigorov@yandex.ru>
2023-11-24 19:15:23 +0300

    feat(Task3): Task completed

---
+++ Course/Lesson2/Task3/Program.cs
@@ -0,0 +1,3 @@
+﻿string name, number;
+Console.Write("Введите имя пользователя: "); name = Console.ReadLine() ?? "Noname"; Console.Write("Введите номер телефона пользоватля: "); number = Console.ReadLine() ?? "0";
+Console. WriteLine("Пользователь: " + name + " добавлен в  контакты. Его номер телефона: " + number);

fe56d3241a46abede6b9d463ab731c1a189fafe8
Ivanova Daria <ida30062006@gmail.com>
2023-11-24 17:47:54 +0300

    featuer: prac4

---
+++ Course/Lesson2/practic4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your text: ");
+
+
+string s = Console.ReadLine();
+int k = s.Count(x => x == '\n');
+Console.WriteLine(k);

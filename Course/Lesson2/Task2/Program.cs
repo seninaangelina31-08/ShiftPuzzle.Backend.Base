09535817e83dbafe7c9db71708d6db10849aba61
Laki <r.matigorov@yandex.ru>
2023-11-24 19:14:53 +0300

    feat(Task2): Task completed

---
+++ Course/Lesson2/Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿int year_born, year;
+year = 2023;
+Console.Write("Пожалуйста, введите свой год рождения: ");
+year_born = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вам " + (year - year_born) + " лет.");

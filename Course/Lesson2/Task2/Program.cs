deb4d29f328f96a71c1dcbb60f6b05fff7fff11d
DABer1805 <DABer1805@yandex.ru>
2023-11-24 20:38:45 +0300

    feature: Add Task2 solution

---
+++ Course/Lesson2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShiftPuzzle.Backend.Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentYear = DateTime.Now.Year;
+            Console.Write("Привет, введи свой год рождения: ");
+            string user_birth_year = Console.ReadLine() ?? "";
+
+            if (user_birth_year.Length == 0) {
+                Console.Write("Что-то ты не разговорчивый :(");
+            }
+            else if (Convert.ToInt64(user_birth_year) > currentYear) {
+                Console.Write("А ты смотрю ещё не родился )))");
+            }
+            else {
+                Console.Write("Твой возраст: ");
+                Console.Write(currentYear - Convert.ToInt64(user_birth_year));
+            }
+
+            
+        }
+    }
+}

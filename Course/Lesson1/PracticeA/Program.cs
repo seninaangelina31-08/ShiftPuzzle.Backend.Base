49329fb62cda10ea77803a394a4b96a4e378c654
Laki <r.matigorov@yandex.ru>
2023-11-24 17:24:13 +0300

    feature: new file

---
+++ Course/Lesson1/PracticeA/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = 2023;
+double y = 16.9d;
+string str = "THE WORLD!";
+bool logic = false;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(str);
+Console.WriteLine(logic);
+// test coment
---
+++ Course/Lesson1/Program.cs
@@ -0,0 +1,8 @@
+﻿int x, y;
+Console.ReadLine(x, y);
+Convert.ToInt64(x);
+Convert.ToInt64(y);
+Console.WriteLine("Сложение:" x + y);
+Console.WriteLine("Вычитание:" x - y);
+Console.WriteLine("Умножение:" x * y);
+Console.WriteLine("Деление:" x / y);

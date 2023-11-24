fa014113ce3d0b914b3230ec84fdc23b845437ed
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-11-24 23:39:17 +0300

    Feat(Lesson 2): Task4, 5

---
+++ Course/Lesson2/Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+
+        Console.WriteLine("Привет!");
+        Console.WriteLine("Готов? (да/нет)");
+
+        string answer1 = Console.ReadLine();
+
+        if (answer1 == "да")
+        {   
+            while(true)
+            {
+            Console.WriteLine("Нажмите на клавишу Enter");
+            Console.ReadLine();
+
+            counter++;
+            Console.WriteLine("Текущее значение: " + counter);
+            }
+        }
+
+        else if (answer1 == "нет")
+        {
+            Console.WriteLine("Хорошо. Как будешь готов, выбирай ответ: да!");
+        }
+
+        else
+        {
+            Console.WriteLine("Ты опечатался :( ");
+        }
+    }
+}
---
+++ Course/Lesson2/Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введи текст:");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("Введенный текст: " + text);
+
+    }
+}

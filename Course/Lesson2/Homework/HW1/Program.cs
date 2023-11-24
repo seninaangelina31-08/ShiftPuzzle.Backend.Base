f8ddac83c7aeb749a406809973ce9d90c4ec00e4
Vladislaw <vlad.kalinin044@gmail.com>
2023-11-24 21:20:53 +0300

    Во втором задание выводится "Больше", Если Вводимые числа когда то были меньше. Решение не нашел(

---
+++ Course/Lesson2/Homework/HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HW1;
+class Program
+/*Создайте калькулятор, который принимает два числа от пользователя, сохраняет их в переменных, складывает их и выводит результат.*/
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int count_1 = Int32.Parse(Console.ReadLine());
+        int count_2 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Сумма: " + (count_1 + count_2));
+    }
+}
---
+++ Course/Lesson2/Homework/HW2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HW2;
+class Program
+/***2.Игра угадай число:**
+Загадайте случайное число в переменной и попросите пользователя угадать это число, предоставляя подсказки (больше/меньше). */
+{
+    static void Main(string[] args)
+    {
+        var rand = new Random();
+        int count;
+        int temp_count;
+        while(true)
+        {
+            Console.WriteLine("Угадайте число");
+            count = rand.Next(100);
+            while(true)
+            {
+                Console.WriteLine("Какое число");
+                temp_count = int.Parse(Console.ReadLine());
+                Console.WriteLine(temp_count + "Вывод");
+                if(temp_count == count)
+                {
+                    Console.WriteLine("Верно");
+                }
+                else if (temp_count > count)
+                {
+                    Console.WriteLine("Меньше");
+                    if (temp_count < count);
+                      {
+                          Console.WriteLine("Больше");
+                      }
+                }
+                Console.WriteLine();
+                Console.WriteLine(count + "Ответ");
+            }
+
+        }
+    }
+}

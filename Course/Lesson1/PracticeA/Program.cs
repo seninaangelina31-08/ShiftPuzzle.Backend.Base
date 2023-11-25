5b8614820dc295cfa70054ba666f1ceb7496a7b8
ElenInka <anonymousinkelen@gmail.com>
2023-11-25 17:05:01 +0400

    feat; new program (PracticeA)

---
+++ Course/Lesson1/PracticeA/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeA;
+class Program
+{
+    string my_town = "Belgorod";
+    int age_town = 427;
+    float square_town = 153.1d;
+    bool place_of_living = true;
+} 
+Console.WriteLine(age_town);

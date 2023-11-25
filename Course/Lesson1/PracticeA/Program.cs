81d2360ae0ee76bd3b9167c000aa620fee75df68
Ваня <ivantimofeev0006@gmail.com>
2023-11-25 12:18:25 +0300

    PracticeA

---
+++ Course/Lesson1/PracticeA/Program.cs
@@ -0,0 +1,9 @@
+Int a = 5;
+float b = 12781.1332;
+strind c = "BACKEND";
+bool d = false;
+
+Console.WriteLine(a) // test comment
+Console.WriteLine(b) // test comment
+Console.WriteLine(c) // test comment
+Console.WriteLine(d) // test comment

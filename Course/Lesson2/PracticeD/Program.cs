1e2d0c5a799b6493918040a708748a771c3dfd9c
timurkaff <timalololohka@gmail.com>
2023-11-25 22:00:00 +0300

    feat:(Добавил практическое задание D)

---
+++ Course/Lesson2/PracticeD/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticeD;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int enterCount = 0; // Переменная для подсчета нажатий Enter
+
+        Console.WriteLine("Нажмите Enter несколько раз. Для завершения введите 'выход' и нажмите Enter.");
+
+        string userInput = "";
+        while (userInput.ToLower() != "выход")
+        {
+            userInput = Console.ReadLine();
+
+            if (userInput == "")
+            {
+                enterCount++; // Увеличение счетчика при нажатии Enter
+                Console.WriteLine($"Количество нажатий Enter: {enterCount}");
+            }
+        }
+
+        Console.WriteLine("Программа завершена.");
+    }
+}

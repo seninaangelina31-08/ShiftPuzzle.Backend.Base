048ed9f53984e711f81128540dc17dd4e5b68231
Maxon <maxn2703@gmail.com>
2023-11-25 17:43:47 +0300

    feature: practice A,B,C

---
+++ Course/Lesson1/Practise B/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practise_B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите 1 число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма {num1 + num2}");
+Console.WriteLine($"Вычитание {num1 - num2}");
+Console.WriteLine($"Деление {num1 / num2}");
+Console.WriteLine($"Умножение {num1 * num2}");
+    }
+}
---
+++ Course/Lesson1/Practise C/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practise_C;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+Console.Write("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1 + 5);
+    }
+}

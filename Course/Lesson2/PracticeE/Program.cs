3566c8805541cd1bb71da4e377d076e8263f8e16
timurkaff <timalololohka@gmail.com>
2023-11-25 22:00:11 +0300

    feat:(Добавил практическое задание E)

---
+++ Course/Lesson2/PracticeE/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PracticeE;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Введите Текст:");  
+    
+    string text = (Console.ReadLine());
+
+    Console.WriteLine($"Ваш текст: {text} "); 
+        
+    }
+}

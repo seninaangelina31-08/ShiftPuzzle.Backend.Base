036d4f07e9d8b344ebff429b2db0ce0a6cb030df
timurkaff <timalololohka@gmail.com>
2023-11-25 21:59:41 +0300

    feat:(Добавил практическое задание B)

---
+++ Course/Lesson2/PracticeB/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите год рождения:");
+        
+        // Чтение введенного года рождения
+        int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+        
+        // Получение текущего года
+        int currentYear = DateTime.Now.Year;
+        
+        // Вычисление возраста
+        int age = currentYear - yearOfBirth;
+        
+        // Вывод возраста
+        Console.WriteLine($"Ваш возраст: {age} лет");
+    }
+}

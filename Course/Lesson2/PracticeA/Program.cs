4773d89e1882ebfbf13bbae4220a1904fcefd311
timurkaff <timalololohka@gmail.com>
2023-11-25 21:59:17 +0300

    feat:(Добавил практическое задание A)

---
+++ Course/Lesson2/PracticeA/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeA;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Привет, " + name + "! Рад тебе.");
+    }
+}

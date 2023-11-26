d61a4f604c78269237ece3ef57843f158f54e766
Ваня <ivantimofeev0006@gmail.com>
2023-11-26 19:08:15 +0300

    homework

---
+++ Course/Lesson2/Homework/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Укажите первое число ");
+string a = Console.ReadLine();
+int number1 = int.Parse(a);
+
+Console.WriteLine("Укажите второе число ");
+string b = Console.ReadLine();
+int number2 = int.Parse(b);
+
+int result = number1 + number2;
+Console.WriteLine(number1 + " + " + number2 + " = " + result);
---
+++ Course/Lesson2/Homework/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = 0;
+int number1 = 67;
+;
+
+Console.WriteLine("Загадано число от 0 до 100");
+Console.WriteLine("Ваша задача угадать данное число. ");
+
+while (true)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number == number1)
+    {
+        Console.WriteLine("Вы угадали число!");
+        break;
+    }
+    else if (number > number1)
+    {
+        Console.WriteLine("Загаданное число меньше...");
+    }
+    else
+    {
+        Console.WriteLine("Загаданное число больше..."); 
+    }
+}

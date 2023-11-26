bebf3b3c90560c0d161949885bc41521c5d411b6
John Doe <johndoe@example.com>
2023-11-26 21:32:06 +0300

    feat: сделали homework

---
+++ Course/Lesson2/Homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ShiftPuzzle.Backend.Base;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double result = a + b;
+        
+        Console.WriteLine("Сумма введенных вами чисел: " + result);
+}
+}
---
+++ Course/Lesson2/Homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ShiftPuzzle.Backend.Base;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int number = rand.Next(1, 101);
+        int chislo = 0;
+        
+        Console.WriteLine("Угадайте случайное число от 1 до 100:");
+        while (chislo != number)
+        {
+            Console.Write("Попробуйте угадать число: ");
+            chislo = Convert.ToInt32(Console.ReadLine());
+            
+            if (chislo < number)
+            {
+                Console.WriteLine("Загаданное число больше");
+                
+            }
+            else if (chislo > number)
+            {
+                Console.WriteLine("Загаданное число меньше.");
+            }
+            else
+            {
+                Console.WriteLine(" Вы угадали! Загаданное число - " + number);
+            }
+        }
+    }
+}
+

218c8f2e82d1aaa56b4b2e9f29a15ddab90752b2
MisterCat100 <mistercat0618@gmail.com>
2023-11-26 17:22:44 +0300

    feat(HW_Task[1-2]): completed HW from boss

---
+++ Course/Lesson2/HW_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HW_Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+
+    }
+}
---
+++ Course/Lesson2/HW_Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW_Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, я загадал случайное число от 1 до 100 включительно, попробуй угадать!)");
+        Random rnd1 = new();
+        int answer = rnd1.Next(1, 100);
+        int userNum = -1;
+        while (answer != userNum)
+        {
+            Console.WriteLine("Введите число, которое я загадал: ");
+            userNum = Convert.ToInt16(Console.ReadLine());
+            if (userNum > answer)
+            {
+                Console.WriteLine("Загаданное число меньше " + userNum);
+            }
+            else
+            {
+                Console.WriteLine("Загаданное число больше " + userNum);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Вы угадали! Загаданное число = " + userNum);
+        
+    }
+}

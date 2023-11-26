6f76132035281eb4a8bb1f122cf94b575526c134
DeveloperInPants <fockstrot057@mail.ru>
2023-11-26 11:54:09 +0300

    feat: made it possible to save and write text

---
+++ Course/Lesson2/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace task5;
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите какой-нибудь текст");
+        string text = Console.ReadLine();
+        Console.WriteLine(text);
+    }
+}

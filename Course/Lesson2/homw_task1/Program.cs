be8edf0c6959affe159586523665fb2a414a7a9b
DeveloperInPants <fockstrot057@mail.ru>
2023-11-26 12:44:06 +0300

    feat: completed my homework

--- Course/Lesson2/homw_task1/Program.cs
+++ Course/Lesson2/homw_task1/Program.cs
@@ -1,9 +1,16 @@
 ﻿namespace homw_task1;
 
+using System;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Напишите первое число");
+        double num1 = Convert.ToDouble(Console.ReadLine()); 
+        Console.WriteLine("Напишите второе число");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double result = 0;
+        result = num1 + num2;
+        Console.WriteLine(result);
     }
 }
---
+++ Course/Lesson2/homw_task2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace homw_task2;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int my_num = 15;
+        Console.WriteLine("Угадайте число от 1 до 50");
+        int user_num;
+        bool answer = false;
+
+        while (!answer)
+        {
+            Console.Write("Введите ваше предположение: ");
+            user_num = Convert.ToInt32(Console.ReadLine());
+
+            if (user_num > my_num)
+            {
+                Console.WriteLine("Нет, моё число меньше");
+            }
+            else if (user_num < my_num)
+            {
+                Console.WriteLine("Нет, моё число больше");
+            }
+            else
+            {
+                Console.WriteLine("Да, вы угадали");
+                answer = true;
+            }
+        }
+
+    }
+}
+

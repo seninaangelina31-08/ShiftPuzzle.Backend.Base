1ae3d7c679b99ae78afef7e485a052ab1ba2df6f
“Valery” <117990439+ValeryManoilov@users.noreply.github.com>
2023-11-26 21:03:18 +0300

    add: have been added and completed Homeworks A/B

---
+++ Course/Lesson2/PracticeA_hm/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PracticeA_hm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Программа: простейший калькулятор");
+        Console.WriteLine("Введите первое число:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Сумма двух ваших чисел: " + (num1 + num2));
+    }
+}
---
+++ Course/Lesson2/PracticeB_hm/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticeB_hm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rdn = new Random();
+        int rd_num = rdn.Next(1, 11);
+        Console.WriteLine("Введите число от 1 до 10:");
+        int user_num = Convert.ToInt32(Console.ReadLine());
+        while (user_num != rd_num){
+            
+            if (user_num < rd_num)
+            {
+                Console.WriteLine("Загаданное число больше");
+                user_num = Convert.ToInt32(Console.ReadLine());
+
+            }
+            else if (user_num > rd_num)
+            {
+                Console.WriteLine("Загаданное число меньше");
+                user_num = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Вы угадали, поздравляю!");
+    }
+}

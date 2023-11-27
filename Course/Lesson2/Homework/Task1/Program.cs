7f0d0c550c9350b51526ac19ad770b559742ec2c
venimu <ven1mu@yandex.ru>
2023-11-27 14:38:04 +0300

    new file:   Course/Lesson2/Homework/Task1/Program.cs 	new file:   Course/Lesson2/Homework/Task1/Task1.csproj 	new file:   Course/Lesson2/Homework/Task2/Program.cs 	new file:   Course/Lesson2/Homework/Task2/Task2.csproj

---
+++ Course/Lesson2/Homework/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Int32.Parse(Console.ReadLine());
+        int b = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(a+b);
+    }
+}
---
+++ Course/Lesson2/Homework/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1343684;
+        Console.WriteLine("Попробуйте угадать число");
+        int b = Int32.Parse(Console.ReadLine());
+        while (b!=a) {
+            if (b < a) {
+                Console.WriteLine('>');
+            } else {
+                Console.WriteLine('<');
+            }
+            b = Int32.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Congrats");
+        
+    }
+}

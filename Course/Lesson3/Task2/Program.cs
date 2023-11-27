be5d1092c3b2c796d0ec98e21485fa8002a5fde9
Shadik23 <kurichjev@yandex.ru>
2023-11-27 20:41:24 +0300

    feat(Lesson3. Выполнены задания с 1 по 9)

---
+++ Course/Lesson3/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myList = new string[3]
+
+        Console.Write("Item 1: ");
+        myList[0] = Console.ReadLine() ??"";
+
+        Console.Write("Item 2: ");
+        myList[1] = Console.ReadLine() ??"";
+
+        Console.Write("Item 3: ");
+        myList[2] = Console.ReadLine() ??"";
+
+        Console.WriteLine($"1. {myList[0]}")
+        Console.WriteLine($"2. {myList[1]}")
+        Console.WriteLine($"3. {myList[2]}")
+    }
+}

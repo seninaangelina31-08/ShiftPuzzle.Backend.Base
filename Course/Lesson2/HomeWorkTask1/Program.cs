a2883ac59e5c6baca97df35d5df90cbea8c23783
DABer1805 <DABer1805@yandex.ru>
2023-11-28 18:48:54 +0300

    feat: add homework solution

---
+++ Course/Lesson2/HomeWorkTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShiftPuzzle.Backend.Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи 1-е число: ");
+            string a = Console.ReadLine() ?? "0";
+            Console.Write("Введи 2-е число: ");
+            string b = Console.ReadLine() ?? "0";
+
+            Console.Write("Сумма чисел: " + Convert.ToString(Convert.ToInt64(a) + Convert.ToInt64(b)));
+        }
+    }
+}
---
+++ Course/Lesson2/HomeWorkTask2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ShiftPuzzle.Backend.Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random_value = rnd.Next(0, 100);
+            string user_ans;
+
+            Console.WriteLine("Привет, ахахах, я загадал чслучайное число от 1 до 100, и пока ты его не назовёшь, я тебя не выпущу )))");
+            while (true) {
+                Console.Write("Введи число: ");
+                user_ans = Console.ReadLine() ?? "";
+                if (user_ans.Length == 0) {
+                    Console.Write("");
+                } else {
+                    if (Convert.ToInt64(user_ans) < random_value) {
+                        Console.WriteLine("Больше");
+                    } else if (Convert.ToInt64(user_ans) > random_value) {
+                        Console.WriteLine("Меньше");
+                    }
+                    else {
+                        Console.WriteLine("Угадал, повезло, повезло )))");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

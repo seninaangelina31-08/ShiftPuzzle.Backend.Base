a6e54a5c6750c3d2bfddc4448cbb8977b67c7c87
Nikita Bykov <nikita.bykov.322@gmail.com>
2023-11-28 17:00:43 +0300

    feat: Lesson2 Homework

---
+++ Course/Lesson2/Homework/Ex1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ex1;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        double pow(double a, double b)
+        {
+            double x = a;
+            for (int i = 2; i <= b; i++)
+            {
+                x = x * a;
+            }
+            return(x);
+        }
+        Console.WriteLine("Введите первое число: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите что вы хотите сделать: (+ - сложить, - - вычесть, * - умножить, / - поделить, % - узнать остаток от деления, ^ - возвести в степень)");
+        string op = Console.ReadLine();
+        switch (op)
+        {
+            case "+":
+                Console.WriteLine($"{a} {op} {b} = {a + b}");
+                break;
+            case "-":
+                Console.WriteLine($"{a} {op} {b} = {a - b}");
+                break;
+            case "*":
+                Console.WriteLine($"{a} {op} {b} = {a * b}");
+                break;
+            case "/":
+                Console.WriteLine($"{a} {op} {b} = {a / b}");
+                break;
+            case "%":
+                Console.WriteLine($"{a} {op} {b} = {a % b}");
+                break;
+            case "^":
+                Console.WriteLine($"{a} {op} {b} = {pow(a, b)}");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Course/Lesson2/Homework/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int randint = rand.Next(0, 101);
+        int pop = 0;
+        int a;
+        while(true)
+        {
+            pop++;
+            Console.Write("Попробуйте отгадать число: ");
+            a = Convert.ToInt16(Console.ReadLine());
+            if (a == randint)
+            {
+                Console.WriteLine($"Поздравляю! Это число {randint}. Вы справились с {pop} раза");
+                break;
+            }
+            else if (a > randint)
+            {
+                Console.WriteLine("Закаганное число меньше");
+            }
+            else
+            {
+                Console.WriteLine("Закаганное число больше");
+            }
+        } 
+    }
+}

705775243469f33be988a28a69c4c6661976f958
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-11-28 20:13:38 +0300

    Feat(Lesson 3): Tasks1-9

---
+++ Course/Lesson3/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = {1, 2, 3, 4, 5};
+        numbers[2] = 100;
+    }
+}
---
+++ Course/Lesson3/Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[5]; 
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+    }
+}

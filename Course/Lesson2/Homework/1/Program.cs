381f2fa4dbfd284a58fccae944e19c6694273b4e
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-11-28 18:42:12 +0300

    Feat(Homework): 1,2

---
+++ Course/Lesson2/Homework/1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        double sum = firstNumber + secondNumber;
+        Console.WriteLine("Сумма чисел равна: " + sum);
+    }
+}
---
+++ Course/Lesson2/Homework/2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _2;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 11); // загадываем число от 1 до 10
+        
+        int guess = 0;
+        bool correctGuess = false;
+
+        Console.WriteLine("Я загадал число от 1 до 10. Попробуйте угадать.");
+
+        while (!correctGuess)
+        {
+            Console.Write("Введите ваше предположение: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < secretNumber)
+            {
+                Console.WriteLine("Загаданное число больше вашего предположения.");
+            }
+            else if (guess > secretNumber)
+            {
+                Console.WriteLine("Загаданное число меньше вашего предположения.");
+            }
+            else
+            {
+                correctGuess = true;
+                Console.WriteLine("Поздравляю! Вы угадали число.");
+            }
+        }
+
+        Console.WriteLine("Игра окончена. Спасибо за игру!");
+    }
+}

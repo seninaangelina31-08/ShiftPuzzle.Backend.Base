9ed88dab2c2bcf4813c96ee95710f4315b090c44
Maxon <maxn2703@gmail.com>
2023-11-30 20:30:58 +0300

    dev: Tasks 1-9 Lesson 3

---
+++ Course/Lesson3/Task 9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] massive = new int[5];
+        massive[1] = 1
+        massive[2] = 1
+    massive[3] = 1
+    massive[4] = 1
+    massive[5] = 1
+    massive[6] = 1
+}
+
+
+

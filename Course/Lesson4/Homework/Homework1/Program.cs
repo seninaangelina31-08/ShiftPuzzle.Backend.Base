ba987c91a4582a9915f862b8eb22a85aaae040e1
Laki <r.matigorov@yandex.ru>
2023-12-04 20:46:25 +0300

    feat(Homework): Complited

---
+++ Course/Lesson4/Homework/Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 2, 3, 4, 5};
+int[] array1 = {6, 7, 8, 9, 10};
+int[] array_mirage = new int[(array.Length + array1.Length)];
+for (int i = 0; i < array.Length; i++)
+{
+    array_mirage[i] = array[i];
+}
+for (int i = 0; i < array1.Length; i++)
+{
+    array_mirage[i + array.Length] = array1[i];
+}
+foreach (int item in array_mirage)
+{
+    Console.WriteLine(item);
+}
---
+++ Course/Lesson4/Homework/Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = {1, 2, 3, 4, 5};
+int[] array1 = new int[array.Length];
+foreach (int arr in array)
+{
+    Console.Write(arr);
+}
+Console.WriteLine();
+int i1 = 0;
+string s = Console.ReadLine() ?? "1";
+int s1 = Convert.ToInt32(s);
+for (int i = 0; i < array.Length; i++)
+{
+    i1 = i + s1;
+    while ((i1 > array.Length) || (i1 == array.Length))
+    {
+        i1 -= array.Length;
+    }
+    array1[i1] = array[i];
+}
+foreach (int arr in array1)
+{
+    Console.Write(arr);
+}
+Console.WriteLine();

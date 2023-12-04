4fe41c863dded196c853b530b1329f13d9561398
Shadik23 <kurichjev@yandex.ru>
2023-12-04 18:28:25 +0300

    feat(Выполнение практики Task1 - Task10)

---
+++ Course/Lesson4/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 2, 4, 6, 8 };
+        Array.Reverse(array);
+ 
+        Console.WriteLine(String.Join(',', array));
+    }
+}
---
+++ Course/Lesson4/Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 2, 7, 1, 9, 3 };
+
+        Console.WriteLine("Отсортированный массив по возрастанию:");
+        Array.Sort(array);
+
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}

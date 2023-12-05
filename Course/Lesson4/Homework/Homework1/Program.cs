fc45ae93626a03d3d9d30f76e8c7d87868e00701
“Valery” <117990439+ValeryManoilov@users.noreply.github.com>
2023-12-05 20:07:53 +0300

    add: have been completed all Homeworks in Lesson4

--- Course/Lesson4/Homework/Homework1/Program.cs
+++ Course/Lesson4/Homework/Homework1/Program.cs
@@ -3,6 +3,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array1 = {1, 1, 2, 3, 5};
+        int[] array2 = {1, 8, 27, 64, 125};
+
+        int[] array_conc = new int[array1.Length + array2.Length];
+
+        for (int i = 0; i < array1.Length; i++){
+            array_conc[i] = array1[i];
+        }
+        for (int i = 0; i < array2.Length; i++){
+            array_conc[i + array1.Length] = array2[i];
+        }
+
+        Console.WriteLine(string.Join(",", array_conc));
     }
 }
---
+++ Course/Lesson4/Homework/Homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array_nums = {1, 2, 3, 4, 5};
+            int[] array_nums_copy = new int [array_nums.Length];
+            int K = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(string.Join(",", array_nums));
+            int[] array_new = array_nums[(array_nums.Length - K)..array_nums.Length];
+
+            for (int i = 0; (i + K) < array_nums.Length; i++)
+            {
+                array_nums_copy[i + K] = array_nums[i];
+            }
+
+            for (int i = 0; i < array_new.Length; i++)
+            {
+                array_nums_copy[i] = array_new[i];
+            }
+            Console.WriteLine(string.Join(",", array_nums_copy));
+    }
+}

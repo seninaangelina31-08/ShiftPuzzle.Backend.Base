fb407b4824cbc9246dbc9ec9c4ea96ff0363cd76
Nikita Bykov <nikita.bykov.322@gmail.com>
2023-12-05 23:25:03 +0300

    feat: Lesson5, A-B

--- Course/Lesson5/PracticeA-B/LessonProject/Program.cs
+++ Course/Lesson5/PracticeA-B/LessonProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Task1 t1 = new Task1();
+            // Task1 t1 = new Task1();
             Task2 t2 = new Task2();
             Task3 t3 = new Task3();
             Task4 t4 = new Task4();
--- Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
+++ Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
@@ -10,7 +10,32 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            Console.Write("Введите длину основного массива: ");
+            int m = Convert.ToInt16(Console.ReadLine());
+            int[] arr = new int[m];
+            Console.WriteLine($"Введите массив длинной {m}:");
+            for(int i=0; i<arr.Length; i++)
+            {
+                Console.Write($"Введите {i+1} элемент: ");
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.Write("Введите длину подмассива: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            while(n > m)
+            {
+                Console.Write("Длинна подмассива должна быть меньше длины массива: ");
+                n = Convert.ToInt16(Console.ReadLine());
+            }
+            int[] arr2 = new int[n];
+            for(int i=0; i<arr2.Length; i++)
+            {
+                int max = arr.Max();
+                arr2[i] = max;
+                int index = Array.IndexOf(arr, max); 
+                arr = arr.Where((e, j) => j != index).ToArray();
+            }
+            Console.WriteLine("Найденный подмасив: ");
+            Console.WriteLine(String.Join(", ", arr2));
             //не выходя за пределы
         }
     }
@@ -23,7 +48,41 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            Random rand = new Random();
+            int[] arr = new int[15];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 51);
+            }
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr.Length-i-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int c = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = c;
+                    }
+                }
+            }
+            bool unique = true;
+            int count = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                unique = true;
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+                if (unique)
+                    count++;
+            }
+            Console.WriteLine(String.Join(", ", arr) + $"\nКоличество уникальных элементов: {count}");
             //не выходя за пределы
         }
     }
@@ -36,7 +95,32 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            Random rand = new Random();
+            int[] arr = new int[15];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 11);
+            }
+            var dict = new Dictionary<int, int>();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int rep_count = 0;
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if(arr[i] == arr[j])
+                    {
+                        rep_count++;
+                    }
+                }
+                dict.TryAdd(arr[i], rep_count);
+            }
+            Console.WriteLine("Массив: ");
+            Console.WriteLine(String.Join(", ", arr));
+            foreach( KeyValuePair<int, int> kvp in dict)
+            {
+                if (kvp.Value > 1)
+                    Console.WriteLine($"Число = {kvp.Key}, количесвто повторений {kvp.Value}");
+            }
             //не выходя за пределы
         }
     }
@@ -49,7 +133,26 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            Random rand = new Random();
+            int[] arr = new int[15];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 11);
+            }
+            int[] arr2 = new int[15];
+            Console.Write("Введите на сколько сдвинуть массив вправо: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            for(int i = 0; i< arr.Length; i++)
+            {
+                if (i+n<arr.Length)
+                    arr2[i+n] = arr[i];
+                else
+                    arr2[i+n-arr.Length] = arr[i];
+            }
+            Console.WriteLine("Исходный массив: ");
+            Console.WriteLine(String.Join(", ", arr));
+            Console.WriteLine("Изменённый массив: ");
+            Console.WriteLine(String.Join(", ", arr2));
             //не выходя за пределы
         
         }
@@ -63,7 +166,34 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            Random rand = new Random();
+            int[] arr = new int[15];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 16);
+            }
+            int[] arr2 = new int[15];
+            for(int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = rand.Next(0, 16);
+            }
+            List<int> arr3 = new List<int>();
+            foreach(int i in arr)
+            {
+                foreach(int j in arr2)
+                {
+                    if (i == j && !arr3.Contains(i))
+                        arr3.Add(j);
+                }
+                
+            }
+            Console.WriteLine("Исходные массивы: ");
+            Console.WriteLine("Первый: ");
+            Console.WriteLine(String.Join(", ", arr));
+            Console.WriteLine("Второй: ");
+            Console.WriteLine(String.Join(", ", arr2));
+            Console.WriteLine("Изменённый массив: ");
+            Console.WriteLine(String.Join(", ", arr3));
             //не выходя за пределы
         }
     }

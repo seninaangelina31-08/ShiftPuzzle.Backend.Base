37bf600c8d14b3205e0544b744aea027657b4486
Ivanova Daria <ida30062006@gmail.com>
2023-12-06 18:20:10 +0300

    feature: hw lesson4

---
+++ Course/Lesson4/Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Practice 1
+int[] mass1 = [2, 6, 12, 45, 5, 2, 4, 5];
+int[] mass2 = [2, 6, 12, 45, 5, 2, 4, 5];
+
+int[] mass3 = new int[mass1.Length + mass2.Length];
+
+for (int i = 0; i<mass1.Length; i++){
+    mass3[i] = mass1[i];
+}
+
+for (int j = 0; j < mass2.Length; j++){
+    mass3[mass1.Length + j] = mass2[j];
+}
+Console.WriteLine("\nОбъединённый массив: " + string.Join(", ", mass3));
+
---
+++ Course/Lesson4/Homework/prac2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] mass = [1, 45, 34, 234, 57, 23];
+int K = Convert.ToInt32(Console.ReadLine());
+ 
+Console.WriteLine(string.Join(" ", mass));
+            
+            
+int[] mass2 = new int[mass.Length];
+
+for (int i = 0; i < mass.Length; i++){   
+    if (i + K>= mass.Length){
+        mass2[(i + K) - mass.Length] = mass[i];
+        } 
+    else{
+        mass2[i + K] = mass[i];
+        }
+    }
+
+Console.WriteLine("Измененный массив: " + string.Join(", ", mass2));
+

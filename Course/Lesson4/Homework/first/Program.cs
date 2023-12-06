a0bcb3261a00a0adc704fcde3eedf491fce78d8a
Юрий <zaretskiy.y.a@gmail.com>
2023-12-06 21:11:50 +0300

    feat homework 4

---
+++ Course/Lesson4/Homework/first/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int[] array1 = new int[10];
+        int[] array2 = new int[array1.Length];
+
+        for (int i = 0; i < array1.Length; i++)
+        {
+            array1[i] = random.Next(1, 11);
+            array2[i] = random.Next(1, 11);
+        }
+
+        int[] b = new int[array1.Length + array2.Length];
+
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (i < array1.Length)
+            {
+                b[i] = array1[i];
+            } else {
+                b[i] = array2[i - array1.Length];
+            }
+        }
+
+        Console.WriteLine("Объединённый массив: " + string.Join(", ", b));
+
+    }
+}

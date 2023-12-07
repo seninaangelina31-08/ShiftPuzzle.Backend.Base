d37e449fc5f90eabb06f8f7101b2f22602a7e549
Ivanova Daria <ida30062006@gmail.com>
2023-12-07 21:10:37 +0300

    feature: lesson6 prac A-B

---
+++ Course/Lesson6/PracticeA/Program.cs
@@ -0,0 +1,145 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int summa(int a, int b)
+{
+    return a+b;
+}
+
+
+
+
+
+void privet(string name)
+{
+    Console.WriteLine("Hello, " + name);
+}
+
+
+
+
+
+void max(int a, int b)
+{
+    if (a > b){
+        Console.WriteLine(a);
+    }
+    else{
+        Console.WriteLine(b);
+    }
+}
+
+
+
+
+static bool chet(int a)
+{
+    if (a%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+
+static double tempF(int tempC)
+{
+    return(tempC*33.8);
+}
+
+
+
+static string Reverse(string line )
+{
+    char[] array = line.ToCharArray();
+    Array.Reverse(array);
+    return new string(array);
+}
+
+
+
+
+static void Vhod(string str, char el)
+{
+int cnt = 0;
+foreach (char c in str) {
+  if (c == el) {cnt++;}
+}
+
+Console.WriteLine(cnt);
+}
+
+
+
+
+
+static int Fact(int a)
+{
+    int n = 1;
+    int fac = 1;
+    while (n<=a){
+        fac *= n;
+        n++;
+    }
+    return fac;
+}
+
+
+
+
+
+
+static void Easy(int a)
+{
+    string answer = "Это число простое";
+    
+    for (int n = 1; n<a; n++){
+        if (a%n==0){
+            answer = "Это число не простое";
+        }
+    }
+
+    Console.WriteLine(answer);
+}
+
+
+
+
+
+
+static int Random(int a,int b)
+{
+    Random rnd = new Random();
+    var result = rnd.Next(a, b);
+    return result;
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(summa(3, 6));
+
+        privet("Dasha");
+
+        max(12, 4);
+
+        Console.WriteLine(chet(23));
+
+        Console.WriteLine(tempF(34));
+
+        Console.WriteLine(Reverse("fghjkl"));
+
+        Vhod("dddddd", 'd')
+
+        Console.WriteLine(Fact(5));
+
+        Easy(6);
+
+        Console.WriteLine(Random(1, 5000));
+    }
+}
+
+
---
+++ Course/Lesson6/PracticeB-С/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пратика B
+static void Task1(int [] mass, int N){
+    var maxIndex = 0;
+    var max = 0;
+    for (var i = 0; i < mass.Length - N; i++){
+        var Sum = 0;
+            for (var j = i; j < i + N; j++){
+                Sum += mass[j];
+                if (max < Sum){
+                    max = Sum;
+                    maxIndex = i;
+                }
+            }
+        }
+ 
+ 
+    for (var i = maxIndex; i < maxIndex + N; i++){
+        Console.WriteLine(mass[i]);
+    }
+}
+
+Task1([34, 1, 45, 1, 3, 5, 4, 2], 5);
+
+//Пратика C is in process.....
+
+
+

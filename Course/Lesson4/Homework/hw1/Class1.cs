ca090db0f74ab9b8c976a7d6e290ac23ac3325dc
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-12-07 16:29:00 +0200

    feat: less4 homework

---
+++ Course/Lesson4/Homework/hw1/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace hw1;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = { 1, 2, 3, 4 };
+        int[] array2 = { 5, 6, 7, 8 };
+
+        int[] array3 = new int[array1.Length + array2.Length];
+
+        Array.Copy(array1, array3, array1.Length);
+        Array.Copy(array2, 0, array3, array1.Length, array2.Length);
+
+        Console.WriteLine(String.Join(" ",array3));
+    }
+}
---
+++ Course/Lesson4/Homework/hw2/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace hw2;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        int[] array = {1, 2, 3, 4, 5};
+        int k = 3;
+
+        Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+
+        int[] array2 = new int[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {   
+            if (i + k >= array.Length)
+            {
+                array2[(i + k) - array.Length] = array[i];
+            } else
+            {
+                array2[i + k] = array[i];
+            }
+        }
+
+        Console.WriteLine("Результат ротации: " + string.Join(", ", array2)); 
+    
+    }
+}

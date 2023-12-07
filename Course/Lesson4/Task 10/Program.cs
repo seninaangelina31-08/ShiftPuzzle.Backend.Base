cbfc3cd9b39c394c2f700eb444f6c5b71c6f572f
Maxon <maxn2703@gmail.com>
2023-12-07 18:19:39 +0300

    dev: lesson 4 Tasks 1-10

---
+++ Course/Lesson4/Task 10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] mass = {1,-1,3,5,-2,-9};
+        foreach(int i in mass)
+        {
+            if (mass[i] < 0) 
+            {mass[i] = 0;}
+        Console.WriteLine(i);}
+        
+    }
+}

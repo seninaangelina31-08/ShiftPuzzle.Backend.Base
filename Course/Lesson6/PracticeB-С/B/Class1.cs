c66c68af3b1d6fb5b5cb9a382529da8cb7388954
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-12-07 20:19:36 +0200

    feat: less6 pract B-C done

---
+++ Course/Lesson6/PracticeB-С/B/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace B;
+
+public class Class1
+{
+    static int[] MaxSubarraySum(int[] nums) {
+        int maxSum = int.MinValue;
+        int currentSum = 0;
+        int start = 0;
+        int end = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                end = i;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+                start = i + 1;
+            }
+        }
+        
+        int[] subarray = new int[end - start + 1];
+        Array.Copy(nums, start, subarray, 0, subarray.Length);
+        
+        return subarray;
+    }
+    
+    static void Main(string[] args) {
+        int[] nums = { 2, -3, 4, -1, -2, 1, 5, -3};
+        int[] result = MaxSubarraySum(nums);
+        
+        Console.Write(string.Join(" ", result));
+    }
+}
---
+++ Course/Lesson6/PracticeB-С/C/Class1.cs
@@ -0,0 +1,45 @@
+﻿namespace C;
+
+public class Class1
+{
+    public static List<List<int>> GetPermutations(int[] nums)
+    {
+        List<List<int>> result = new List<List<int>>();
+        GeneratePermutations(nums, new List<int>(), new HashSet<int>(), result);
+        return result;
+    }
+
+    private static void GeneratePermutations(int[] nums, List<int> currentPerm, HashSet<int> usedNums, List<List<int>> result)
+    {
+        if (currentPerm.Count == nums.Length)
+        {
+            result.Add(new List<int>(currentPerm));
+            return;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!usedNums.Contains(nums[i]))
+            {
+                currentPerm.Add(nums[i]);
+                usedNums.Add(nums[i]);
+
+                GeneratePermutations(nums, currentPerm, usedNums, result);
+
+                currentPerm.RemoveAt(currentPerm.Count - 1);
+                usedNums.Remove(nums[i]);
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        int[] nums = { 1, 2, 3 };
+        List<List<int>> permutations = GetPermutations(nums);
+
+        foreach (List<int> permutation in permutations)
+        {
+            Console.WriteLine(string.Join(" ", permutation));
+        }
+    }
+}

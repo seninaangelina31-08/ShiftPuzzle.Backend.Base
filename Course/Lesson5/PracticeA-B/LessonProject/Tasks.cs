009983c26c7b83efeb6ec3721d65b7a4bcc56fc2
DeveloperInPants <fockstrot057@mail.ru>
2023-12-07 19:05:44 +0300

    feat: completed practice A

--- Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
+++ Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
@@ -10,6 +10,31 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            int[] numbers = { 3, 0, 2, 6, 8, 0, -3, 7, 3 };
+            int N = 3;
+            int maxSum = int.MinValue;
+            int start_Index = 0;
+
+            for (int i = 0; i <= numbers.Length - N; i++)
+            {
+                int tec_Sum = 0;
+
+                for (int j = i; j < i + N; j++)
+                {
+                    tec_Sum += numbers[j];
+                }
+
+                if (tec_Sum > maxSum)
+                {
+                    maxSum = tec_Sum;
+                    start_Index = i;
+                }
+            }
+            Console.WriteLine("Подмассив с максимальной суммой:");
+            for (int i = start_Index; i < start_Index + N; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
 
             //не выходя за пределы
         }
@@ -23,7 +48,45 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            Random random = new Random();
+            int size = 10;
+            
+            int[] numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next(100);
+            }
+            
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
 
+            int uniqueCount = 1;
+            for (int i = 1; i < size; i++)
+            {
+                if (numbers[i] != numbers[i - 1])
+                {
+                    uniqueCount++;
+                }
+            }
+
+            Console.WriteLine("Отсортированный массив:");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Количество уникальных чисел: " + uniqueCount);
             //не выходя за пределы
         }
     }
@@ -36,6 +99,39 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            int[] numbers = {1, 2, 3, 4, 5, 5, 6,7, 7, 7, 7, 8, 9, 9, 9, 9};
+            int maxOccurrences = 0;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                int count = 1;
+                for (int j = i + 1; j < numbers.Length; j++) 
+                {
+                    if (numbers[i] == numbers[j]) 
+                    {
+                    count++;
+                    }
+                }
+                if (count > maxOccurrences)
+                {
+                    maxOccurrences = count;
+                }
+            }
+            Console.WriteLine("Числа, которые встречаются чаще всего:");
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                int count = 1;
+                for (int j = i + 1; j < numbers.Length; j++) 
+                {
+                    if (numbers[i] == numbers[j]) 
+                    {
+                        count++;
+                    }
+                }
+                if (count == maxOccurrences) 
+                {
+                    Console.WriteLine(numbers[i] + " встречается " + count + " раз(а)");
+                }
+            }
 
             //не выходя за пределы
         }
@@ -49,7 +145,41 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            static void Rotate(string[] args)
+            {
+                int[] array = { 1, 2, 3, 4, 5 };
+                int k = 2;
+                Console.WriteLine("Исходный массив:");
+                PrintArray(array);
+                RotateArray(array, k);
+                Console.WriteLine("Измененный массив:");
+                PrintArray(array);
+            }
 
+            static void RotateArray(int[] array, int k)
+            {
+                int length = array.Length;
+                int[] temp = new int[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    temp[(i + k) % length] = array[i];
+                }
+
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = temp[i];
+                }
+            }
+
+            static void PrintArray(int[] array)
+            {
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+            }
             //не выходя за пределы
         
         }
@@ -63,8 +193,42 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 4, 5, 6, 7, 8 };
+            int[] resultArray = new int[array1.Length + array2.Length];
+            int index = 0;
 
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        bool alreadyExists = false;
+                        for (int k = 0; k < index; k++)
+                        {
+                            if (resultArray[k] == array1[i])
+                            {
+                            alreadyExists = true;
+                            break;
+                            }
+                        }
+                        if (!alreadyExists)
+                        {
+                            resultArray[index] = array1[i];
+                            index++;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(resultArray[i]);
+            }
             //не выходя за пределы
         }
     }
+
 }
---
+++ Course/Lesson6/PracticeA/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace PracticeA;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        static int Sum(int a, int b)
+        {
+            return a+b;
+        }
+
+        string hello = "Hello, user!!!";
+
+        static void Hello_user(string hello)
+        {
+            Console.WriteLine(Hello_user);
+        }
+
+        static int Maxn(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+
+        int c = 8;
+        static bool Num_chet(int c)
+        {
+            if (c % 2 == 2)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        double cels = 9;
+        static double C_to_f(double cels)
+        {
+            return (cels * 9 / 5) + 32;
+        }
+
+        string str = "Hello";
+        static string Reverse(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        
+        char symbol = 'l';
+        static int Num_string(string str, char symbol)
+        {
+            byte count = 0;
+            foreach(char i in str)
+            {
+                if (i == symbol)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+        int num = 5;
+        static int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else{
+                int result = 1;
+                for (int i = 1; i<= num; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+        }
+
+        static bool Prime_num(int num)
+        {
+            if(num <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        int num2 = 13;
+        static int Random_num(int num,int num2)
+        {
+            Random random = new Random();
+            int random_num = random.Next(num,num2);
+            return random_num;
+        }
+
+    }
+}

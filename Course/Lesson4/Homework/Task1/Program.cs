727e38cac53a9ce4a957e216cf3aaaab89844858
Satlykovs <satlykovs@gmail.com>
2023-12-07 23:28:38 +0300

    feat: Homework finished Lesson4

---
+++ Course/Lesson4/Homework/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array_1 = {1, 2, 3, 4, 5, 6};
+        int[] array_2 = {4, 5, 6, 7, 8, 9};
+
+        int[] merged_array = new int[array_1.Length + array_2.Length];
+
+        int n = 0;
+        Console.WriteLine("Исходный массив 1: ");
+        foreach (int el in array_1)
+        {
+            merged_array[n] = el;
+            n++;
+            Console.Write(el + " ");
+        }
+        Console.WriteLine("\nИсходный массив 2: ");
+        foreach (int el in array_2)
+        {
+            merged_array[n] = el;
+            n++;
+            Console.Write(el + " ");            
+        }  
+
+        Console.WriteLine("\nСлитый массив: ");
+        foreach (int el in merged_array)
+        {
+            Console.Write(el + " ");            
+        }
+    }
+}
---
+++ Course/Lesson4/Homework/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {1, 2, 3, 4, 5};
+        int k = 2;
+        int[] rotated_array = new int[array.Length];
+
+        Console.WriteLine("Исходный массив: ");
+        
+        foreach (int el in array)
+        {
+            Console.Write(el + " ");
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            rotated_array[(i + k) % array.Length] = array[i];
+        }
+
+        Console.WriteLine("\nРезультат: ");
+        foreach (int el in rotated_array)
+        {
+            Console.Write(el + " ");
+        }
+    }
+
+}

2e3f19019c5d67aede46e67243707483722055e6
John Doe <johndoe@example.com>
2023-12-08 16:57:32 +0300

    feat: дз lesson4

---
+++ Course/Lesson4/Homework/homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿ namespace homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = { 4, 5, 6 };
+            
+            int[] mergedArray = new int[arr1.Length + arr2.Length];
+            
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                mergedArray[i] = arr1[i];
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                mergedArray[i + arr1.Length] = arr2[i];
+            }
+            Console.WriteLine("Объединенный массив:");
+            foreach (int num in mergedArray)
+            {
+                Console.Write(num + " ");
+            }
+    } 
+}
---
+++ Course/Lesson4/Homework/homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿ namespace homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int rotations = 2;
+
+        RotateArray(arr, rotations);
+
+        Console.WriteLine("Rotated array:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+
+    static void RotateArray(int[] arr, int rotations)
+    {
+        int length = arr.Length;
+
+        int[] temp = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            temp[(i + rotations) % length] = arr[i];
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = temp[i];
+        }
+    }
+}

4b6f9d1823ea8b184c2f2331694abc1dd5822446
venimu <ven1mu@yandex.ru>
2023-12-08 14:49:53 +0300

    new file:   Course/Lesson4/Homework/Homework.sln 	new file:   Course/Lesson4/Homework/Task1/Program.cs 	new file:   Course/Lesson4/Homework/Task1/Task1.csproj 	new file:   Course/Lesson4/Homework/Task2/Program.cs 	new file:   Course/Lesson4/Homework/Task2/Task2.csproj 	new file:   Course/Lesson5/PracticeA-B/PracticeA-B.sln

---
+++ Course/Lesson4/Homework/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = { 3, 4, 5, 6, 7 };
+            bool flag = true;
+            int[] mergedArr = new int[arr1.Length+arr2.Length];
+            for (int i = 0; i < arr1.Length; i++) {
+                flag = true;
+                for (int j = 0; j < mergedArr.Length; j++) {
+                    mergedArr[i] = arr1[i];
+                } 
+            }
+            for (int i = 0; i < arr2.Length; i++) {
+                flag = true;
+                for (int j = 0; j < mergedArr.Length; j++) {
+                    mergedArr[i+arr1.Length] = arr2[i];
+                }
+            }
+            
+    }
+}
---
+++ Course/Lesson4/Homework/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2;
+class Program
+{
+    int[]arr = {1, 2, 3, 4, 5};
+    int n = arr.Length;
+    int k = Int32.Parse (Console.ReadLine());
+    for (int i = 0; i < n; i++) {
+        Console.Write(arr[i]);
+        Console.Write(' ');
+    }
+    Console.Write('\n');
+    int[] temp = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+    for (int i = 0; i < n; i++) {
+        Console.Write(arr[i]);
+        Console.Write(' ');
+    }
+}
+

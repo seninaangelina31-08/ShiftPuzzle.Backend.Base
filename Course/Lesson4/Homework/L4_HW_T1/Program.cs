44faf6e909f4a69fd7ecf392a56c0d27b04a7359
MisterCat100 <mistercat0618@gmail.com>
2023-12-08 13:39:23 +0300

    feat(Homework): completed tasks from HW_L4

---
+++ Course/Lesson4/Homework/L4_HW_T1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace L4_HW_T1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random generator = new();
+        
+        Console.WriteLine("Введите размер 1 массива");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите размер 2 массива");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        int[] nums1 = new int[n1];
+        int[] nums2 = new int[n2];
+
+        // создание массива случайно
+        for (int i = 0; i < n1; i++)
+        {
+            nums1[i] = generator.Next(20);
+        }
+
+        for (int i = 0; i < n2; i++)
+        {
+            nums2[i] = generator.Next(20);
+        }
+        // Объединение массиовов
+        int[] mergedArray = new int[nums1.Length + nums2.Length];
+        
+        for (int i = 0; i < n1; i++)
+        {
+            mergedArray[i] = nums1[i];
+        }
+
+        for (int i = 0; i < n2; i++)
+        {   
+
+            mergedArray[i + nums1.Length] = nums2[i];
+        }
+        // вывод
+        string numsStr = "{";
+        foreach (int num in mergedArray)
+        {
+            numsStr += $"{num}, ";
+        }
+        Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+        
+
+    }
+}
---
+++ Course/Lesson4/Homework/L4_HW_T2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace L4_HW_T2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random generator = new();
+
+        Console.WriteLine("Введите размер массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение, на которое сдвинуть массив");
+        int k = Convert.ToInt32(Console.ReadLine());
+        int[] nums = new int[n];
+        int[] movedNums = new int[n];
+
+        // создание массива случайно
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = generator.Next(20);
+        }
+
+        // логика
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int index = i - k;
+            if (index < 0) index += nums.Length;
+            movedNums[i] = nums[index];
+        }
+        // вывод
+        string numsStr = "{";
+        foreach (int num in nums)
+        {
+            numsStr += $"{num}, ";
+        }
+        Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+        numsStr = "{";
+        foreach (int num in movedNums)
+        {
+            numsStr += $"{num}, ";
+        }
+        Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+    }
+}

335c4eebe541f905db4abf6cc3ff3ec34e51ad2c
Frain <89194392174pp@gmail.com>
2023-12-08 16:55:46 +0300

    feature(lesson4): дз выполнено

---
+++ Course/Lesson4/Homework/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] mas_1 = {1, 2, 3, 4, 5};
+        int[] mas_2 = {1, 2, 3, 4, 5};
+
+        int[] ans = new int[mas_1.Length + mas_2.Length];
+
+        for (int i = 0; i < mas_1.Length; i++) {
+            ans[i] = mas_1[i];
+            ans[i + mas_2.Length] = mas_2[i];
+        }
+        Console.Write("Итоговый массив: ");
+        foreach (int ar in ans) {
+            Console.Write($"{ar} ");
+        }
+    }
+}
---
+++ Course/Lesson4/Homework/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] mas = {1, 2, 3, 4, 5};
+
+        int[] new_mas = new int[mas.Length];
+
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < mas.Length; i++) {
+            if (i < mas.Length - k){
+                new_mas[i+k] = mas[i];
+            }
+            else {
+            new_mas[i + k - mas.Length] = mas[i];
+            }
+        }
+
+        foreach (int a in mas) {
+            Console.Write($"{a} ");
+        }
+        Console.Write("\n");
+        foreach (int a in new_mas) {
+            Console.Write($"{a} ");
+        }
+    }
+}

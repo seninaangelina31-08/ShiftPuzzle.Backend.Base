8fe387c315d5c9662cb1bd3985b7a26d1c42fb1c
DABer1805 <DABer1805@yandex.ru>
2023-12-08 16:57:21 +0300

    feat: add Homework solution to Lesson4 folder

---
+++ Course/Lesson4/Homework/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers_1 = {1, 2, 3, 4, 5};
+            int[] numbers_2 = {6, 7, 8, 9, 10};
+            int[] result = new int[numbers_1.Length + numbers_2.Length];
+
+            numbers_1.CopyTo(result, 0);
+            numbers_2.CopyTo(result, numbers_1.Length);
+
+            Console.Write("Итоговый массив: ");
+            foreach(int number in result) {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
---
+++ Course/Lesson4/Homework/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5};
+            int[] rotated_numbers = new int[5];
+
+            Console.Write("Введите на сколько сдвигать массив: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i=0; i < numbers.Length; i++)
+            {
+                rotated_numbers[(i + k) % numbers.Length] = numbers[i];
+            }
+            
+            Console.Write("Исходный массив: ");
+            foreach (int number in numbers) {
+                Console.Write($"{number} ");
+            };
+            Console.WriteLine();
+            
+            Console.Write($"Массив со сдвигом {k} вправо: ");
+            foreach (int number in rotated_numbers) {
+                Console.Write($"{number} ");
+            };
+            Console.WriteLine();
+        }
+    }
+}

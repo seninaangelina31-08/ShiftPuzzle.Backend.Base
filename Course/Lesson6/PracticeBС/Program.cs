6965246f1c9235f38138f307f1fded345a1b3f53
John Doe <johndoe@example.com>
2023-12-09 00:49:01 +0300

    feat: B

---
+++ Course/Lesson6/PracticeBС/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace PracticeBC;
+
+class Program
+{
+    public static int[] MaxSum(int[] nums)
+    {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        int start = 0;
+        int end = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (currentSum < 0)
+            {
+                currentSum = nums[i];
+                start = i;
+                end = i;
+            }
+            else
+            {
+                currentSum += nums[i];
+                end = i;
+            }
+
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+
+        int[] subarray = new int[end - start + 1];
+
+        for (int i = start; i <= end; i++)
+        {
+            subarray[i - start] = nums[i];
+        }
+
+        return subarray;
+    }
+
+static void Main(string[] args)
+
+    {
+        int[] nums = { 1, 2, 4, -3, -5, 5, 2, 6};
+        int[] subarr = MaxSum(nums);
+
+        Console.WriteLine(string.Join(", ", subarr)); 
+    }
+}
+
+
+

e805470ef2d5ed251a45f4f955aa38e7e8fa7246
“Stranik19” <220v.adidas@gmail.com>
2023-12-09 20:42:14 +0300

    feat:(dz1)

---
+++ Course/Lesson2/zadacha1dz/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double result = num1 + num2;
+
+Console.WriteLine("Результат: " + result);

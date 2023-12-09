306a5e6f4a53204c98ff17407300c741ce2ca383
Maxon <maxn2703@gmail.com>
2023-12-10 00:20:01 +0300

    dev: Lesson 6 Practice A Tsk 1-8

---
+++ Course/Lesson6/PracticeA/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+namespace PracticeA;
+
+class Program
+{
+
+//Task 1
+static int Sum( int a, int b)
+{
+int result = a + b;
+return result;
+}
+//Task 2
+static void Greeting(string name)
+{
+Console.WriteLine($"Здорово {name} !");
+}
+//Task 3
+
+static int Max_number(int a, int b)
+{
+    if (a > b)
+    {
+    return a;   
+    }
+    else
+    {
+    return b;
+    }
+    
+}
+//Task 4
+static int Num(int a)
+{
+// string t = "true";
+// string f = "false";
+    if ((a % 2) == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+//Task 5
+static int Constructor(int a)
+{
+     int result = (a * 9 / 5) + 32;
+    return result;
+}
+//Task 6
+static string Perevorot(string a)
+{
+    char[] arr = a.ToCharArray();
+    Array.Reverse(arr);
+    return new String(arr);
+}
+
+//Task 7
+static int (string word)
+{
+char a = 'a'
+int var = 0;
+for (int i = 0; i < word.Length; i++)
+char c = word[i]
+if (char == a)
+{
+    var++
+}
+}
+
+
+
+//Task 8
+static int Easy_num(int n)
+{
+    if (n % 2 != 0 )
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
+
+    static void Main(string[] args)
+    {
+    int result = Sum(1,3);
+    Console.WriteLine(result);
+
+    string greet = "maxim";
+    Greeting(greet);
+
+    int max_num = Max_number(1,100);
+    Console.WriteLine(max_num);
+    
+    int num = Num(101);
+    Console.WriteLine(num);
+    
+    int conv = Constructor(10);
+    Console.WriteLine("По Фарангейту это: " + conv);
+
+    string rev = Perevorot("pazl");
+    Console.WriteLine(rev);
+
+    string word = Simbol("banana");
+    Console.WriteLine(word);
+
+    int esnum = Easy_num(101);
+    Console.WriteLine(esnum);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

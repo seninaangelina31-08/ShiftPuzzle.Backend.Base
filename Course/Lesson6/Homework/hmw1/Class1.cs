6ea0c96c9cd3d223772dfd55f2e0966848fdd2fd
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-12-09 17:53:19 +0200

    feat: less 6 homework done

---
+++ Course/Lesson6/Homework/hmw1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace hmw1;
+
+public class Class1
+{
+    static void Main(string[] args) {
+        int[] a = {1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+        var most = a.GroupBy(x => x).OrderByDescending(x => x.Count()).First();
+        Console.WriteLine("Наиболее часто встречается {0} в количестве {1}", most.Key, most.Count());
+    }
+}
---
+++ Course/Lesson6/Homework/hmw2/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace hmw2;
+
+public class Class1
+{
+    static void Main()
+    {
+        int[,] matrix = { { 1, 2, 3 },
+                          { 4, 5, 6 },
+                          { 7, 8, 9 } };
+
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        int[,] transposedMatrix = new int[columns, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        Console.WriteLine("Транспонированная матрица:");
+        PrintMatrix(transposedMatrix);
+    }
+    
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+

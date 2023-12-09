c91c7ddc9a5256676c65344b2519b0b6891a249e
Maxon <maxn2703@gmail.com>
2023-12-09 17:08:15 +0300

    dev: Lesson 4 Homework 1-2 fix: Lesson 5 Task 4

--- Course/Lesson5/Task 4/Program.cs
+++ Course/Lesson5/Task 4/Program.cs
@@ -4,51 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-int[] arr = {1, 2, 3, 4, 5,};
-int k = 2;
-int time_per = 0;
-int[] tempArray = new int[arr.Length];
 
 
-        foreach (var i in arr)
+         int[] arr = { 1, 2, 3, 4, 5 };
+            int k = 3;
+            int tmp;
+            int per = 0;
+ 
+            Console.WriteLine(string.Join(" ", arr) + "==> ");
+ 
+            for (int i = 0; i < arr.Length-1; ++i)
             {
-                Console.Write(i + " " );
-                
+                per += k;
+                per %= arr.Length;
+ 
+                tmp = arr[per];
+                arr[per] = arr[0];
+                arr[0] = tmp;
             }
-
-        for (int i = 1; i < arr.Length - 1; i++)
-        {
-                time_per += k;
-                time_per %= arr.Length;
-                tempArray[i] = arr[time_per];
-        }
-    Console.WriteLine("==> " + string.Join(" ", tempArray));
-
-            //Второй вариант решения
-
-            // for (int i = 1; i < array.Length; i++)
-            // {
-            //     tempArray[(i + k) % array.Length] = array[i];
-            //     Console.WriteLine(i + " ");
-            // }
-
-            // for (int i = 0; i < array.Length; i++)
-            // {
-            //     array[i] = tempArray[i];
-            // }
-            // Console.Write("");
-            // foreach (var i in array)
-            // {
-            //     Console.Write(i + " ");
-            // }
-
-
-
-
-
-
-
-
+ 
+            Console.WriteLine(string.Join(" ", arr))
 
 
     }

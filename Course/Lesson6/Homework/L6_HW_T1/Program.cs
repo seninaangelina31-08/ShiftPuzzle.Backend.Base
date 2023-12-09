4eede98027c1761b9f861c8e91044ecf1dd679d1
MisterCat100 <mistercat0618@gmail.com>
2023-12-10 00:31:39 +0300

    feat(Homework): completed tasks from HW from Lesson 6

---
+++ Course/Lesson6/Homework/L6_HW_T1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace L6_HW_T1;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Введите размер массива");
+        int size = Convert.ToInt16(Console.ReadLine());
+        
+        Random generator = new();
+        int[] nums = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            nums[i] = generator.Next(-10, 10);
+        }
+
+        // вывод
+        string numsStr = "{";
+        foreach (int num in nums)
+        {
+            numsStr += $"{num}, ";
+        }
+        Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+        var most = (from i in nums
+            group i by i into grp
+            orderby grp.Count() descending
+            select grp.Key).First();
+
+        Console.WriteLine($"Чаще всего встречалось {most}");
+    }
+}
---
+++ Course/Lesson6/Homework/L6_HW_T2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace L6_HW_T2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random generator = new();
+
+        Console.WriteLine("Введите размер матрицы");
+        int matrixSize = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Введите размер элементов матрицы");
+        int arrayInMatrixSize = Convert.ToInt16(Console.ReadLine());
+
+        int[,] matrix = new int[matrixSize, arrayInMatrixSize];
+        int[,] result = new int[arrayInMatrixSize, matrixSize];
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < arrayInMatrixSize; j++)
+            {
+                matrix[i, j] = i * j;
+            }
+        }
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < arrayInMatrixSize; j++)
+            {   
+                result[j, i] = matrix[i, j];
+            }
+        }
+
+
+        // вывод
+        for (int i = 0; i < matrixSize; i++)
+        {
+            string numsStr = "{";
+            for (int j = 0; j < arrayInMatrixSize; j++)
+            {
+                numsStr += $"{matrix[i, j]}, ";
+            }
+            Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < arrayInMatrixSize; i++)
+        {
+            string numsStr = "{";
+            for (int j = 0; j < matrixSize; j++)
+            {
+                numsStr += $"{result[i, j]}, ";
+            }
+            Console.WriteLine(numsStr.Substring(0, numsStr.Length - 2) + "}");
+        }
+    }
+}

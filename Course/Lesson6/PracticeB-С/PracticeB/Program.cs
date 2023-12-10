7561d5153d125e5e11d03ea406e08ab48c4f0b8b
Satlykovs <satlykovs@gmail.com>
2023-12-11 00:31:19 +0300

    feat: PracticeB-C finished Lesson6

---
+++ Course/Lesson6/PracticeB-С/PracticeB/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace PracticeB_С;
+
+class Program
+{
+
+    public static int[] F(int[] x)
+    {
+        int[] array = x;
+        int maxsum = int.MinValue;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int gr = -1;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+            
+            if (sum > maxsum)
+            {
+                maxsum = sum;
+                end = i;
+                start = gr + 1;
+            }
+
+            if (sum < 0)
+            {
+                sum = 0;
+                gr = i;
+            }
+
+        }   
+        
+        int[] new_arrray = new int[end - start + 1];
+        int c = 0;
+        for (int i = start; i <= end; i++)
+        {
+            new_arrray[c] = array[i];
+            c++;
+        }
+        return new_arrray;
+
+
+        
+    }
+    static void Main(string[] args)
+    {
+        int[] a = {1, 2, 3, -100, 3, 5};
+        int[] b = F(a);
+        foreach (int el in b)
+        {
+            Console.Write(el + " ");
+        }
+        
+    }
+}
---
+++ Course/Lesson6/PracticeB-С/PracticeC/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PracticeC;
+
+class Program
+{
+     public static void FindPermutations(int[] nums)
+    {
+        int[] permutation = new int[nums.Length];
+        bool[] used = new bool[nums.Length];
+
+        Permute(nums, permutation, used, 0);
+    }
+
+    public static void Permute(int[] nums, int[] permutation, bool[] used, int rec_depth)
+    {
+        if (rec_depth == nums.Length)
+        {
+            PrintPermutation(permutation);
+            return;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (used[i] == false)
+            {
+                permutation[rec_depth] = nums[i];
+                used[i] = true;
+                Permute(nums, permutation, used, rec_depth + 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    public static void PrintPermutation(int[] permutation)
+    {
+        foreach (int num in permutation)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = {1, 2, 3};
+        FindPermutations(nums);
+    }
+}

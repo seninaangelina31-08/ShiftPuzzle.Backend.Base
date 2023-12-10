c5fad1f3a718c1ea33cc7e5e635a6c0ba8313acb
Satlykovs <satlykovs@gmail.com>
2023-12-11 01:38:23 +0300

    feat: Homework finished Lesson6 Tasks 1-2

---
+++ Course/Lesson6/Homework/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task1;
+
+class Program
+{
+    public static int Count(int[] array)
+    {
+        int max_el = array[0];
+        int max_count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] == array[i])
+                {
+                    count++;
+                }
+            }
+            if (max_count < count)
+            {
+                max_count = count;
+                max_el = array[i];
+            }
+        }
+        
+        return max_el;
+    }
+    static void Main(string[] args)
+    {  
+        int[] b = {1, 2, 3, 1, 3, 3, 3, 2, 1, 2, 2};
+        int a = Count(b);
+        Console.Write(a); 
+    }
+}
---
+++ Course/Lesson6/Homework/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task2;
+
+class Program
+{
+        public static int[,] Transpose(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+    
+        int[,] transposed_matrix = new int[columns, rows];
+    
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                transposed_matrix[j, i] = matrix[i, j];
+            }
+        }   
+        return transposed_matrix;         
+    }
+    
+    public static void MatrixPrint(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+        int[,] matrix = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+        
+        int[,] transposed_matrix = Transpose(matrix);
+
+        Console.WriteLine("Исходная матрица: ");
+        MatrixPrint(matrix);
+        Console.WriteLine("\nТранспонированная матрица: ");
+        MatrixPrint(transposed_matrix);
+    
+    }
+}

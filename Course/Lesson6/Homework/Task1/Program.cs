19a4f515dffa317cadefde47b1ec70ca17fb725d
venimu <ven1mu@yandex.ru>
2023-12-11 14:43:00 +0300

    Homework 	new file:   Course/Lesson6/Homework/Homework.sln 	new file:   Course/Lesson6/Homework/Task1/Program.cs 	new file:   Course/Lesson6/Homework/Task1/Task1.csproj 	new file:   Course/Lesson6/Homework/Task2/Program.cs 	new file:   Course/Lesson6/Homework/Task2/Task2.csproj

---
+++ Course/Lesson6/Homework/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main() {
+        int[] numbers = { 1, 2, 3, 4, 5, 2, 3, 2, 2, 6, 7, 7, 7, 7 };
+        int mostFrequent = FindMostFrequentElement(numbers);
+        Console.WriteLine("The Most Frequent " + mostFrequent);
+    }
+
+    static int FindMostFrequentElement(int[] array) {
+        Dictionary<int, int> frequencies = new Dictionary<int, int>();
+
+        foreach (int num in array) {
+            if (frequencies.ContainsKey(num)) {
+                frequencies[num]++;
+            }
+            else {
+                frequencies[num] = 1;
+            }
+        }
+
+        int mostFrequent = frequencies.OrderByDescending(x => x.Value).First().Key;
+        return mostFrequent;
+    }
+}
+
---
+++ Course/Lesson6/Homework/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task2;
+using System;
+
+class Program {
+
+    static void PrintMatrix(int[,] matrix, int rows, int columns) {
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    static void Main() {
+        int rows = 3;    
+        int columns = 3; 
+
+        int[,] matrix = new int[3, 3] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix, rows, columns);
+
+        int[,] transposedMatrix = TransposeMatrix(matrix, rows, columns);
+
+        Console.WriteLine("Транспонированная матрица:");
+        PrintMatrix(transposedMatrix, rows, columns);
+    }
+
+    static int[,] TransposeMatrix(int[,] matrix, int rows, int columns) {
+
+
+        int[,] transposedMatrix = new int[columns, rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
+

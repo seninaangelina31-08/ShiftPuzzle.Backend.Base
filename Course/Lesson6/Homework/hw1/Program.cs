be81d1da73c1f2a454c28ae7483fafccb184ae9c
Ivanova Daria <ida30062006@gmail.com>
2023-12-11 15:40:16 +0300

    feature: Hw1-2

---
+++ Course/Lesson6/Homework/hw1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[]mass = new int[]{ 12, 4, 4, 0, 9, 4, 3, 5, 5, 3, 4 };
+            Array.Sort(mass);
+            Dictionary<int,int> dict=new Dictionary<int,int>();
+            int c=1;
+            for(int i = 0; i < mass.Length-1; i++)
+            {
+                if (mass[i]==mass[i+1]){
+                    c+=1;
+                }else{
+                    dict.Add(mass[i], c);
+                }
+            }
+            int max=0;
+            foreach (var item in dict){
+                if (item.Value>max){
+                    max=item.Value;
+                }
+            }
+            foreach (var item in dict){
+                if (item.Value==max){
+                    Console.WriteLine("Count:"+item.Value+" Number:"+item.Key);
+                    break;
+                }
+            }
+    }
+}
---
+++ Course/Lesson6/Homework/hw2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+            Console.Write("N: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("M: ");
+            int M = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nИсходная матрица: \n");
+            int[,] A = new int[N, M];
+            Random rnd = new Random();
+ 
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    A[i, j] = rnd.Next(-20, 20);
+                    Console.Write(A[i, j] + " \t ");
+                }
+                Console.WriteLine();
+            }
+            int[,] AT = new int[M, N];
+            Console.WriteLine("\nТранспонированная матрица: ");
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    AT[i, j] = A[j, i];
+                    Console.Write(AT[i, j] + " \t ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+    }
+}

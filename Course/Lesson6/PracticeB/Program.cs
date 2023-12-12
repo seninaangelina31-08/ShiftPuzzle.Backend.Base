15ee95697bc8312ea706b6eec80d12a8bef3f091
DeveloperInPants <fockstrot057@mail.ru>
2023-12-12 16:37:46 +0300

    feat: confirm Practice b-c in lesson6

---
+++ Course/Lesson6/PracticeB/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PracticeB;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { -9, 1, 3, 4, 5, 2, 1};
+        int[] subarray = FindMax(nums);
+
+        Console.WriteLine("Maximum Subarray:");
+        foreach (int num in subarray)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+     public static int[] FindMax(int[] nums)
+    {
+        int maxSum = int.MinValue;
+        int currentSum = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            currentSum += nums[i];
+
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+                endIndex = i;
+            }
+
+            if (currentSum <= 0)
+            {
+                currentSum = 0;
+                startIndex = i + 1;
+            }
+        }
+
+        int[] subarray = new int[endIndex - startIndex + 1];
+        Array.Copy(nums, startIndex, subarray, 0, endIndex - startIndex + 1);
+
+        return subarray;
+    }
+}
---
+++ Course/Lesson6/PracticeC/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System;
+namespace PracticeC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3 };
+        List<List<int>> permutations = GeneratePermutations(nums);
+        
+        foreach (List<int> permutation in permutations)
+        {
+            Console.WriteLine(string.Join(" ", permutation));
+        }
+    }
+    public static List<List<int>> GeneratePermutations(int[] nums)
+    {
+        List<List<int>> permutations = new List<List<int>>();
+        GeneratePermutations(nums, new bool[nums.Length], new List<int>(), permutations);
+        return permutations;
+    }
+    
+    private static void GeneratePermutations(int[] nums, bool[] used, List<int> current, List<List<int>> permutations)
+    {
+        if (current.Count == nums.Length)
+        {
+            permutations.Add(new List<int>(current));
+            return;
+        }
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!used[i])
+            {
+                current.Add(nums[i]);
+                used[i] = true;
+                GeneratePermutations(nums, used, current, permutations);
+                used[i] = false;
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+    }
+
+}

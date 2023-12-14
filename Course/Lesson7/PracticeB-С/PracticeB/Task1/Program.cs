511280acf1bbe6f3dbabfdba7f20936d833f41b7
Satlykovs <satlykovs@gmail.com>
2023-12-14 20:45:38 +0300

    feat: PracticeB finished Lesson7

---
+++ Course/Lesson7/PracticeB-С/PracticeB/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PracticeB;
+
+class Program
+{
+    public static bool FindPath(int[,] maze)
+    {
+        int rows = maze.GetLength(0);
+        int columns = maze.GetLength(1);
+        return Solve(maze, 0, 0);
+    }
+    public static bool Solve(int[,] maze, int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= maze.GetLength(0) || y >= maze.GetLength(1) || maze[x, y] == 0)
+        {
+            return false;
+        }
+        if (maze[x, y] == 3)
+        {
+            return true;
+        }
+
+        if (maze[x, y] != 0)
+        {      
+            maze[x, y] = 0;
+            if (Solve(maze, x + 1, y) || Solve(maze, x - 1, y) || Solve(maze,x, y + 1) || Solve(maze, x, y - 1))
+            {
+                return true;
+            }
+        
+        }
+        return false;
+    }
+    static void Main(string[] args)
+    {
+       int[,] maze = {
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0, 1, 1},
+    {0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+    {0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 3},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        
+        Console.Write(FindPath(maze));
+
+    }
+}
---
+++ Course/Lesson7/PracticeB-С/PracticeB/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task2;
+
+class Program
+{
+    public static void Sort(int[] array, int left, int right)
+    {
+        if (left < right)
+ {          foreach (int el in array)
+            // {
+                // Console.Write(el + " ");
+            // }
+            Console.WriteLine();
+            int index = Pivot(array, left, right);
+            Sort(array, left, index - 1);
+            Sort(array, index + 1, right);
+        }
+    }
+
+    public static int Pivot(int[] array, int left, int right)
+    {
+        int pivot = array[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++)
+        {
+            if (array[j] < pivot)
+            {
+                i++;
+                Swap(array, i, j);
+            }
+        }
+
+        Swap(array, i + 1, right);
+        return i + 1;
+    }
+
+    public static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    static void Main(string[] args)
+    {
+        int[] array = {5, 2, 9, 1, 3, 6};
+        Sort(array, 0, array.Length - 1);
+
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}

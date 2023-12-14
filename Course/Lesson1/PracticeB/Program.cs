41b719deca25c12358a89ed2e4054f320ab20953
ElenInka <anonymousinkelen@gmail.com>
2023-12-14 19:36:46 +0400

    feat: homework

---
+++ Course/Lesson1/PracticeB/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticeB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Здравствуйте! Я буду выполнять арифметические операции над числами!");
+        Console.WriteLine("Введите первое целое число:");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Введите второе целое число:");
+        int num2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Сложение: " + (num1 + num2));
+        Console.WriteLine("Вычитание: " + (num1 - num2));
+        Console.WriteLine("Умножение: " + (num1 * num2));
+        Console.WriteLine("Целое от деления: " + (num1 / num2));
+        Console.WriteLine("Возведение в степень: " + (Math.Pow(num1, num2)));
+
+    }
+}
---
+++ Course/Lesson1/PracticeC/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticeC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите любое число: ");
+        int num = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(num*5);
+
+    }
+}

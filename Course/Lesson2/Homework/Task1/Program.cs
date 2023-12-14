3313ddbf9a98eeed24585146a1557dcadd3229d2
ElenInka <anonymousinkelen@gmail.com>
2023-12-14 20:17:39 +0400

    feat: homework Lesson2

---
+++ Course/Lesson2/Homework/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Здравствуйте! Я мини-калькулятор! Умею только складывать небольшие числа!");
+        Console.WriteLine("Введите первое любое небольшое число: ");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Введите второе любое небольшое число: ");
+        int num2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine((num1+num2));
+    }
+}
---
+++ Course/Lesson2/Homework/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int my_num = 88;
+        Console.WriteLine("Привет! Давай поиграем в битву экстрасенсов! Я загадал двузначное число, попробуй его отгадать!");
+        Console.WriteLine("Итак, я загадал число... (введи число): ");
+        int your_num = Convert.ToInt16(Console.ReadLine());
+        if (your_num > my_num)
+        {
+            Console.WriteLine("Неверно! Моё число меньше твоего! Перезапусти программу и повтори попытку!");
+        }
+
+        else if (your_num < my_num)
+        {
+            Console.WriteLine("Неверно! Моё число больше твоего! Перезапусти программу и повтори попытку!");
+        }
+        else
+        {
+            Console.WriteLine("Правильно! Да ты настоящий экстрасенс!!!");
+        }
+    }
+}

6c92c866ea575a3182ebb4bef12803ea5f427907
DABer1805 <DABer1805@yandex.ru>
2023-12-15 19:13:57 +0300

    feat: add homework solution to lesson_6 folder

---
+++ Course/Lesson6/Homework/Tasks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Задание 1 ---");
+            Task1();
+            Console.WriteLine("--- Задание 2 ---");
+            Task2();
+        }
+
+        static void Task1() {
+            // Исходный массив с числами
+            int[] numbers = {4, 45, 45, 3, 3, 0, 1, 32, 100, 777, 454, 111};
+            if (numbers.Length > 0) {
+                // Группируем массив, используя как ключи сами числа, а значения - количество данных чисел в массиве
+                var grouped_numbers = numbers.GroupBy(x => x);
+                // Располагаем элементы группы в порядке убывания их количества в массиве
+                grouped_numbers = grouped_numbers.OrderByDescending(x => x.Count());
+                // Выводи самое часто встречающееся число - первый элемент нашей новой последовательности
+                Console.WriteLine($"Самое часто встречающееся число - {grouped_numbers.First().Key}");
+            } else {
+                // Чтоб ничего не поламалось
+                Console.WriteLine("Массив пустой!!!");
+            }
+        }
+
+        static void Task2() {
+                // Создадим думерный массив, который выполнит роль матрицы
+                int[,] matrix = new int[2, 3] {{0, 2, 5}, {0, 1, 3}};
+
+                // Это будет транспонированная матрица
+                var new_matrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+                
+                // Транспонируем матрицу
+                for (int j = 0; j < new_matrix.GetLength(0); ++j)
+                    for (int i = 0; i < new_matrix.GetLength(1); ++i)
+                        new_matrix[j, i] = matrix[i, j];
+
+                // Распечатаем транспонированную матрицу
+                for (int i = 0; i < new_matrix.GetLength(0); ++i) {
+                    for (int j = 0; j < new_matrix.GetLength(1); ++j) {
+                        Console.Write($"{new_matrix[i, j]} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+}
---
+++ Course/Lesson7/PracticeA/Tasks/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- 1-я задача ---");
+            Console.WriteLine(Factorial(1));
+            Console.WriteLine(Factorial(2));
+            Console.WriteLine(Factorial(3));
+
+            Console.WriteLine("--- 2-я задача ---");
+            Console.WriteLine(Fib(2));
+            Console.WriteLine(Fib(3));
+            Console.WriteLine(Fib(4));
+
+            Console.WriteLine("--- 3-я задача ---");
+            Console.WriteLine(Reverse("Строка"));
+            Console.WriteLine(Reverse("I LOVE C#"));
+            Console.WriteLine(Reverse("It's task №3"));
+
+            Console.WriteLine("--- 4-я задача ---");
+            double[] a = {1, 2, 3, 4, 5};
+            double[] b = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+            double[] c = {};
+            Console.WriteLine(Sum(a));
+            Console.WriteLine(Sum(b));
+            Console.WriteLine(Sum(c));
+
+            Console.WriteLine("--- 5-я задача ---");
+            Console.WriteLine(NOD(5, 2));
+            Console.WriteLine(NOD(6, 3));
+            Console.WriteLine(NOD(20, 15));
+
+            Console.WriteLine("--- 6-я задача ---");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        // 1-я задача
+        static double Factorial(double number) {
+            if (number <= 0) {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+
+        // 2-я задача
+        static double Fib(double number) {
+            if (number <= 1) {
+                return number;
+            }
+            return Fib(number - 1) + Fib(number - 2);
+        }
+
+        // 3-я задача
+        static string Reverse(string str) {
+            // Делаем пока строка не кончится
+            if (str.Length <= 1)
+            {
+                return str;
+            }
+            // Использкем срез, чтобы проигнорить первый элемент и лепим в конец 
+            // тот самый первый элемент, при этом в нашу функцию попадает строка 
+            // на один символ меньше
+            return Reverse(str.Substring(1)) + str[0];
+        }
+
+        // 4-я задача
+        static double Sum(double[] array) {
+            // Делаем пока длина массива не станет равняться 0
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            
+            // Массив, длинной на один меньше
+            double[] new_array = new double[array.Length - 1];
+            
+            // Заполняем массив
+            for (int i=0; i < new_array.Length; ++i) {
+                new_array[i] = array[i];
+            }
+
+            // Приплюсовываем к результату тот самый вырезанный элемент и передаем в функцию уже порезанный массив
+            return array[array.Length - 1] + Sum(new_array);
+        }
+
+        // 5-я задача
+        static int NOD (int a, int b) {
+            if (a == 0 || b == 0) {
+                return a + b;
+            }
+            if (a > b) {
+                return NOD(a - b, b);
+            }
+            return NOD(a, b - a);
+        }
+    }
+}

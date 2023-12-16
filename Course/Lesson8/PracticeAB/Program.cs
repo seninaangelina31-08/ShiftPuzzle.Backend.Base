dc763bc6a183f5184b2808ce18ce30201c316275
Maxon <maxn2703@gmail.com>
2023-12-16 19:57:33 +0300

    Lesson Practice A-B

--- Course/Lesson8/PracticeAB/Program.cs
+++ Course/Lesson8/PracticeAB/Program.cs
@@ -2,66 +2,108 @@
 
 class Program
 {
-
-    
-    // Творим тут
-
-
-    //1
-    // public static int Task1(int a, int b){
-    //     return (a + b);
-    // }
-
-    // //2
-    // static Task2()
-    // {
-
-    // }
-
-    // //3
-    // static Task3()
-    // {
-
-    // }
-
-    // //4
-    // static Task4()
-    // {
-
-    // }
-
-    // // 5
-    // static Task5()
-    // {
-
-    // }
-
-    // // 6
-    // static Task6()
-    // {
-
-    // }
-
-    // //7
-    // static Task7()
-    // {
-
-    // }
-
-    // //8
-    // static Task8()
-    // {
-
-    // }
-
+// Запуск функций
     static void Main(string[] args)
     {
-        // это функция мейн которая вызывает все отсальные функции для практики А и Б
-        //вызов первой функци.... и т.п.
-        Console.WriteLine(Task1(1, 2));
+int[] mass = {1, 2, 3, 4, 5, 6, 7, 8};
+Console.WriteLine(Sum(2,2));
+Console.WriteLine(Chet_num(3));
+Console.WriteLine(String_rev("Pumba"));
+Console.WriteLine(Max_number(mass));
+Console.WriteLine(Salary(70000));
+Console.WriteLine(Celsius_to_fahrenheit(25));
+Console.WriteLine(Count_vowels("bananananananaaa"));
+Console.WriteLine(Generate_password("102102"));
+    }
 
+//1  
+static int Sum( int a, int b)
+{
+    return a + b;
+}
+
+//2
+static bool Chet_num(int a)
+{
+    if (a % 2 == 0)
+    {
+        return true;
     }
-    public static int Task1(int a, int b){
-        return (a + b);
+    else
+    {
+        return false;
     }
+
+}
+
+//3
+static string String_rev(string a)
+{
+char[] charArray = a.ToCharArray();
+Array.Reverse(charArray);
+return new string(charArray);
+
+}
+
+//4
+static int Max_number(int[] array )
+{
+int num = array.Max();
+return num;
+}
+//5
+static int Salary(int a)
+{
+    int ans = a * 12;
+    return ans;
+}
+//6
+static int Celsius_to_fahrenheit(int a)
+{
+    return a * 9/5 + 32;
+}
+
+static int Count_vowels(string s)
+{
+    char a = 'a';
+    int ans = 0;
+    foreach( int i in s)
+    {
+    if (i == a)
+    {
+        ans++;
+    }
+    }
+    return ans;
+}
+
+static int Generate_password(string passtohack)
+{
+    int count = 0;
+    for (int x = 0; x <= 10; ++x )
+    {
+        for (int y = 0; y <= 10; ++y )
+        {
+            for ( int z = 0; z <= 10; ++z)
+            {
+                for ( int h = 0; h <= 10; ++h)
+                {
+                count++;
+                string generatedpass = x.ToString() + y.ToString() + z.ToString() + h.ToString();
+                if (generatedpass == passtohack)
+                {
+                    Console.WriteLine("Your MotherHaker!!!");
+                    return count;
+                }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
 }

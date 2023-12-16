56bbe6e7e44a764c209670adf386ebe7ff80d397
“Stranik19” <220v.adidas@gmail.com>
2023-12-16 23:19:38 +0300

    feat: (lesson4, dz)

---
+++ Course/Lesson4/Homework/z1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr1 = {1, 2, 3};
+int [] arr2 = {4, 5, 6};
+
+int[] result = new int [arr1.Length + arr2.Length];
+Arr.Copy(arr1, result, arr1.Length)
+Arr.Copy(arr2, 0, result, arr1.Length, arr2.Length);
+
+foreach (int i in result)
+{
+    Console.Write(i + " ");
+}
+
+
---
+++ Course/Lesson4/Homework/z2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr= {1, 2, 3, 4, 5};
+int rotate = 2;
+
+int[] result = new int[arr.Length];
+
+for ( i = 0; i < arr.Length; i++)
+{
+    int nowoe = (i + rotate)  % arr.Length;
+    result[nowoe] = arr[i];
+}
+
+foreach (int i in result)
+{
+    Console.Write(i + " ");
+}
+

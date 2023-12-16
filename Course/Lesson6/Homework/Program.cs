3d9a79c28bdd44cd72f9dc3578a50a8e97b1e515
Maxon <maxn2703@gmail.com>
2023-12-16 16:28:24 +0300

    fix: lesson 6 Homework tsk 2

--- Course/Lesson6/Homework/Program.cs
+++ Course/Lesson6/Homework/Program.cs
@@ -27,8 +27,49 @@ Console.WriteLine(mass[max]);
 
 
 
-
-
-
+        Console.WriteLine("Введите количество NxM элементов матрицы sample: ");
+        int N = 0;
+        int M = 0;
+        
+        string str = Console.ReadLine();
+        string stl = Console.ReadLine();
+        N = int.Parse(str);
+        M = int.Parse(stl);
+        
+ 
+        Console.WriteLine("Исходная матрица: ");
+        Console.WriteLine();
+        int[,] sample = new int[N, M];
+        Random rnd = new Random();
+ 
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                sample[i, j] = rnd.Next(1, 100); // заполнение матрицы случайными числами
+ 
+                Console.Write(sample[i, j] + " \t ");
+ 
+                if (j == M - 1)
+                    Console.WriteLine();
+            }
+        }
+        int[,] trans = new int[M, N];
+        Console.WriteLine();
+        Console.WriteLine("Транспонированная матрица: ");
+        Console.WriteLine();
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                trans[i, j] = sample[j, i];
+                Console.Write(trans[i, j] + " \t ");
+ 
+                if (j == M - 1)
+                    Console.WriteLine();
+            }
+        }
+ 
+        Console.ReadLine();
     }
 }

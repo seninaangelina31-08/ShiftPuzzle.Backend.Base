d18759e5a3009b6f29db57d0c946e4e758b706f5
“IvanSobolev” <i@bezlikiy.ru>
2023-12-18 18:55:20 +0300

    feat: improvements

---
+++ Course/Lesson6/Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static int oftenNumber(int[] nums)
+    {
+        Dictionary<int, int> RecordedNums = new Dictionary<int, int>();
+        foreach (int num in nums)
+        {
+            if (RecordedNums.ContainsKey(num))
+            {
+                RecordedNums[num]++;
+            }
+            else
+            {
+                RecordedNums.Add(num, 0);
+            }
+        }
+        int MaxVal = RecordedNums.Values.First();
+        foreach (var i in RecordedNums)
+        {
+            if (i.Value > RecordedNums[MaxVal])
+            {
+                MaxVal = i.Key;
+            }
+        }
+        return MaxVal;
+    }
+
+     static int[,] TransposeMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[,] transposedMatrix = new int[cols, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
--- Course/Lesson7/PracticeB-С/Program.cs
+++ Course/Lesson7/PracticeB-С/Program.cs
@@ -3,6 +3,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
     }
+    static void QuickSortAlgorithm(int[] array, int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex = Partition(array, low, high);
+
+        QuickSortAlgorithm(array, low, pivotIndex - 1);
+        QuickSortAlgorithm(array, pivotIndex + 1, high);
+    }
+}
+
+static int Partition(int[] array, int low, int high)
+{
+    int pivot = array[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            Swap(ref array[i], ref array[j]);
+        }
+    }
+
+    Swap(ref array[i + 1], ref array[high]);
+    return i + 1;
+}
+
+static void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
 }

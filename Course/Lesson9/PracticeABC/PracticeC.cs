6f90742427466fa08b76f14b4ea73d83e62a4293
EvaKoz <eviguzatova@mail.ru>
2023-12-18 19:13:03 +0300

    feat(Lesson9): Finished PracticeС

--- Course/Lesson9/PracticeABC/PracticeC.cs
+++ Course/Lesson9/PracticeABC/PracticeC.cs
@@ -4,3 +4,61 @@ public class PracticeC
 {
     
 }
+
+public class Person
+{
+    public string Name { get; set; }
+    private int age;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Возраст не может быть отрицательным!");
+            }
+            else
+            {
+                age = value;
+            }
+        }
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine("Привет, мое имя " + Name);
+    }
+}
+
+public class Employee : Person
+{
+    public string Position { get; set; }
+
+    public Employee(string name, int age, string position)
+        : base(name, age)
+    {
+        Position = position;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine("Привет, мое имя " + Name + ", я работаю на должности " + Position);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee("Эвелина", 17, "будущий педагог!");
+        employee.Introduce();
+    }
+}

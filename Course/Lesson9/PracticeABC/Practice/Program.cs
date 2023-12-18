3329dfdbc54abf9c1bd0a1da55c7a52ddd988052
Shadik23 <kurichjev@yandex.ru>
2023-12-18 21:11:20 +0300

    feat(lessone9): PracticeA

---
+++ Course/Lesson9/PracticeABC/Practice/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice;
+
+class Persone
+{
+    public string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.Age = age;
+    }
+
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Привет, меня зовут {name}");
+        Console.WriteLine($"Мне {Age} лет");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[]
+            {
+                new Person("Саша", 25),
+                new Person("Антон", 13),
+                new Person("Миша", 17)
+            };
+
+            foreach (var person in people)
+            {
+                person.Introduce();
+            }
+        }
+    }
+}

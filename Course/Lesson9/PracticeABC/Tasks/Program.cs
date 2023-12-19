5a21eb85b7d150c82cbb2eabe97ea8bc6efef8c6
DABer1805 <DABer1805@yandex.ru>
2023-12-19 20:31:48 +0300

    feat: add Practice B solution to Lesson9 folder

--- Course/Lesson9/PracticeABC/Tasks/Program.cs
+++ Course/Lesson9/PracticeABC/Tasks/Program.cs
@@ -6,39 +6,52 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            // №1
+            // №A1
             // Закомментил, т.к. при наличии конструктора оно ломается, оставил так, что б код не дублировать
             // Person person = new Person();
             // person.age = 16;
             // person.name = "NoName";
-            // №3
+            // №A3
             Person super_person = new Person("RandomName", 18);
-            // №2
+            // №A2
             super_person.Introduce();
-            // №4
+            // №A4
             Person[] array = {new Person("Саня", 24), new Person("Ваня", 21), new Person("Римас", 23)};
             foreach (Person elem in array) {
                 elem.Introduce();
             }
+            // B1
+            // Вылетит ошибка
+            // Person not_yet_born_person = new Person("NoName", -666);
         }
     }
 }
 
 public class Person
 {
-    // Поля класса (№1)
+    // Поля класса (№A1)
     public string name;
     public int age;
 
     public Person(string name, int age)
     {
         this.name = name;
-        this.age = age;
+        this.age = this.SetAge(age);
     }
 
-    // №2
+    // №A2
     public void Introduce()
     {
         Console.WriteLine($"Привет, мое имя - {this.name}");
+    }
+
+    // №B2
+    public int SetAge(int age)
+    {
+        if (age < 0) {
+            // Кидаем ошибку, если неверный врзраст
+            throw new ArgumentException("Возраст должен быть положительным!!!");
+        }
+        return age;
     }
 }

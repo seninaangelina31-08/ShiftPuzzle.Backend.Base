86c14d1e340d3fcf00c9f2865a72568b32c647f8
Валерий Манойлов <manojlovv@inbox.ru>
2023-12-19 18:50:26 +0300

    Practice B have been completed

--- Course/Lesson9/PracticeABC/Practices/Program.cs
+++ Course/Lesson9/PracticeABC/Practices/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Person person1 = new Person("Valery", 16);
         Person person2 = new Person("Ivan", 8);
-        Person person3 = new Person("Eugene", 26);
+        Person person3 = new Person("Eugene", -26);
         Person person4 = new Person("Branislav", 43);
 
         Person[] array_persons = {person1, person2, person3, person4};
@@ -25,9 +25,16 @@ public class Person{
     public Person(string name, int age)
     {
         this.Name = name;
-        this.Age = age;
+        this.Age = set_age(age);
     }
 
+    public int set_age(int age){
+        if (age < 0){
+            Console.WriteLine($"Uncorrect Value (age is not negative num) in {Name}! Have been set age = 0");
+            return 0;
+        }
+        return age;
+    }
     public void Introduce(){
         Console.WriteLine($"Hello, my name is {Name}");
     }

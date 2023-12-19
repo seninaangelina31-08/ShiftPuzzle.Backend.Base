7bf0ac85d01aa1955e2e2089c500bf049797fa07
lisyonok14 <olesya.nikulina.2007@mail.ru>
2023-12-19 18:48:54 +0200

    feat: less9 prA done

---
+++ Course/Lesson9/PracticeABC/ABC/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ABC;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        Person p1 = new Person("Петя", 10);
+        Person p2 = new Person("Ваня", 30);
+        Person p3 = new Person("Катя", 20);
+
+        Person[] people = { p1, p2, p3 };
+
+        foreach (Person p in people)
+        {
+            p.Introduce();
+        }
+    }
+}
+public class Person
+{
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine("Привет, моё имя " + name);
+    }
+}

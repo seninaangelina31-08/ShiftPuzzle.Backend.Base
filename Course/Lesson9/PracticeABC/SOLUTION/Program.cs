4a95d5330ff011cb6d783dda1128f051ea956cf0
Ssotnikovkirill <kirillgor1217@gmail.com>
2023-12-19 20:20:13 +0300

    Feat(Lesson9): PracticeA in SOLUTION

--- Course/Lesson9/PracticeABC/SOLUTION/Program.cs
+++ Course/Lesson9/PracticeABC/SOLUTION/Program.cs
@@ -2,8 +2,34 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Person[] people = new Person[3];
+
+        people[0] = new Person("Акшин", 23);
+        people[1] = new Person("Дима", 5);
+        people[2] = new Person("Саша", 92);
+
+        foreach (Person person in people)
+        {
+            person.Introduce();
+        }
+    }
+}
+
+public class Person
+{
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public virtual void Introduce()
+    {
+        Console.WriteLine($"Hi! My name is {Name}");
     }
 }

0d9c307e3dfe0a3e09b93fd8532cc79e372f350d
Ivanova Daria <ida30062006@gmail.com>
2023-12-20 17:05:21 +0300

    feature: hw8

--- Course/Lesson8/Homework/Program.cs
+++ Course/Lesson8/Homework/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿namespace PracticeAB;
+
+class Program
+{
+
+    public static string loan_approval(int balance){
+        if (balance <= 0){
+            return "Кредит одобрен!";
+        }
+        else{
+            return "У вас достаточно средств, кредит не нужен.";
+        }
+    }
+
+    public static int deposit_withdrawal(int deposit, int balance)
+    {
+        return balance - deposit;
+    }
+
+
+
+
+    public static void calculate_compound_interest(double sum, int pr, int year)
+    {
+        
+        
+        for (int i=1; i<=year; i++){
+            sum = sum + sum/100*pr;
+        }
+        Console.WriteLine(sum);
+    }
+
+
+
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine(loan_approval(-100));
+            Console.WriteLine(deposit_withdrawal(100, 300));
+            calculate_compound_interest(1000, 15, 2);
+        }
+}
+
+

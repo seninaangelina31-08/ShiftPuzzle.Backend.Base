a23fa19b9673911eb73d5e82947244fdbac7858b
Валерий Манойлов <manojlovv@inbox.ru>
2023-12-21 18:23:15 +0300

    Complete PracticeA in Lesson10

--- Course/Lesson10/PracticeA/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PracticeA;
-class FileOperations
-{
-    static void Main(string[] args)
-    { 
-
-        // чтение запись в файл test.txt
-    }
-}
---
+++ Course/Lesson10/PracticeA2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticeA2;
+class FileOperations
+{
+    static void Main(string[] args)
+    { 
+        // чтение запись в файл test.txt
+        WriteFunc();
+        ReadFunc();
+    }
+
+    public static void WriteFunc(){
+        string[] words = new string[6];
+        for (int i = 0; i < 6; i++){
+            words[i] = Console.ReadLine();
+        }
+        File.WriteAllLines("words.txt", words);
+    }
+    public static void ReadFunc(){
+        string[] words = File.ReadAllLines("words.txt");
+        Console.WriteLine("Words in file:");
+        foreach (string word in words){
+            Console.WriteLine(word);
+        }
+    }
+}

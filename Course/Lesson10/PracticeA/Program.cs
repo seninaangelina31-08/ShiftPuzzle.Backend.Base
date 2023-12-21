943c1c865812a8e36dafb1771ee9afd2e33fe98f
DariaN <m.dzino@ya.ru>
2023-12-21 12:51:53 +0300

    feat: create PrA Less10

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace PracticeA;
+﻿using System;
+using System.IO;
+
+namespace PracticeA;
 class FileOperations
 {
+    static string _file="Text.txt";
+    public static void WriteToFile()
+    {
+        string[] lines = new string[5];
+        for (int i = 1; i <= lines.Length; i++)
+        {
+            lines[i-1] = "test"+i.ToString();
+        }
+        File.WriteAllLines(_file, lines);
+    }
+
+    public static void ReadFromFileAndPrint()
+    {
+        string[] lines = File.ReadAllLines(_file);
+        foreach(string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
     static void Main(string[] args)
     { 
-
-        // чтение запись в файл test.txt
+        ReadFromFileAndPrint();
+        WriteToFile();
     }
 }

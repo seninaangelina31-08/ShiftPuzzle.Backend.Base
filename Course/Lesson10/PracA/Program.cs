746081c19a82f2657a9732bc7c14aa00d7b751bf
Ivanova Daria <ida30062006@gmail.com>
2023-12-21 18:36:12 +0300

    Feature: lesson10, pracA

---
+++ Course/Lesson10/PracA/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PracticeA;
+class FileOperations
+{
+
+    public static void WriteToFile()
+    {
+        string[] lines = new string[3];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i] = "Alice, 24";
+        }
+        File.WriteAllLines("lesson10.txt", lines);
+    }
+
+
+    public static void ReadFromFileAndPrint()
+    {
+        string[] lines = File.ReadAllLines("lesson10.txt");
+        foreach(string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    static void Main(string[] args)
+    { 
+
+        WriteToFile();
+        ReadFromFileAndPrint();
+    }
+}
+

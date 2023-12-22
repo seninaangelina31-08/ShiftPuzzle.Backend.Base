e0e184db1d6d737c0171822b81321018d300fd27
Laki <r.matigorov@yandex.ru>
2023-12-22 18:06:08 +0300

    feat(PracticeA): PracticeA complited

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -3,7 +3,15 @@ class FileOperations
 {
     static void Main(string[] args)
     { 
-
+        string[] test = {"Пробный", "Тест"};
+        File.WriteAllLines("Test.txt", test);
+        string[] lines = File.ReadAllLines("Test.txt");
         // чтение запись в файл test.txt
+        // test
+        foreach (string str in lines)
+        {
+            Console.WriteLine(str);
+        }
     }
+
 }

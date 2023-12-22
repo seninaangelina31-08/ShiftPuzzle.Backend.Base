c19edb50352bc370bbec93ab1ea3f1d11d3d70c2
Satlykovs <satlykovs@gmail.com>
2023-12-22 17:57:49 +0300

    feat: PracticeA finished Lesson10

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace PracticeA;
 class FileOperations
 {
+
+    public static void Write()
+    {
+        string[] strs = new string[10];
+        for (int i = 0; i < strs.Length; i++)
+        {
+            strs[i] = $"Primer_stroki {i}";
+        }
+
+        File.WriteAllLines("Test.txt", strs);        
+    }
+
+    public static string[] Read()
+    {
+        return File.ReadAllLines("Test.txt");
+    }
     static void Main(string[] args)
     { 
-
-        // чтение запись в файл test.txt
+        Write();
+        string[] file = Read();
+        
+        foreach (string el in file)
+        {
+            Console.WriteLine(el);
+        }
     }
 }

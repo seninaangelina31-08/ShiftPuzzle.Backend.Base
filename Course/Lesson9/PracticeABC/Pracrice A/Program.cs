34cb95eb221987b16ce447b0f89d24210a450f72
venimu <ven1mu@yandex.ru>
2023-12-22 16:28:42 +0300

    Practice B

---
+++ Course/Lesson9/PracticeABC/Pracrice A/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace Pracrice_A;
+
+
+
+class Person {
+
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age) {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public void Introduce() {
+        Console.WriteLine("Hello, my name is "+this.Name);
+    }
+
+    public bool W_age(int age) {
+        if (age > 0) {
+            this.Age = age;
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person man = new Person("some name", 15);
+        man.Name = "abc";
+        man.Age = 25;
+        Person[] arr = new Person[10];
+        for (int i = 0; i < 10; i++) {
+            Person mdn = new Person("Clone"+i, 10+i);
+            arr[i] = mdn;
+            mdn.Introduce();
+        }
+
+    }
+}

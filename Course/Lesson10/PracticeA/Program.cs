3254323142ebee9502cc4c22fca1e0d0732c0026
venimu <ven1mu@yandex.ru>
2023-12-22 18:04:16 +0300

    Practice A 	new file:   Course/Lesson10/Lesson10.sln 	modified:   Course/Lesson10/PracticeA/Program.cs 	new file:   Course/Lesson10/PracticeA/test.txt

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace PracticeA;
+using System;
+using System.IO;
+
+
 class FileOperations
 {
-    static void Main(string[] args)
-    { 
+    static void Main(string[] args) { 
+        string[] lines = new string[5];
+        for (int i = 0; i < lines.Length; i++) {
+            lines[i] = "aaaaaa";
+        }
+        File.WriteAllLines("test.txt", lines);
 
-        // чтение запись в файл test.txt
+        string[] strs = File.ReadAllLines("test.txt");
+        foreach(string line in strs){
+            Console.WriteLine(line);
+        }
     }
 }

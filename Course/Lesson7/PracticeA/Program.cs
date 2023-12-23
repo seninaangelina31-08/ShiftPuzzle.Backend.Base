1429c38ae203d27fcdcc021cf47af005c8096117
Nazelko <nazelko228@gmail.com>
2023-12-23 23:35:47 +0300

    feat: added answer to task 7

--- Course/Lesson7/PracticeA/Program.cs
+++ Course/Lesson7/PracticeA/Program.cs
@@ -1,22 +1,40 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 namespace PracticeA;
 =======
 ﻿namespace PracticeA;
 >>>>>>> main
+=======
+﻿namespace PracticeA;
+
+>>>>>>> 2a6ab33e (feat: added answer to task 7)
 class Program
 {
     static void Main(string[] args)
     {
+<<<<<<< HEAD
     }
     //task1
     static int Factorial(int a)
     {
         if (a <= 1)
+=======
+        string t1 = rev("mmeow");
+    
+        Console.WriteLine(t1);
+    }
+    //**Факториал числа**
+    //- Написать рекурсивную функцию для вычисления факториала числа.
+    public static int recurs(int c)
+    {
+        if (c<=1)
+>>>>>>> 2a6ab33e (feat: added answer to task 7)
         {
             return 1;
         }
         else
         {
+<<<<<<< HEAD
             return a * Factorial(a - 1);
         }
     }
@@ -240,3 +258,176 @@ class BinaryTree
         }
     }
 }
+=======
+            return c*recurs(c-1);
+        }
+
+    }
+
+
+
+    // **Числа Фибоначчи**
+    // Рекурсивно находить n-ое число Фибоначчи.
+    public static int fib(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else{
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    // **Обращение строки**
+    //Создать рекурсивную функцию для обращения строки.
+    public static string rev(string str)
+    {
+        if (str.Length <= 1)
+        {
+            return str;
+        }
+        return rev(str.Substring(1)) + str[0];
+    }
+
+    //**Сумма элементов массива**
+    //Написать рекурсивную функцию для нахождения суммы элементов массива.
+    public static int summas(int[] numbers, int i = 0)
+    {
+        if (i >= numbers.Length){
+            return 0;
+        }
+        return numbers[i] + summas(numbers, i + 1);
+    }
+    
+
+    //**Наибольший общий делитель (НОД)**
+    //Реализовать алгоритм Евклида для нахождения НОД двух чисел с использованием рекурсии.
+    public static int nod(int a, int b)
+    {
+        if (b == 0){
+            return a;
+        }
+        return nod(b, a % b);
+
+    }
+
+    //**Проверка палиндрома**
+    //Создать рекурсивную функцию, которая проверяет, является ли строка палиндромом.
+    public static bool pal(string str)
+    {
+        if (str.Length <= 1)
+        {
+            return true;
+        }
+        if (str[0] != str[str.Length - 1])
+        {
+            return false;
+        }
+        return pal(str.Substring(1, str.Length - 2));
+
+    }
+
+    // **Ханойские башни**
+    // Решить задачу о Ханойских башнях с использованием рекурсии.
+    public static void HanoiTower(int n, char a, char b, char c)
+    {
+    if (n == 1)
+    {
+        Console.WriteLine($"Переместить дпервый диск с {a} на {c}");
+        return;
+    }
+
+    HanoiTower(n - 1, a, c, b);
+    Console.WriteLine($"Переместить диск  {n} с {a} на {c}");
+    HanoiTower(n - 1, b, a, c);
+    }
+
+    // **Перебор подмножеств множества**
+    // Написать функцию, которая рекурсивно перебирает все подмножества данного множества.
+    public static void GenerateSubsets(int[] set, int i, int[] podmn)
+    {
+        if (i == set.Length)
+        {
+            // Обработка полученного подмножества
+            Console.Write("Subset: ");
+            foreach (int e in podmn)
+            {
+                Console.Write(e + " ");
+            }
+            Console.WriteLine();
+            return;
+        }
+
+        // Вызов функции для следующего элемента, включая текущий элемент в подмножество
+        podmn[i] = set[i];
+        GenerateSubsets(set, i + 1, podmn);
+
+        // Вызов функции для следующего элемента, исключая текущий элемент из подмножества
+        podmn[i] = 0;
+        GenerateSubsets(set, i + 1, podmn);
+    }
+
+    // **Глубокое копирование объекта**
+    // Реализовать рекурсивное глубокое копирование сложного объекта (например, дерева).
+    public void insert(int x) 
+    {
+        root = ins(root, x);
+    }
+    
+    private static Node ins(Node node, int x) {
+        if (node == null) 
+        {
+            return new Node(x);
+        }
+        if (x < node.key) 
+        {
+            node.left = ins(node.left, x);
+        } 
+        else if (x > node.key) 
+        {
+            node.right = ins(node.right, x);
+        }
+        return node;
+    }
+    // **Генерация всех возможных перестановок**
+    // Написать функцию для генерации всех возможных перестановок заданного списка или строки.
+    public static string generate(string str)
+{
+    if (str.Length <= 1)
+    {
+        return str;
+    }
+
+    string ab = string.Empty;
+
+    foreach (char c in str)
+    {
+        string cd = string.Empty;
+
+        foreach (char i in str)
+        {
+            if (i != c)
+            {
+                cd += i;
+            }
+        }
+
+        string remab = generate(cd);
+
+        foreach (char a in remab)
+        {
+            ab += c + a.ToString() + " ";
+        }
+    }
+
+    return ab.TrimEnd(',');
+}
+
+}
+
+>>>>>>> 2a6ab33e (feat: added answer to task 7)
--- Course/Lesson7/PracticeB-С/Program.cs
+++ Course/Lesson7/PracticeB-С/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace PracticeB_С;
 class Program
 {
@@ -5,4 +6,72 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+=======
+﻿using System;
+
+class Program
+{
+    
+        static void Main(string[] args)
+    {
+        Console.Write("  Выделите ячейки памяти = ");
+        var len = Convert.ToInt32(Console.ReadLine());
+        var b = new int[len];
+        for (var i = 0; i < b.Length; ++i)
+        {
+            Console.Write("a[{0}] = ", i);
+            b[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", QuickSort(b)));
+    }
+    
+   
+   
+    //метод для обмена элементов массива
+            static void Swap(ref int x, ref int y)
+            {
+                var t = x;
+                x = y;
+                y = t;
+            }
+
+            //метод возвращающий индекс опорного элемента
+            static int Partition(int[] numbers, int mini, int maxi)
+            {
+                var p = mini - 1;
+                for (var i = mini; i < maxi; i++)
+                {
+                    if (numbers[i] < numbers[maxi])
+                    {
+                        p++;
+                        Swap(ref numbers[p], ref numbers[i]);
+                    }
+                }
+
+                p++;
+                Swap(ref numbers[p], ref numbers[maxi]);
+                return p;
+            }
+
+            //быстрая сортировка
+            static int[] QuickSort(int[] numbers, int mini, int maxi)
+            {
+                if (mini >= maxi)
+                {
+                    return numbers;
+                }
+
+                var ind = Partition(numbers, mini, maxi);
+                QuickSort(numbers, mini, ind - 1);
+                QuickSort(numbers, ind + 1, maxi);
+
+                return numbers;
+            }
+
+            static int[] QuickSort(int[] numbers)
+            {
+                return QuickSort(numbers, 0, numbers.Length - 1);
+            }
+>>>>>>> 2a6ab33e (feat: added answer to task 7)
 }

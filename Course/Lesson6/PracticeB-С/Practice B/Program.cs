83514a6f26a92f508ad4342e22df89aa5c49c389
Nazelko <nazelko228@gmail.com>
2023-12-23 23:34:14 +0300

    feat: added answer to task 6

---
+++ Course/Lesson6/PracticeB-С/Practice B/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Practice_B;
+
+class Program
+{
+    static void Main(string[] args)
+        {
+            int[] t1 = podmas(new int[]{ 2, 4, 7, 9, 2, 6, 4, -2, 1, 8 }, 3);
+        }
+    
+    public static int[] podmas(int[] numbers, int n)
+    {
+    //*Функция Поиска Подмассива с Максимальной Суммой (Задача о Максимальной Подпоследовательности)**
+    //Описание**: Напишите функцию, которая принимает массив целых чисел и возвращает подмассив с максимальной суммой элементов.
+    // Это задача о максимальной подпоследовательности, требующая выявления подмассива с максимальной суммой среди всех подмассивов.
+    //Сложность**: Требуется использование циклов для итерации по массиву и алгоритмический подход для вычисления максимальной суммы.
+        
+        // длина подмассива
+        int max = -10000;
+        int indmaxsum = 0;
+
+        for (int i = 0; i <= numbers.Length - n; i++)
+        {
+            int sum = 0;
+
+            for (int j = i; j < i + n; j++)
+            {
+                sum += numbers[j];
+            }
+
+            if (sum > max)
+            {
+                max = sum;
+                indmaxsum = i;
+            }
+        }
+
+        int[] numb = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            numb[i] = numbers[indmaxsum + i];
+        }
+                
+        Console.WriteLine("Найденный подмассив с максимальной суммой элементов:");
+        Console.WriteLine(string.Join(" ", numb));
+        return numb;
+    }
+}
---
+++ Course/Lesson6/PracticeB-С/Practice C/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice_C;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 2,6, 2, 8 , 0 };
+        recurs(numbers, 0);
+    }
+    //Функция Поиска Всех Уникальных Перестановок Массива**
+    //Описание**: Создайте функцию, которая принимает массив уникальных целых чисел и возвращает список всех возможных 
+    //уникальных перестановок этих чисел. Задача включает в себя генерацию всех комбинаций элементов массива.
+    //Сложность**: Необходимо использовать рекурсию или итеративный подход с использованием стека/очереди
+    // для генерации всех возможных комбинаций. Функция должна уметь обрабатывать массивы разной длины.
+    static void recurs(int[] numbers, int n)
+    {
+        if (n == numbers.Length)
+        {
+            Console.WriteLine(string.Join(", ", numbers));
+            return;
+        }
+        
+        for (int i = n; i < numbers.Length; i++)
+        {
+            perem(numbers, n, i);
+            recurs(numbers, n + 1);
+            perem(numbers, n, i);
+        }
+    }
+    
+    static void perem(int[] numbers, int i, int j)
+    {
+        int novch = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = novch;
+    }
+}

b51b7f5d552fa4e22bcb5ef215eb0b406d027b94
Nazelko <nazelko228@gmail.com>
2023-12-23 23:11:49 +0300

    feat: added answer to task 5

--- Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
+++ Course/Lesson5/PracticeA-B/LessonProject/Tasks.cs
@@ -10,10 +10,41 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
+           
+            
+              int[] numbers = { 2, 4, 7, 9, 2, 6, 4, -2, 1, 8 }; // пример массива чисел
+                int n = 3; // длина подмассива
+                int max = -10000;
+                int indmaxsum = 0;
 
+                for (int i = 0; i <= numbers.Length - n; i++)
+                {
+                    int sum = 0;
+
+                    for (int j = i; j < i + n; j++)
+                    {
+                        sum += numbers[j];
+                    }
+
+                    if (sum > max)
+                    {
+                        max = sum;
+                        indmaxsum = i;
+                    }
+                }
+
+                int[] numb = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numb[i] = numbers[indmaxsum + i];
+                }
+                
+            Console.WriteLine("Найденный подмассив с максимальной суммой элементов:");
+            Console.WriteLine(string.Join(" ", numb));
             //не выходя за пределы
-        }
-    }
+          }    
+    }    
+    
 
     public class Task2
     {
@@ -23,7 +54,13 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            
+            int[] numbers={2,4,7,9,2,6,4,2,1,8};
+            Array.Sort(numbers);
+            var str = string.Join(" ", numbers);
+            Console.WriteLine(str);
+            int unik = numbers.Distinct().Count();
+            Console.WriteLine(unik);
             //не выходя за пределы
         }
     }
@@ -35,21 +72,63 @@ namespace LessonProject
             Console.WriteLine("\n\n\nЗадача 3: Анализировать массив целых чисел и определить число, которое встречается чаще всего. Если таких чисел несколько, вывести их все и указать, сколько раз каждое из них встречается в массиве.");
             Console.WriteLine("РЕШЕНИЕ:");
 
+            
             //решайте задачу здесь
+            
+            int[] numbers={2,4,7,9,2,6,4,2,1,8};
+            int[] counts = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int k = 1;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        k++;
+                    }
+                }
+                counts[i] = k;
+            }
 
+            int max = counts.Max();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (counts[i] == max)
+                {
+                    Console.WriteLine("Число "+ numbers[i] +" встречается " +  counts[i] +" раз.");
+                }
+            }
             //не выходя за пределы
         }
     }
 
-    public class Task4
+    public class Task4 
+
     {
         static Task4()
         {
             Console.WriteLine("\n\n\nЗадача 4: Выполнить циклическую ротацию массива на K позиций вправо. Вывести исходный и измененный массивы.");
             Console.WriteLine("РЕШЕНИЕ:");
-
+            
             //решайте задачу здесь
+            int K=3;
 
+            int[] numbers = {1, 3, 7, 8,3,1};
+            
+                
+            for (int i = 0; i < K; i++)
+            {
+                int sdv = numbers[numbers.Length - 1];
+
+                for (int j= numbers.Length - 1; j > 0; j--)
+                {
+                    numbers[j] = numbers[j - 1];
+                }
+
+                numbers[0] = sdv;
+            }
+        
+            Console.WriteLine(string.Join(" ", numbers));
             //не выходя за пределы
         
         }
@@ -63,7 +142,20 @@ namespace LessonProject
             Console.WriteLine("РЕШЕНИЕ:");
 
             //решайте задачу здесь
-
+            
+        
+            int[] numbers = {1, 3, 7, 8, -6, 4, -2, 12, 6};
+            int[] numb = {5, 7, 8, 2, 6};
+            int[] nu = numbers.Union(numb).ToArray();
+            
+            foreach (int i in nu)
+            {
+                if (numbers.Contains(i)&&numb.Contains(i))
+                {
+                    Console.WriteLine("Число, встречающееся в обоих массивах:"+i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", nu));
             //не выходя за пределы
         }
     }

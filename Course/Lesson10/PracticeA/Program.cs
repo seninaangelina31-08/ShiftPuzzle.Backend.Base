3f0e15a0a554b1d95c2ab82845e33779fbfc0852
Юрий <zaretskiy.y.a@gmail.com>
2023-12-27 13:16:48 +0300

    feature: lesson 10

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -4,6 +4,25 @@ class FileOperations
     static void Main(string[] args)
     { 
 
-        // чтение запись в файл test.txt
+        WriteToFile();
+        ReadFromFileAndPrint();
+    }
+    public static void WriteToFile()
+    {
+        string[] lines = new string[3];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i] = (i + 1) + ") string";
+        }
+        File.WriteAllLines("test.txt", lines);
+    }
+
+    public static void ReadFromFileAndPrint()
+    {
+        string[] lines = File.ReadAllLines("test.txt");
+        foreach(string line in lines)
+        {
+            Console.WriteLine(line);
+        }
     }
 }

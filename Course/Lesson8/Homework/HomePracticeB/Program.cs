986fba8bd7313935414461bdc194cf41e3294eb4
timurkaff <timalololohka@gmail.com>
2023-12-29 22:27:48 +0400

    feat:(Сделал Homework a и b)

---
+++ Course/Lesson8/Homework/HomePracticeB/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomePracticeB;
+
+class InterestCalculator
+{
+    public static double CalculateCompoundInterest(double principal, double annualRate, int years)
+    {
+        for (int year = 0; year < years; year++)
+        {
+            principal += principal * annualRate;
+        }
+        return principal;
+    }
+
+    static void Main(string[] args)
+    {
+        double initialPrincipal = 1000;
+        double annualRate = 0.05;
+        int numberOfYears = 5;
+
+        double finalAmount = CalculateCompoundInterest(initialPrincipal, annualRate, numberOfYears);
+        Console.WriteLine($"Конечная сумма после {numberOfYears} лет: {finalAmount}");
+    }
+}
---
+++ Course/Lesson8/Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework;
+
+class LoanApproval
+{
+    public static string CheckLoanApproval(double balance)
+    {
+        if (balance <= 0)
+        {
+            return "Кредит одобрен!";
+        }
+        else
+        {
+            return "У вас достаточно средств, кредит не нужен.";
+        }
+    }
+}
+
+class DepositWithdrawal
+{
+    public static double ModifyBalance(double deposit, double balance)
+    {
+        return balance - deposit;
+    }
+}

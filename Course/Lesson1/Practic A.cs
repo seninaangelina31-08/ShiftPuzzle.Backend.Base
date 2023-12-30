e7e8303091e9347217217b703e83d55e0c6102a7
Maxon <maxn2703@gmail.com>
2023-12-30 21:49:50 +0300

    Lesson 10 Practice A

---
+++ Course/Lesson1/Practic A.cs
@@ -0,0 +1,22 @@
+namespace TeleprompterConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+double half;
+string words;
+char world;
+
+number = 3;
+half = 1.3;
+world = 'c';
+words = "Nice day";
+
+Console.WriteLine(number);
+Console.WriteLine(half);
+Console.WriteLine(words);
+Console.WriteLine(world);
+    }
+}
--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace PracticeA;
+﻿using System;
+using System.IO;
+namespace PracticeA;
 class FileOperations
 {
     static void Main(string[] args)
-    { 
+{ 
+    
+        WriteToFile();
+        ReadAllFiles();
+}
 
-        // чтение запись в файл test.txt
+
+public static void WriteToFile()
+{
+string[] new_file = new string[10];
+for(int i = 0; i < new_file.Length; i++)
+{
+    new_file[i] = "С Новым годом!!!!!";
+}
+File.WriteAllLines("Congratilation.txt", new_file);
+}
+
+public static void ReadAllFiles()
+{
+    string[] new_file = File.ReadAllLines("Congratilation.txt");
+    foreach (string i in new_file)
+    {
+        Console.WriteLine(i);
     }
+}
+
+
 }

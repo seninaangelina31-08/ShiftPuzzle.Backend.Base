0e66200a4eb38d03b9d71ca8b5dd2f699002f5ca
Maxon <maxn2703@gmail.com>
2023-12-30 21:45:18 +0300

    dev: Lesson 10 Practice A

---
+++ Course/Lesson10/Practice A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Practice_A;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    WriteToFile();
+    ReadAllFiles();
+    }
+
+
+public static void WriteToFile()
+{
+string[] new_file = new string[10];
+for(int i = 0; i < new_file.Length; i++)
+{
+    new_file[i] = "С Новым годом!!!!!";
+}
+File.WriteAllLines("Congratilation.txt", new_file);
+}
+
+public static void ReadAllFiles()
+{
+    string[] new_file = File.ReadAllLines("Congratilation.txt");
+    foreach (string i in new_file)
+    {
+        Console.WriteLine(i);
+    }
+}
+}

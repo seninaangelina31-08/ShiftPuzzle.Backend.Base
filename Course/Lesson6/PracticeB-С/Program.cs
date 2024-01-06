751b71351012c4330c7ad3e73b5cb5fe6ec64ae7
LokiMATI <r.matigorov@yandex.ru>
2024-01-06 15:32:18 +0300

    feat(Lesson6): PracticeC complited.

--- Course/Lesson6/PracticeB-С/Program.cs
+++ Course/Lesson6/PracticeB-С/Program.cs
@@ -29,18 +29,54 @@ foreach (int arr in array_test)
 }
 
 // // Практика C
-// int[] array = {1, 2, 3};
-// int l = array.Length;
-// int[] level = new int[l];
-// for (int i = 0; i < level.Length; i++)
-// {
-//     level[i] = 0;
-// }
-// void Variation(int[] array)
-// {
-//     if (level[l - 1] == l)
-//     {
-//         level[l - 2]++;
-//     }
-//     else
-// }
+void Perestanovka(string[] array)
+{
+    int[] count = new int[array.Length];
+    string str = "";
+    void Recursiv_10(string[] array, int[] count, string str, int i = 0, bool test = true)
+    {
+        if (count[0] >= array.Length)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            if (i >= count.Length)
+            {
+                count[i - 1]++;
+                i = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    for (int k = 0; k < str.Length; k++)
+                    {
+                        if (str[j] == str[k] && j != k) test = false;
+                    }
+                }
+                if (test) 
+                {
+                    Console.WriteLine(str);
+                }
+                str = "";
+            }
+            if (i != (count.Length - 1) && count[i + 1] >= count.Length)
+            {
+                count[i + 1] = 0;
+                count[i]++;
+            }
+            if (count[i] >= count.Length && i != 0)
+            {
+                count[i] = 0;
+                count[i - 1]++;
+            }
+            if (count[0] >= count.Length)
+            {
+                return;
+            }
+            str += array[count[i]];
+            Recursiv_10(array, count, str, i + 1);
+        }
+    }
+    Recursiv_10(array, count, str);
+}
+string[] array_10 = {"0", "1", "2", "3"};
+Perestanovka(array_10);

ac2875451576f46c438a7ddb5c33d8825601cf46
Maxon <maxn2703@gmail.com>
2024-01-07 17:02:36 +0300

    dev: lesson 8 Homework

---
+++ Course/Lesson8/Lesson7/PracticeA/Practices/Practice4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     int[] arr = [2, 3, 1];
+        Console.WriteLine(Maxelement(arr, (uint)arr.Length));
+        Console.ReadKey(true);
+    }
+    static int Maxelement(int[] arr, uint size)
+    {
+        if (size > 1u)
+            return Math.Max(arr[size - 1], Maxelement(arr, size - 1));
+        return arr[0];
+    }
+}

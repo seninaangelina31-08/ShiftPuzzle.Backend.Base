0d22a168d5eeecf5f31fe56d798133b90172b8f5
LokiMATI <r.matigorov@yandex.ru>
2024-01-07 16:24:38 +0300

    feat(Lesson7): PracticeA Task 8-10 complited.

--- Course/Lesson7/PracticeA/Program.cs
+++ Course/Lesson7/PracticeA/Program.cs
@@ -89,20 +89,145 @@ int Hanoisk(int n, int i = 0, int sum = 1)
     sum *= 2;
     return Hanoisk(n, i + 1, sum);
 }
-// Console.WriteLine(Hanoisk(8));
 
-// void Perebor(int[] array, int i = 0, int end = 1, int length_01)
-
-void f(int[] arr, int i = 0)
+string[] array_8 = {"0", "1", "2", "asd"};
+void Perebor(string[] array)
 {
-    if (i >= arr.Length)
+    int[] count = new int[1];
+    void Recursiv_8(string[] array, int[] count, string str_copy = "", string str_copy_Kostil = "", int i = 0)
     {
-        Console.WriteLine();
-        return;
+        if (count.Length >= array.Length)
+        {
+            // Console.WriteLine("EXIT");
+            return;
+        }
+        if (count[0] >= array.Length && count.Length < (array.Length - 1))
+        {
+            // Console.WriteLine("IF 1");
+            Array.Resize(ref count, count.Length + 1);
+            for (int k = 0; k < count.Length; k++)
+            {
+                count[k] = k;
+            }
+        }
+        if (i >= count.Length)
+        {
+            // Console.WriteLine("IF 2");
+            count[count.Length - 1]++;
+            i = 0;
+        }
+        if (count[i] >= array.Length && i != 0)
+        {
+            // Console.WriteLine("IF 3");
+            count[i] = 0;
+            count[i - 1]++;
+        }
+        for (int k = 0; k < count.Length; k++)
+        {
+            if (count[k] >= array.Length && k != 0)
+            {
+                // Console.WriteLine("IF 4");
+                count[k - 1]++;
+                count[k] = count[k - 1] + 1;
+            }
+            if (count[0] >= array.Length && count.Length < array.Length)
+            {
+                // Console.WriteLine("IF 5");
+                Array.Resize(ref count, count.Length + 1);
+                for (int j = 0; j < count.Length; j++)
+                {
+                    count[j] = j;
+                }
+            }
+        }
+        if (str_copy == "")
+        {
+            // Console.WriteLine("IF 6");
+            foreach (int arr in count)
+            {
+                // Console.WriteLine("TEX: " + arr);
+                if (arr < array.Length){
+                // Console.WriteLine("IF 7");
+                str_copy += array[arr];
+                }
+                else{
+                    // Console.WriteLine("IF 8");
+                    return;
+                }
+            }
+            if (str_copy != str_copy_Kostil) Console.WriteLine(str_copy);
+        }
+        Recursiv_8(array, count, "", str_copy, i + 1);
     }
-    Console.Write(i);
-    i++;
-    f(arr, i);
+    Recursiv_8(array, count);
 }
-int[] array = {3, 3};
-f(array);
+Perebor(array_8);
+
+int[] array_9 = {1, 2, 4, 6, 1, 2, 1, 10 , 123, 0};
+int[] Copy(int[] array)
+{
+    int[] array_copy = new int[array.Length];
+    int[] Recursiv_9(int[] array, int[] array_copy, int i = 0)
+    {
+        if (i >= array.Length) return array_copy;
+        array_copy[i] = array[i];
+        return Recursiv_9(array, array_copy, i + 1);
+    }
+    return Recursiv_9(array, array_copy);
+}
+// foreach (int arr in Copy(array_8))
+// {
+//     Console.WriteLine(arr);
+// }
+
+void Perestanovka(string[] array)
+{
+    int[] count = new int[array.Length];
+    string str = "";
+    void Recursiv_10(string[] array, int[] count, string str, int i = 0, bool test = true)
+    {
+        if (count[0] >= array.Length)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            if (i >= count.Length)
+            {
+                count[i - 1]++;
+                i = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    for (int k = 0; k < str.Length; k++)
+                    {
+                        if (str[j] == str[k] && j != k) test = false;
+                    }
+                }
+                if (test) 
+                {
+                    Console.WriteLine(str);
+                }
+                str = "";
+            }
+            if (i != (count.Length - 1) && count[i + 1] >= count.Length)
+            {
+                count[i + 1] = 0;
+                count[i]++;
+            }
+            if (count[i] >= count.Length && i != 0)
+            {
+                count[i] = 0;
+                count[i - 1]++;
+            }
+            if (count[0] >= count.Length)
+            {
+                return;
+            }
+            str += array[count[i]];
+            Recursiv_10(array, count, str, i + 1);
+        }
+    }
+    Recursiv_10(array, count, str);
+}
+// string[] array_10 = {"0", "1", "2", "3"};
+// Perestanovka(array_10);

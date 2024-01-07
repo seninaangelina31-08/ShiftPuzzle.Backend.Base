302e4ecb1ab736a001da5e8a14da555b791e5ddc
Frain <89194392174pp@gmail.com>
2024-01-07 23:15:04 +0300

    feature(lesson10): practiceBC complete

--- Course/Lesson10/PracticeBC/Program.cs
+++ Course/Lesson10/PracticeBC/Program.cs
@@ -51,8 +51,52 @@ public class Employee : Person
 
 public class PersonFileService
 {
+    public List<Person> People;
+    public PersonFileService(List<Person> people)
+    {
+        this.People = people;
+    }
+
+    public static void WritePeopleToFile(List<Person> people)
+    {   
+        string[] lines = new string[5];
     
+        int c = 0;
+        foreach (var ar in people)
+        {
+            if (ar.GetType() == typeof(Employee))
+            {
+                lines[c] = $"{ar.Name} {ar.Age}";
+            }
+
+            if (ar.GetType() == typeof(Person))
+            {
+                lines[c] = $"{ar.Name} {ar.Age}";
+            }
+
+            c++;
+        }
+
+
+        File.WriteAllLines("test.txt", lines);
+    }
+    public static Person[] ReadPeopleFromFile(string name_file)
+    {
+        string[] lines = File.ReadAllLines(name_file);
+
+        Person[] ans = new Person[lines.Length-2];
+
+
+        for (int i = 0; i < lines.Length-2; i++)
+        {
+            string[] line = lines[i].Split(' ');
+            ans[i] = new Person(line[0], Convert.ToInt32(line[1]));
+        }
+
+        return ans;
+    }
 }
+
 
 
 public class Program
@@ -69,16 +113,15 @@ public class Program
 
 
         // Writing people to the file
-        //PersonFileService.WritePeopleToFile(people);
+        PersonFileService.WritePeopleToFile(people);
 
 
         // Reading people from the file
-        //var peopleFromFile = PersonFileService.ReadPeopleFromFile();
+        var peopleFromFile = PersonFileService.ReadPeopleFromFile("test.txt");
         
-        
-        //foreach (var person in peopleFromFile)
-        //{
-        //    person.Introduce();
-        //}
+        foreach (var person in peopleFromFile)
+        {
+            person.Introduce();
+        }
     }
 }

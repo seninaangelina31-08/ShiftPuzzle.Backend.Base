54f04bb8d48274222f8a4532f8a07478e1730daf
IvanSobolev <i@bezlikiy.ru>
2024-01-08 19:11:56 +0300

    feat: homework4 and 8

---
+++ Course/Lesson4/Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    //task 1
+    static int[] splitArray(int[] arrA, int[] arrB)
+    {
+        int[] result = new int[arrA.Length + arrB.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (i < arrA.Length)
+            {
+                result[i] = arrA[i];
+            }
+            else
+            {
+                result[i] = arrB[i - arrA.Length];
+            }
+        }
+        return result;
+    }
+
+    //task 2
+    static int[] reverseNum(int[] arr, int num)
+    {
+        int[] result = new int[arr.Length];
+        if (num > arr.Length)
+        {
+            num = arr.Length;
+        }
+        for (int i = arr.Length - num; i < arr.Length; i++)
+        {
+            result[i + num - arr.Length] = arr[i];
+        }
+        for (int i = 0; i < arr.Length - num; i++)
+        {
+            result[i + num] = arr[i];
+        }
+        return result;
+    }
+}
---
+++ Course/Lesson8/Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace homework;
+
+class Program
+{
+    static String loan_approval(int balance)
+    {
+        if (balance <= 0)
+        {
+            return "Кредит одобрен!";
+        }
+        else
+        {
+            return "У вас достаточно средст, кредит не нужен";
+        }
+    }
+    int deposit_withdrawal(int deposit, int balance)
+    {
+        return balance - deposit;
+    }
+    
+    static int calculate_compound_interest(int startSum, int proch, int numYear)
+    {
+        if (numYear <= 1)
+        {
+            return startSum + (startSum * proch);
+        }
+        else
+        {
+            return calculate_compound_interest(startSum + (startSum * proch), proch, numYear - 1);
+        }
+    }
+}

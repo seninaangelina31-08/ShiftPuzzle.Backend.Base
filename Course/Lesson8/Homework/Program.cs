904583ea1d61131cbad5b26a3f8360da0feb85d5
venimu <ven1mu@yandex.ru>
2024-01-08 16:59:29 +0300

    new file:   Course/Lesson8/Homework/Homework.csproj 	new file:   Course/Lesson8/Homework/Homework.sln 	new file:   Course/Lesson8/Homework/Program.cs Homework for 8-th lesson

---
+++ Course/Lesson8/Homework/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework;
+
+class Program
+{
+    public string loan_approval(int balance){
+        if (balance <= 0){
+            return "Кредит одобрен!";
+        }
+        else {
+            return "У вас достаточно средств, кредит не нужен.";
+        }
+    }
+    public int deposit_withdrawal(int deposit, int balance) {
+        return balance - deposit;
+    }
+
+    public float calculate_compound_interest(float balance, float percentage, int years) {
+        for (int i = 0; i < years; i++) {
+            balance += balance*percentage;
+        }
+        return balance;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

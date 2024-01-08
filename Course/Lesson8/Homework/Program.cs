65acc9fca995d86d9cb1ad0be6cb494e07f2126a
Nazelko <nazelko228@gmail.com>
2024-01-08 21:42:05 +0300

    feat: added answer to task 8

---
+++ Course/Lesson8/Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework;
+
+class Program
+
+{
+    public string LoanApproval(int balance)
+    {
+        if (balance <= 0)
+        {
+            return "Кредит одобрен!";
+        }
+        else
+        {
+            return "У вас достаточно средств, кредит не нужен.";
+        }
+    }
+
+    public int DepositWithdrawal(int deposit, int balance)
+    {
+        return balance - deposit;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+    }
+
+    public static double CalculateCompoundInterest(double nach, double procent, int years)
+    {
+
+        double con = nach * Math.Pow(1 + procent/100, years);
+        return con;
+    }
+}

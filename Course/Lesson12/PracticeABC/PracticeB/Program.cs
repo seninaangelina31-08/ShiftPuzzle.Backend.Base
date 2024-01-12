ab82d1f3cd9b04227644b0e5cd4f392b69735a38
Frain <89194392174pp@gmail.com>
2024-01-12 18:03:13 +0300

    fixed(broken_json_files): all fix

---
+++ Course/Lesson12/PracticeABC/PracticeB/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace practiceb;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Product
+{
+    public string Name;
+    public int Price;
+    public string Description;
+
+    public Product(string name, int price, string description)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Description = description;
+    }
+}

7fb9efb531f496fc5e5056ae5e5f15beb29d0e43
“Stranik19” <220v.adidas@gmail.com>
2024-01-12 11:31:46 +0300

    feat:(Lesson7)

---
+++ Course/Lesson7/PracticeA/zzz1/Program.cs
@@ -0,0 +1,28 @@
+﻿public int EuclideanAlgorithm(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return EuclideanAlgorithm(b, a % b);
+    }
+}
+
+//НОД ^
+
+
+public int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+// Факториал числа ^

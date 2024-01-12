eb6e1f3a324439f68910299a9c29306790e7cdd6
Юрий <zaretskiy.y.a@gmail.com>
2024-01-12 19:32:01 +0300

    feature lesson 12 C

--- Course/Lesson12/PracticeABC/PracticeC/Program.cs
+++ Course/Lesson12/PracticeABC/PracticeC/Program.cs
@@ -1,4 +1,27 @@
 ﻿namespace PracticeC;
+using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int price { get; set; }
+}
+
+public class Order
+{
+    public int user_id { get; set; }
+    public int product_id { get; set; }
+}
+
+public class User
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int money { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)

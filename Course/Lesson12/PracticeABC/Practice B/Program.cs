784e6615b3fdbff93b2d0f8f63390b8512765f9d
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-01-12 19:53:41 +0300

    Feat(Lesson 12): A

---
+++ Course/Lesson12/PracticeABC/Practice B/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice_B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+{
+    "category": "Electronics",
+    "products": [
+        "TV",
+        "Radio",
+        "Camera"
+    ]
+}
+
+class Product
+{
+    string name;
+    float price;
+    string description
+}
+
+class Order
+{
+    string category;
+    List<string> products
+}

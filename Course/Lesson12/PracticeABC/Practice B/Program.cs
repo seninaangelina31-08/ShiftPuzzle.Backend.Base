7658559010c01b26a2e8df94fbbdaf1fbbdad044
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-01-12 20:09:30 +0300

    Feat(Lesson 12): B

--- Course/Lesson12/PracticeABC/Practice B/Program.cs
+++ Course/Lesson12/PracticeABC/Practice B/Program.cs
@@ -8,24 +8,68 @@ class Program
     }
 }
 
-{
-    "category": "Electronics",
-    "products": [
-        "TV",
-        "Radio",
-        "Camera"
-    ]
-}
+
 
 class Product
 {
     string name;
-    float price;
-    string description
+    decimal price;
+    string description;
+}
+
+class Type
+{
+    string category;
+    List<string> products;
 }
 
 class Order
 {
-    string category;
-    List<string> products
+    int id;
+    List<string> items;
+    decimal total;
+}
+
+class User
+{
+    string name;
+    string email;
+    int purchases;
+}
+
+class Cart
+{
+    List<string> cart;
+}
+
+class Shipping
+{
+    string method;
+    decimal price;
+    int estimated_days;
+}
+
+class Payment
+{
+    string method;
+    string status;
+}
+
+class Review
+{
+    string product;
+    decimal rating;
+    string comment;
+}
+
+class Discount
+{
+    string product;
+    string discount;
+}
+
+class Address
+{
+    string type;
+    string address;
 }

b67b8f4236a6e196a48236092c1f8a0d37a7a028
Maxon <maxn2703@gmail.com>
2024-01-13 16:15:16 +0300

    dev: Lesson 11 Practice A

--- Course/Test_File/Program.cs
+++ Course/Test_File/Program.cs
@@ -5,35 +5,15 @@ class Program
     static void Main(string[] args)
     {
 
+Dictionary<string, string> students = new Dictionary<string, string>();
 
+students.Add("one", "Max");
+//students.Add("two", "Mox");
+foreach(var pers in students)
+{
+    Console.WriteLine(pers.Value);
+}
 
-    Random random = new Random();
-    int number = random.Next(1,10);
-    Console.WriteLine(number);
-
-
-    int i = 5; //Число
-    int b = 5;
-    string str; //Строка
-    str= i.ToString() + b.ToString();
-
-Console.WriteLine(str);
-
-    for (int x = 0; x <= 10; ++x )
-    {
-        for (int y = 0; y <= 10; ++y )
-        {
-            for ( int z = 0; z <= 10; ++z)
-            {
-                for ( int h = 0; h <= 10; ++h)
-                {
-                //count++;
-                string generatedpass = x.ToString() + y.ToString() + z.ToString() + h.ToString();
-                Console.WriteLine(generatedpass);
-                }
-            }
-        }
-    }
 
 
     }

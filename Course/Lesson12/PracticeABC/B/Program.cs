5a3850a7331f537e9c1b57a3ad309b98eebec4b0
IvanSobolev <i@bezlikiy.ru>
2024-01-13 20:27:55 +0300

    feat lesson 12

---
+++ Course/Lesson12/PracticeABC/B/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+class product
+{
+    string name;
+    int price;
+    string description;
+}
+
+class BJ1
+{
+    string category;
+    string[] products;
+}
+
+class order
+{
+    double id;
+    string[] items;
+    int total;
+}
+
+class user
+{
+    string name;
+    string email;
+    int purchases;
+}
+
+class card
+{
+    string[] cart;
+}
+
+class shipping
+{
+    string method;
+    int price;
+    int estimated_days;
+}
+class payment
+{
+    string method;
+    string status;
+}
+
+class reviews
+{
+    string product;
+    int rating;
+    string comment;
+}
+
+class discounts
+{
+    string product;
+    string discount;
+}
+
+class addresses
+{
+    string type;
+    string address;
+}

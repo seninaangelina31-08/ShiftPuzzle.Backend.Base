538c6bf4c818b6a38619988163184523cb632412
Nikita Bykov <nikita.bykov.322@gmail.com>
2024-01-16 23:57:22 +0300

    feat: Lesson13 PracticeB

--- Course/Lesson13/PracticeAB/Program.cs
+++ Course/Lesson13/PracticeAB/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 namespace PracticeAB;
 
@@ -24,25 +26,58 @@ namespace PracticeAB;
 
 [System.Serializable] public class Orders
 {
-    public string orderID {get; set;}
+    public string orderId {get; set;}
     public string customerName {get; set;}
     public int totalPrice {get; set;}
     public List<string> items {get; set;}
 
     public Orders() {}
 
-    public Orders(string OrderID, string CustomerName, int TotalPrice, List<string> Items)
+    public Orders(string OrderId, string CustomerName, int TotalPrice, List<string> Items)
     {
-        this.orderID = OrderID;
+        this.orderId = OrderId;
         this.customerName = CustomerName;
         this.totalPrice = TotalPrice;
         this.items = Items;
     }
 }
+
+public class Books
+{
+    public string title {get; set;}
+    public string author  {get; set;}
+    public int year {get; set;}
+
+    public Books(string Title, string Author, int Year)
+    {
+        this.title = Title;
+        this.author = Author;
+        this.year = Year;
+    }
+}
+[System.Serializable] public class Libraries
+{
+    public string libraryName {get; set;}
+    public List<Books> books {get; set;}
+
+    public Libraries() { }
+    
+    public Libraries(string LibraryName, List<Books> Books)
+    {
+        this.libraryName = LibraryName;
+        this.books = Books;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
+        var options1 = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        };
         string jsonFromFile = File.ReadAllText("1.json");
         Users user = JsonSerializer.Deserialize<Users>(jsonFromFile);
         if (user != null)
@@ -50,7 +85,9 @@ class Program
             if (user.name == "Иван Иванов")
                 Console.WriteLine($"Пользователь {user.name}:\nПочта: {user.email}\nID: {user.id}");
         }
+
         Console.WriteLine();
+
         jsonFromFile = File.ReadAllText("2.json");
         Orders order = JsonSerializer.Deserialize<Orders>(jsonFromFile);
         if (order != null)
@@ -65,6 +102,32 @@ class Program
                 Console.WriteLine($"на сумму {order.totalPrice}\nИтого к оплате со скидкой 10%: {order.totalPrice - order.totalPrice*10/100}");
             }
         }
+
         Console.WriteLine();
+
+        order.items.Add("Салфетки для монитора");
+        order.totalPrice += 1550;
+        order.totalPrice -= order.totalPrice * 2 / 100;
+        string json = JsonSerializer.Serialize(order, options1);
+        Console.WriteLine(json);
+        File.WriteAllText("4.json", json);
+
+        Console.WriteLine();
+
+        jsonFromFile = File.ReadAllText("5.json");
+        Libraries library = JsonSerializer.Deserialize<Libraries>(jsonFromFile);
+        if (library != null)
+        {
+            Console.WriteLine($"{library.libraryName}");
+            foreach(Books book in library.books)
+            {
+                Console.WriteLine($"{book.title} {book.author} {book.year}");
+            }
+        }
+        Books new_book = new Books("Капитанская дочка", "Александр Пушкин", 1836);
+        library.books.Add(new_book);
+        json = JsonSerializer.Serialize(library, options1);
+        Console.WriteLine(json);
+        File.WriteAllText("5.json", json);
     }
 }

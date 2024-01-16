31341880687c34b0ab1b0cdecb240cb03719546f
Юрий <zaretskiy.y.a@gmail.com>
2024-01-16 19:01:35 +0300

    feature lesson 13 B

--- Course/Lesson13/PracticeAB/Program.cs
+++ Course/Lesson13/PracticeAB/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic; // библиотека для работы с List и Dictionary
 using System.Text.Json; // библиотека для работы с JSON
 
@@ -44,10 +46,45 @@ namespace PracticeAB;
     }
 }
 
+[System.Serializable] public class Book
+{
+    // поля класса
+    public string title { get; set; }
+    public string author { get; set; }
+    public int year { get; set; }
+
+    public Book() { } // Пустой конструктор для десериализации
+
+    // конструктор класса
+    public Book(string title, string author, int year)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+
+[System.Serializable] public class Library
+{
+    // поля класса
+    public string libraryName { get; set; }
+    public List<Book> books { get; set; }
+
+    public Library() { } // Пустой конструктор для десериализации
+
+    // конструктор класса
+    public Library(string libraryName, List<Book> books)
+    {
+        this.libraryName = libraryName;
+        this.books = books;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
+        // Practic A
         string path = "1.json";
         string jsonFromFile = File.ReadAllText(path);
         User ivan = JsonSerializer.Deserialize<User>(jsonFromFile);
@@ -72,5 +109,24 @@ class Program
                 Console.WriteLine(item);
             }
         }
+
+        // Practic B
+
+        order_anna.totalPrice = Convert.ToInt32((order_anna.totalPrice + 1550) * 0.98);
+        order_anna.items.Add("Салфетки для монитора");
+
+        string json = JsonSerializer.Serialize(order_anna);
+        path = "2_new.json";
+        File.WriteAllText(path, json, Encoding.UTF8);
+
+        path = "5.json";
+        string jsonFromFile3 = File.ReadAllText(path);
+        Library library = JsonSerializer.Deserialize<Library>(jsonFromFile3);
+
+        library.books.Add(new Book { title = "Капитанская дочка", author = "Александр Пушкин", year = 1836});
+
+        string json2 = JsonSerializer.Serialize(library);
+        path = "5_new.json";
+        File.WriteAllText(path, json2, Encoding.UTF8);
     }
 }

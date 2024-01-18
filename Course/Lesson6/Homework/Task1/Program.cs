574edef658ecfda782a40fe7ee402274b45e21b8
Vanya Timofeev <ivantimofeev0006@gmail.com>
2024-01-18 19:47:43 +0300

    Feat: Lesson6 Homework completed

---
+++ Course/Lesson6/Homework/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] numbers = {1, 2, 3, 1, 2, 3};
+int mostFrequent = FindMostFrequentElement(numbers);
+Console.WriteLine($"Число, которое встречается часще всего в массиве: {mostFrequent}");
+
+static int FindMostFrequentElement(int[] number)
+{
+    Dictionary<int, int> mostFrequencyMap = new Dictionary<int, int>();
+
+    foreach (int num in number)
+    {
+        if (mostFrequencyMap.Containskey(num))
+        {
+            FrequencyMap[num]++;
+        }
+        else
+        {
+            FrequencyMap[num] = 1;
+        }
+    }
+
+    int maxFrequency = FrequencyMap.max(x => x.Value);
+    int mostFrequent = FrequencyMap.FirstOrDefalut(x => x.Value == maxFrequency).key;
+
+    return mostFrequent;
+}
---
+++ Course/Lesson6/Homework/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿int[,] matrix = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+    };
+    
+    Console.WriteLine("Исходная матрица:");
+    PrintMatrix(matrix);
+    
+    int[,] transposedMatrix = TransposeMatrix(matrix);
+    
+    Console.WriteLine("\nТранспонированная матрица:");
+        PrintMatrix(transposedMatrix);
+        
+        static int[,] TransposeMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            
+            int[,] transposedMatrix = new int[columns, rows];
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    transposedMatrix[j, i] = matrix[i, j];
+                    }
+                    }
+                    
+                    return transposedMatrix;
+                    }
+                    
+                    static void PrintMatrix(int[,] matrix)
+                    {int rows = matrix.GetLength(0);
+                    int columns = matrix.GetLength(1);
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            Console.Write(matrix[i, j] + " ");
+                            }
+                            Console.WriteLine();
+                            }
+                    }

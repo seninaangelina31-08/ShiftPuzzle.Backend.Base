f58cd142ab7d91537b713958fb8e7e53755fb9bb
DABer1805 <DABer1805@yandex.ru>
2024-01-19 18:55:19 +0300

    feat add homework solution to Lesson 8

---
+++ Course/Lesson8/Homework/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Homework {
+    class Program {
+         public static string LoanApproval(double balance) {
+            if (balance <= 0) {
+                return "Кредит одобрен!";
+            }
+            else {
+                return "У вас достаточно средств, кредит не нужен.";
+            }
+        }
+
+        public static (double, double) InterestCalculation(double amountToWithdraw, double balance) {
+            double interest = amountToWithdraw * 0.05;
+            double newBalance = balance - interest;
+            double bankProfit = interest;
+            return (newBalance, bankProfit);
+        }
+
+        static double DepositWithdrawal(double deposit, double balance) {
+            return balance - deposit;
+        }
+
+        public static double CalculateCompoundInterest(double principal, double annualRate, int years) {
+            if (years == 0) {
+                return principal;
+            }
+            double amount = principal + principal * annualRate;
+            return CalculateCompoundInterest(amount, annualRate, years - 1);
+        }
+
+        
+        static void Main(string[] args) {
+            double balance = 100;
+            double bankProfit;
+
+            (balance, bankProfit) = InterestCalculation(50, balance);
+            Console.WriteLine($"Баланс после снятия: {balance}, прибыль банка: {bankProfit}");
+
+            balance = DepositWithdrawal(50, balance);
+            Console.WriteLine($"Баланс после вклада: {balance}");
+
+            string loanStatus = LoanApproval(balance);
+            Console.WriteLine(loanStatus);
+            // Резюме - C# - топ язык :)
+        }
+    }
+}

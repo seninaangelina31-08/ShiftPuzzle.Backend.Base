db8d372fd4b5c2fd44500ff8d21c6f0ff419fb32
Satlykovs <satlykovs@gmail.com>
2024-01-19 17:36:13 +0300

    feat: PracticeC Lesson13 in progress

---
+++ Course/Lesson13/PracticeC/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PracticeC;
+using System;
+using System.Text.Json;
+
+
+[System.Serializable] public class Film
+{
+    public string title { get; set; }
+    public int year { get; set; }
+    public Dictionary<string, string> director { get; set; }
+    public List<Actor> cast { get; set; }
+    public List<string> genres { get; set; }
+    public Rating ratings { get; set; }
+
+}
+
+[System.Serializable] public class Actor
+{
+    public string name { get; set; }
+    public string role { get; set }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}

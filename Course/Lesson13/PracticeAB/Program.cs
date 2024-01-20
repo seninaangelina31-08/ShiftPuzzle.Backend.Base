e2d7747698527760097ba3531474649ed9c872ac
IvanSobolev <i@bezlikiy.ru>
2024-01-20 20:04:06 +0300

    idk2

---
+++ Course/Lesson13/PracticeAB/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PracticeAB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonFromFile = File.ReadAllText("1.json");
+        User user = JsonSerializer.Deserialize<User>(jsonFromFile);
+        if (user != null)
+        {
+            Console.WriteLine($"Name: {user.Name}\nDescription: {user.Description}\nPrice: {user.Price}");
+        }
+    }
+}
+
+[System.Serializable] public class User
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public bool isVerified { get; set; }
+
+    public User() { } // Пустой конструктор для десериализации
+
+    // конструктор класса
+    public User(int id, string name, string email, bool isVerified)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.isVerified = isVerified;
+    }
+}
+

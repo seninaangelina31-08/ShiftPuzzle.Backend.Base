f171f774e4b3dd8191ecd5f15db18451448cfca6
Shadik23 <kurichjev@yandex.ru>
2024-01-22 18:34:36 +0300

    feat(lesson 14): PracticeA

---
+++ Course/Lesson14/PracticeABC/PracticeA2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace PracticeA2
+{
+    public class FactsResponse
+    {
+        public string fact { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            PrintRandomCatFact();
+
+            while (true)
+            {
+                Console.WriteLine("Нажмите клавишу 'R' для получения нового случайного факта или 'Q' для выхода.");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.R)
+                {
+                    Console.WriteLine();
+                    PrintRandomCatFact();
+                }
+                else if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nНеправильный ввод. Пожалуйста, нажмите 'R' или 'Q'.");
+                }
+            }
+        }
+
+        private static void PrintRandomCatFact()
+        {
+            string coindeskURL = "https://catfact.ninja/fact";
+            string jsonFromCoindesk = GetRequest(coindeskURL);
+
+            FactsResponse response = JsonSerializer.Deserialize<FactsResponse>(jsonFromCoindesk);
+
+            string randomCatFact = response.fact;
+            Console.WriteLine("Рандомный факт о кошке: " + randomCatFact);
+        }
+
+        public static string GetRequest(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(dataStream);
+            string jsonResponse = streamReader.ReadToEnd();
+
+            streamReader.Close();
+            response.Close();
+            return jsonResponse;
+        }
+    }
+}
---
+++ Course/Lesson14/PracticeABC/PracticeA3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace PracticeA3
+{
+    public class Joke
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string jokesApiUrl = "https://official-joke-api.appspot.com/random_joke";
+
+            Joke joke = GetRandomJoke(jokesApiUrl);
+
+            Console.WriteLine("Случайная шутка:");
+            Console.WriteLine($"{joke.setup}\n{joke.punchline}");
+
+            AddJokeToFile(joke, "jokes.txt");
+
+            Console.WriteLine("Шутка добавлена в файл 'jokes.txt'.");
+        }
+
+        private static Joke GetRandomJoke(string apiUrl)
+        {
+            string jsonJoke = GetRequest(apiUrl);
+            Joke joke = JsonSerializer.Deserialize<Joke>(jsonJoke);
+            return joke;
+        }
+
+        private static void AddJokeToFile(Joke joke, string filePath)
+        {
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine($"Setup: {joke.setup}");
+
+                    writer.WriteLine($"Punchline: {joke.punchline}");
+
+                    writer.WriteLine(new string('-', 30));
+                }
+            }
+        }
+
+        private static string GetRequest(string url)
+        {
+            {
+                WebRequest request = WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(dataStream);
+                string jsonResponse = streamReader.ReadToEnd();
+
+                streamReader.Close();
+                response.Close();
+                return jsonResponse;
+            }
+        }
+    }
+}

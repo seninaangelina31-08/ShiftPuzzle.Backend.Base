face24014bb710ab9d6c2a74c50f7053d8075193
Vanya Timofeev <ivantimofeev0006@gmail.com>
2024-01-23 19:01:15 +0300

    Feat Lesson10 PracticeA completed

--- Course/Lesson10/PracticeA/Program.cs
+++ Course/Lesson10/PracticeA/Program.cs
@@ -3,7 +3,26 @@ class FileOperations
 {
     static void Main(string[] args)
     { 
+        WriteToFile();
+        ReadFromFileAndPrint();
+    }
 
-        // чтение запись в файл test.txt
+    public static void WriteToFile()
+    {
+        string[] lines = new string[5];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i] = "Vanya";
+        }
+        File.WriteAllLines("name.txt", lines);
+    }
+
+    public static void ReadFromFileAndPrint()
+    {
+        string[] lines = File.ReadAllLines("name.txt");
+        foreach(string line in lines)
+        {
+            Console.WriteLine(line);
+        }
     }
 }

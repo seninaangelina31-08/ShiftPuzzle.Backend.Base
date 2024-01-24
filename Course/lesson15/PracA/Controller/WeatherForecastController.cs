aa149b065a6474d66c8d57988edf29a92855469a
Ivanova Daria <ida30062006@gmail.com>
2024-01-24 19:43:39 +0300

    feat: les15 pracB

--- Course/lesson15/PracA/Controller/WeatherForecastController.cs
+++ Course/lesson15/PracA/Controller/WeatherForecastController.cs
@@ -1,32 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace PracA;
+namespace PracABC;
 
-
+[Route("store/")]
 [ApiController] 
 public class StoreController : ControllerBase
 {
-    public List<string> shop_list = new List<string>{"potato", "bread", "milk"};
+    public List<Prod> shop_list = new List<Prod>{new Prod { name="milk", cost = 79, in = true}};
 
     [HttpGet("add-product")]
     public string add(string product)
     {
         shop_list.Add(product);
         return shop_list[4];
+        return "OK";
     }
 
     [HttpGet("delete-product")]
-    public string delete(string product)
+    public string delete(Prod product)
     {
         shop_list.Remove(product);
-        return shop_list[-1];
+        return "OK";
     }
 
     [HttpGet("view-product")]
-    public List<string> view()
+    public List<Prod> view()
     {
         return shop_list;
     }
 }
 
+[System.Serializable] public class Prod
+{
+    public string name { get; set; }
+    public int cost { get; set; }
+    public bool in { get; set; }
+    
+    
+    public Prod() { }
+    
+    public Prod(string name, int cost, bool in)
+    {
+        this.name = name;
+        this.cost = cost;
+        this.in = in;
+    }
+}

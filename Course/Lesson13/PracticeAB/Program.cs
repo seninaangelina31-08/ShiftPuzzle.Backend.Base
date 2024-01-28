7c1884616a14dfa40f8d28f3111d3db480d5a146
Nazelko <nazelko228@gmail.com>
2024-01-28 20:44:45 +0300

    feat: added answer to task 13

---
+++ Course/Lesson13/PracticeAB/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace PracticeAB;
+using System.Collections.Generic; // библиотека для работы с List и Dictionary
+
+using System.Text.Json; 
+//*ЧастьА:* 
+//1. Выведи почту и ID пользователя Иванов Иван
+//2. Выводи список товаров которая купила Анна Петрова и сделай скидку 10%
+//3. Выведи средний балл Сергея Сидорова
+//*ЧастьБ:*
+//4. Добавь в список покупок Анны салфетки для мониторв за 1550р и сделай к итогу скидку 2%. и Запиши  в файл.
+//5. Добавь книгу любую Пушкина и запиши в файл
+
+
+[System.Serializable] public class Anna
+{
+    // поля класса
+    public string orderId { get; set; }
+    public string customerName { get; set; }
+    public int totalPrice { get; set; }
+    public List<string> items { get; set; }
+
+    public Anna() { } // Пустой конструктор для десериализации
+
+   
+}
+[System.Serializable] public class Product
+{
+    // поля класса
+    public int id { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public bool isVerified { get; set; }
+
+    public Product() { } // Пустой конструктор для десериализации
+
+   
+}
+[System.Serializable] public class Library
+{
+    public string libraryName { get; set; }
+    public List<Book> books { get; set; }
+
+    public class Book
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public int year { get; set; }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       
+
+        const string path = "1.json";
+        const string path2 = "2.json";
+        const string path3 = "6.json";
+        const string path5 = "5.json";
+
+        // Десериализация из JSON
+        string js = File.ReadAllText(path);
+        Product info = JsonSerializer.Deserialize<Product>(js);
+
+        string js2 = File.ReadAllText(path2);
+        Anna inf2 = JsonSerializer.Deserialize<Anna>(js2);
+
+        string js3 = File.ReadAllText(path3);
+        Anna inf3 = JsonSerializer.Deserialize<Anna>(js3);
+
+        string js4 = File.ReadAllText(path5);
+        Library inf4 = JsonSerializer.Deserialize<Library>(js4);
+
+       
+
+        if (info != null)
+        {
+            // вывод полей объекта
+            Console.WriteLine($"Name: {info.name}\nEmail: {info.email}");
+        
+           
+        }
+        if (inf2!=null)
+        {
+            Console.WriteLine("Список продуктов:");
+            foreach (string prd in inf2.items)
+            {
+                Console.WriteLine(prd);
+            }
+            Console.WriteLine($"Цена покупки со скидкой:{inf2.totalPrice*0.9}");
+        }
+        // Добавляем значение "книга" в массив "items"
+        inf3.items.Add("салфетки для монитора");
+
+        
+        inf3.totalPrice = 7150;
+        Console.WriteLine("Список продуктов обновленный:");
+        // Сериализуем обновленный объект в JSON
+        string updatedJson = JsonSerializer.Serialize(inf3);
+        foreach (string p in inf3.items)
+        {
+            Console.WriteLine(p);
+        }
+        Console.WriteLine($"Цена покупки со скидкой: {inf3.totalPrice * 0.98}");
+
+
+        Library.Book newlib = new  Library.Book { title = "Руслан и Людмила", author = "А. С.Пушкин", year= 1820 };
+
+        inf4.books.Add(newlib);
+
+        string lbsb = JsonSerializer.Serialize(inf4);
+
+        
+    }
+}
---
+++ Course/Lesson13/PracticeC/Program.cs
@@ -0,0 +1,155 @@
+﻿//*Часть С:*
+//1. Добавь 3 фильма из кинопоиска и сохрани в файл 
+//2. Добавь должность программиста с ЗП 250.000. 
+//Создай новый JSON который хранит количество работников по каждой специальности. Запись в файл должно прлоисходить только через приложение.
+//И сделай анализ по ЗП в компании - нужно вывести среднюю ЗП.
+//3. Напиши систему которая будет менять статус и добавлять новых сотрудников
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace PracticeC;
+    public class Movie
+    {
+        public string title { get; set; }
+        public int year { get; set; }
+        public Director director { get; set; }
+        public List<CastMember> cast { get; set; }
+        public List<string> genres { get; set; }
+        public Ratings ratings { get; set; }
+
+        public class Director
+        {
+            public string name { get; set; }
+            public DateTime born { get; set; }
+        }
+
+        public class CastMember
+        {
+            public string name { get; set; }
+            public string role { get; set; }
+        }
+
+        public class Ratings
+        {
+            public double IMDb { get; set; }
+            public string RottenTomatoes { get; set; }
+        }
+
+        public static Movie FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Movie>(json);
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public void AddCastMember(string name, string role)
+        {
+            if (cast == null)
+            {
+                cast = new List<CastMember>();
+            }
+            cast.Add(new CastMember { name = name, role = role });
+        }
+    }
+
+    [System.Serializable]public class Company
+    {
+        public string companyName { get; set; }
+        public List<Employee> employees { get; set; }
+        public Location location { get; set; }
+
+        public class Employee
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string position { get; set; }
+            public List<string> skills { get; set; }
+            public int salary { get; set; }
+        }
+
+        public class Location
+        {
+            public string city { get; set; }
+            public string address { get; set; }
+        }
+
+        public static Company FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Company>(json);
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public void AddEmployee(int id, string name, string position, List<string> skills, int salary)
+        {
+            if (employees == null)
+            {
+                employees = new List<Employee>();
+            }
+            employees.Add(new Employee
+            {
+                id = id,
+                name = name,
+                position = position,
+                skills = skills,
+                salary = salary
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Добавь 3 фильма из кинопоиска и сохрани в файл 
+            //2. Добавь должность программиста с ЗП 250.000. 
+
+            //Создай новый JSON который хранит количество работников по каждой специальности. Запись в файл должно происходить только через приложение.
+
+            //И сделай анализ по ЗП в компании - нужно вывести среднюю ЗП.
+
+            //3. Напиши систему которая будет менять статус и добавлять новых сотрудников
+            const string path = "2.json";
+            string js = File.ReadAllText(path);
+            Company company = JsonSerializer.Deserialize<Company>(js);
+
+            company.AddEmployee(464, "Леша Алексеев", "Программист", new List<string> { "Программирует программы" }, 250000);
+            foreach (var employee in company.employees)
+            {
+                Console.WriteLine($"ID: {employee.id}, Name: {employee.name}, Position: {employee.position}, Skills: {string.Join(", ", employee.skills)}, Salary: {employee.salary}");
+            }
+            double zap = 0;
+            foreach (var employee in company.employees)
+                {
+                    zap += employee.salary;
+                }
+            double srzap = zap / company.employees.Count;
+
+            Console.WriteLine($"Средняя зарплата в компании: {srzap}");
+            // Подсчет количества работников по каждой специальности
+            Dictionary<string, int> podschet = new Dictionary<string, int>();
+            foreach (var employee in company.employees)
+            {
+                if (podschet.ContainsKey(employee.position))
+                {
+                    podschet[employee.position]++;
+                }
+                else
+                {
+                    podschet[employee.position] = 1;
+                }
+            }
+            
+            string emplcount = JsonSerializer.Serialize(podschet);
+            File.WriteAllText("emplcount.json", emplcount);
+
+        }
+    }

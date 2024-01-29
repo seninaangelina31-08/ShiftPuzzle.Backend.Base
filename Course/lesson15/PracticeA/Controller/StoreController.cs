89fe9cd95897e67c626b992bac90f93776d0358d
Frain <89194392174pp@gmail.com>
2024-01-29 16:47:33 +0300

    feature(lesson15): practice C complete

--- Course/lesson15/PracticeA/Controller/StoreController.cs
+++ Course/lesson15/PracticeA/Controller/StoreController.cs
@@ -52,5 +52,68 @@ public class StoreController : ControllerBase
         string json = json = JsonSerializer.Serialize(Products);
         return Ok(json);
     }
+
+    [HttpGet]
+    [Route("store/change_price")]
+    public IActionResult Change_price(string name, int new_price)
+    {
+        foreach (Product ar in Products)
+        {
+            if (ar.name == name)
+            {   
+                Products.Remove(ar);
+                Product a = new Product(ar.name, new_price, ar.haveInStore);
+                Products.Add(a);
+                string json = JsonSerializer.Serialize(a);
+                return Ok(json);
+            } else
+            {
+                continue;
+            }
+        }
+        return NotFound();
+    }
+
+    [HttpGet]
+    [Route("store/change_name")]
+    public IActionResult Change_name(string name, string new_name)
+    {
+        foreach (Product ar in Products)
+        {
+            if (ar.name == name)
+            {   
+                Products.Remove(ar);
+                Product a = new Product(new_name, ar.price, ar.haveInStore);
+                Products.Add(a);
+                string json = JsonSerializer.Serialize(a);
+                return Ok(json);
+            } else
+            {
+                continue;
+            }
+        }
+        return NotFound();
+    }
+
+
+    [HttpGet]
+    [Route("store/viewno")]
+    public IActionResult View_no()
+    {
+        List<string> lst = new List<string>{};
+
+        foreach (Product ar in Products)
+        {
+            if (ar.haveInStore == false)
+            {   
+                lst.Add(ar.name);
+            } else
+            {
+                continue;
+            }
+        }
+        string json = JsonSerializer.Serialize(lst);
+        return Ok(json);
+    }
 }
 

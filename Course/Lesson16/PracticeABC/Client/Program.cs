1686fbcddee113babd1eeec275b9dd4f796ae8c2
DariaN <m.dzino@ya.ru>
2024-01-31 12:07:46 +0300

    feat L16 PrABC

--- Course/Lesson16/PracticeABC/Client/Program.cs
+++ Course/Lesson16/PracticeABC/Client/Program.cs
@@ -2,12 +2,79 @@
 using System.Text.Json;
 
 namespace Client;
-
+ 
 class Program
 {
+
+    public class User
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        public bool isAuthorized { get; set; }
+
+        public User(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+            this.isAuthorized = false;
+        }
+    }
+
+    static void CreateDBUser(User user)
+    {
+        string url = "http://localhost:5087/store/add_user";
+        var user_send = new 
+        {
+            name = user.name,
+            password = user.password,
+            isAuthorized = false,
+        };
+
+        var client = new HttpClient();
+        var json = JsonSerializer.Serialize(user_send);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = client.PostAsync(url, content).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseContent);
+        }
+        else{
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+    }
+    static void Logining(User user)
+    {
+        string url = "http://localhost:5087/store/login";
+        var user_send = new 
+        {
+            name = user.name,
+            password = user.password,
+        };
+
+        var client = new HttpClient();
+        var json = JsonSerializer.Serialize(user_send);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = client.PostAsync(url, content).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            user.isAuthorized = true;
+            Console.WriteLine(responseContent);
+        }
+        else{
+            Console.WriteLine($"Error: {response.StatusCode}; {response.Content.ReadAsStringAsync().Result}");
+        }
+    }
     
     static void Main(string[] args)
     { 
-
+        User user1 = new User("Vasya", "123");
+        CreateDBUser(user1);
+        Logining(user1);
     }
+    
 }
--- Course/Lesson16/PracticeABC/Server/Controllers/StoreController.cs
+++ Course/Lesson16/PracticeABC/Server/Controllers/StoreController.cs
@@ -1,112 +1,141 @@
-namespace PracticeA;
-
-using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-public class StoreController : ControllerBase
+namespace L16
 {
-    public class Product
+    using Microsoft.AspNetCore.Mvc;
+    [ApiController]
+    public class StoreController : ControllerBase
     {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
-
-        public Product(string name, double price, int stock)
+        public class Product
         {
-            Name = name;
-            Price = price;
-            Stock = stock;
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public int Stock { get; set; }
+
+            public Product(string name, double price, int stock)
+            {
+                Name = name;
+                Price = price;
+                Stock = stock;
+            }
+        }
+
+
+        public class User
+        {
+            public string name { get; set; }
+            public double password { get; set; }
+            public bool isAuthorized { get; set; }
+
+            public User() { }
+            public User(string name, double password, bool isAuthorized)
+            {
+                this.name = name;
+                this.password = password;
+                this.isAuthorized = false;
+            }
+        }
+        private static readonly List<Product> Items = new List<Product>();
+        private static readonly List<User> Users = new List<User>();
+
+        [HttpGet]
+        [Route("/store/updateprice")]
+        public IActionResult UpdatePrice(string name, double newPrice)
+        {
+            var product = Items.FirstOrDefault(p => p.Name == name);
+            if (product != null)
+            {
+                product.Price = newPrice;
+                return Ok($"{name} - новая цена {newPrice}");
+            }
+            else
+            {
+                return NotFound($" {name} не найден");
+            }
+        }
+        // PracticeA
+        [HttpPost]
+        [Route("/store/add")]
+        public IActionResult Add([FromBody] Product newProduct)
+        {
+            Items.Add(newProduct);
+            return Ok(Items);
+        }
+
+        [HttpPost]
+        [Route("/store/delete")]
+        public IActionResult Delete(string name)
+        {
+            foreach (var prod in Items)
+            {
+                if (prod.Name == name)
+                {
+                    Items.Remove(prod);
+                    return Ok(Items);
+                }
+            }
+            return NotFound("Продукт не найден");
+        }
+        [HttpGet]
+        [Route("/store/show")]
+        public IActionResult Show()
+        {
+            return Ok(Items);
+        }
+        // PracticeB
+        [HttpPost]
+        [Route("/store/add_user")]
+        public IActionResult AddUser([FromBody] User user)
+        {
+            Users.Add(user);
+            return Ok($"{user.name} был добавлен");
+        }
+
+        [HttpPost]
+        [Route("/store/login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            foreach (var us in Users)
+            {
+                if (us.name == user.name)
+                {
+                    if (us.password == user.password)
+                    {
+                        user.isAuthorized = true;
+                        return Ok("Всё кус!!!");
+                    }
+                }
+            }
+            return NotFound("Что-то не так");
+        }
+
+        //PracticeC
+        [HttpPost]
+        [Route("/store/updatename")]
+        public IActionResult UpdateName(string currentName, string newName)
+        {
+            foreach (var prod in Items)
+            {
+                if (prod.Name == currentName)
+                {
+                    prod.Name = newName;
+                    return Ok(Items);
+                }
+            }
+            return NotFound("Продукт не найден");
+        }
+
+        [HttpGet]
+        [Route("/store/outofstock")]
+        public IActionResult OutOfStock()
+        {
+            var outOfStockItems = Items.Where(p => p.Stock == 0).ToList();
+            if (outOfStockItems.Any())
+            {
+                return Ok(outOfStockItems);
+            }
+            else
+            {
+                return Ok("Всё в наличии");
+            }
         }
     }
-
-    private static readonly List<Product> Items = new List<Product>();
-
-    [HttpGet]
-    [Route("/store/updateprice")]
-    public IActionResult UpdatePrice(string name, double newPrice)
-    {
-        var product = Items.FirstOrDefault(p => p.Name == name);
-        if (product != null)
-        {
-            product.Price = newPrice;
-            return Ok($"{name} обновлен с новой ценой: {newPrice}");
-        }
-        else
-        {
-            return NotFound($"Продукт {name} не найден");
-        }
-    }
-
-    [HttpGet]
-    [Route("/store/updatename")]
-    public IActionResult UpdateName(string currentName, string newName)
-    {
-        var product = Items.FirstOrDefault(p => p.Name == currentName);
-        if (product != null)
-        {
-            product.Name = newName;
-            return Ok($"Имя продукта изменено с {currentName} на {newName}");
-        }
-        else
-        {
-            return NotFound($"Продукт {currentName} не найден");
-        }
-    }
-
-
-    [HttpGet]
-    [Route("/store/outofstock")]
-    public IActionResult OutOfStock()
-    {
-        var outOfStockItems = Items.Where(p => p.Stock == 0).ToList();
-        if (outOfStockItems.Any())
-        {
-            return Ok(outOfStockItems);
-        }
-        else
-        {
-            return Ok("Все продукты в наличии");
-        }
-    }
-
-
-
-
-
-
-    [HttpGet]
-    [Route("/store/add")]
-    public IActionResult Add(string name, double price, int stock)
-    {
-        var product = new Product(name, price, stock);
-        Items.Add(product);
-        return Ok(Items);
-    }
-
-
-    [HttpGet]
-    [Route("/store/delete")]
-    public IActionResult Delete(string name)
-    {
-        var product = Items.FirstOrDefault(p => p.Name == name);
-        if (product != null)
-        {
-            Items.Remove(product);
-            return Ok($"{name} удален");
-        }
-        else
-        {
-            return NotFound($"{name} не найден");
-        }
-    }
-
-
-    [HttpGet]
-    [Route("/store/show")]
-    public IActionResult Show()
-    {
-        return Ok(Items);
-    }
-
-
 }

4d8b602c556f74cb6c5c5a90eb8f35890e542fdf
Satlykovs <satlykovs@gmail.com>
2024-02-01 21:34:20 +0300

    feat: Practices ABC finished Lesson 17

--- Course/Lesson17/PracticeABC/Client/Program.cs
+++ Course/Lesson17/PracticeABC/Client/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json;
-
+using System;
+using System.Net;
+using System.IO;
 namespace Client;
 
 class Program
@@ -24,15 +26,29 @@ class Program
     
     static bool IsAuthorized = false;
 
+    public static string Request(string url)
+    {
+        WebRequest webrequest = WebRequest.Create(url);
+        WebResponse response = webrequest.GetResponse();
+        Stream stream = response.GetResponseStream();
+        StreamReader streamReader = new StreamReader(stream);
+        string json_string = streamReader.ReadToEnd();
+        streamReader.Close();
+        response.Close();
+        return json_string;
+    }
     static void DisplayProducts()
         {
-            var url = "http://localhost:5087/store/show"; // Замените на порт вашего сервера
-            
-            // реализуй логику
+            var url = "http://localhost:5087/store/show";
 
 
             Console.WriteLine("-----------------------------------------------------------------");
             Console.WriteLine("| Название продукта | Цена | Количество на складе |"); 
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(Request(url));
+            foreach (var product in products)
+            {
+                Console.WriteLine($"| {product.name, -17} | {product.price, -4} | {product.stock, -19} |");
+            }             
 
 
 
@@ -84,7 +100,7 @@ class Program
     {       
         
         
-            var url = "http://localhost:5087/store/????"; // Замените на порт вашего сервера, также замените символы на правильный апи
+            var url = "http://localhost:5087/store/auth"; // Замените на порт вашего сервера, также замените символы на правильный апи
             var userData = new
             {
                 User = "admin",
@@ -108,20 +124,111 @@ class Program
                 IsAuthorized = false;
             }
     }
+    
+    public static void UpdatePrice()
+    {   
+        Console.Write("Введите имя продукта: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите новую цену продукта: ");
+        double new_price = Convert.ToDouble(Console.ReadLine());
+        var url = "http://localhost:5087/store/updateprice";
+        var client = new HttpClient();
 
+        var data = new 
+        {
+            Name = name,
+            Price = new_price,
+            Stock = 0
+        };
+        var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+
+        var response = client.PostAsync(url, content).Result;
+        if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseContent);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+    }
+
+    public static void UpdateName()
+    {   
+        Console.Write("Введите текущее имя продукта: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите новое имя продукта: ");
+        string new_name = Console.ReadLine();
+        var url = "http://localhost:5087/store/updatename";
+        var client = new HttpClient();
+
+        var data = new 
+        {
+            Name = name,
+            NewName = new_name
+        };
+        var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+
+        var response = client.PostAsync(url, content).Result;
+        if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseContent);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+    }
 
     static void Main(string[] args)
-    { 
+    {   bool online = true;
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        while (true)
+        while (online)
                 {
                     Console.WriteLine("Выберите опцию:");
+                    Console.WriteLine("1.Авторизация\n2.Добавление продукта\n3.Вывод списка\n4.Выход");
+                    Console.WriteLine("5.Обновление цены\n6.Обновление имени");
                      
 
                     var choice = Console.ReadLine();
 
                     switch (choice)
                     { 
+                        case "1":
+                        {
+                            Auth();
+                            break;
+                        }
+                        case "2":
+                        {
+                            SendProduct();
+                            break;
+                        }
+                        case "3":
+                        {
+                            DisplayProducts();
+                            break;
+                        }
+                        case "4":
+                        {
+                            online = false;
+                            IsAuthorized = false;
+                            break;
+                        }
+                        case "5":
+                        {
+                            UpdatePrice();
+                            break;
+                        }
+                        case "6":
+                        {
+                            UpdateName();
+                            break;
+                        }
                         default:
                             Console.WriteLine("Неверный выбор. Попробуйте снова.");
                             break;

a365f1a44468e91d6eb392513f3e214d02105d4e
Angelina <lina.senina.07@mail.ru>
2024-02-05 20:10:29 +0300

    hw14

---
+++ Course/Lesson14/Homework/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Homework;
+using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        string apiUrl = "https://api.publicapis.org/entries";
+        string apiData = FromApi(apiUrl);
+
+        if (apiData != null)
+        {
+
+            ApiResponse response = JsonSerializer.Deserialize<ApiResponse>(apiData);
+
+            if (response != null)
+            {
+
+                WritToFile(response, "FREE_API.txt");
+                Console.WriteLine("URL-ссылки  записаны в файл.");
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    static string FromApi(string url)
+    {
+
+        using (WebClient client = new WebClient())
+        {
+            return client.DownloadString(url);
+        }
+    }
+
+    static void WritToFile(ApiResponse response, string filePath)
+    {
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var entry in response.Entries)
+                {
+                    if (entry.Link.Contains("github.com"))
+                    {
+                        writer.WriteLine(entry.Link);
+                    }
+                }
+            }
+        
+        
+    }
+}
+
+class ApiResponse
+{
+    public ApiEntry[] Entries { get; set; }
+}
+
+class ApiEntry
+{
+    public string Link { get; set; }
+}

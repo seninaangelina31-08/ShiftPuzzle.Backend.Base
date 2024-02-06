8575fc1bfe908f1cb00e03fe780516ca1e085297
Ivanova Daria <ida30062006@gmail.com>
2024-02-06 18:31:48 +0300

    feat: prA les19

---
+++ Course/Lesson19/PracticeABC/Server/Models/UserCredentials.cs
@@ -0,0 +1,10 @@
+public class UserCredentials
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string User { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Pass { get; set; }
+
+    }

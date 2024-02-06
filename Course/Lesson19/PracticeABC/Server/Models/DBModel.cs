62f48a05717b004c1a5c01e88c794964d6819b46
Юрий <zaretskiy.y.a@gmail.com>
2024-02-06 18:52:24 +0300

    feat lesson 19 B

---
+++ Course/Lesson19/PracticeABC/Server/Models/DBModel.cs
@@ -0,0 +1,52 @@
+namespace PracticeA
+
+public class DBModel
+{
+    private string _jsonFilePath;
+    public DBModel(string jsonfilepath)
+    {
+        _jsonFilePath = jsonfilepath;
+        ReadDataFromFile();
+    }
+    private List<Product> ConvertTextDBToList(string json)
+    {
+        return JsonSerializer.Deserialize<List<Product>>(json)
+    }
+
+    private string ReadDB()
+    {
+        return System.IO.File.ReadAllText(_jsonFilePath);
+    }
+
+    private bool DBExist()
+    {
+        return System.IO.File.Exists(_jsonFilePath);
+    }
+
+    private void ReadDataFromFile()
+    {
+        if (DBExist())
+        { 
+            Items =  ConvertTextDBToList(ReadDB());
+        }
+    }
+
+    #endregion
+ 
+
+    private string  ConvertDBtoJson()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        retunr JsonSerializer.Serialize(Items, options);
+    }
+
+    private void WriteTiDB(string json)
+    {
+        System.IO.File.WriteAllText(_jsonFilePath, json);
+    }
+
+    private void WriteDataToFile()
+    { 
+        WriteTiDB(ConvertDBtoJson());
+    }
+}

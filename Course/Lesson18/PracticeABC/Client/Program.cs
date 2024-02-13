8c92b7798883b079021b3e1103306c74b2cdb5cf
Nikita Bykov <nikita.bykov.322@gmail.com>
2024-02-13 17:56:02 +0300

    refact: Lesson18 PracticeC

--- Course/Lesson18/PracticeABC/Client/Program.cs
+++ Course/Lesson18/PracticeABC/Client/Program.cs
@@ -6,89 +6,90 @@ namespace Client;
 
 class Program
 { 
+
+    private const string BaseUrl = "http://localhost";
+    private const string Port = "5087";
+    private const string AuthMethod = "/store/auth";
+    private const string AddProductMethod = "/store/add";
+    private const string ShowProductsMethod = "/store/show";
+
     [System.Serializable]
     public class Product
     {
-    [Required]
-    [StringLength(100, MinimumLength = 3)]
-    public string name { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string name { get; set; }
 
-    [Range(0.01, 10000)]
-    public double price { get; set; }
+        [Range(0.01, 10000)]
+        public double price { get; set; }
 
-    [Range(0, 10000)]
-    public int stock { get; set; }
-
-        
+        [Range(0, 10000)]
+        public int stock { get; set; }
     }
     
     static bool IsAuthorized = false;
+    static readonly HttpClient client = new HttpClient(); 
 
     static void DisplayProducts()
+    {
+        var url = $"{BaseUrl}:{Port}{ShowProductsMethod}"; // Замените на порт вашего сервера
+        var response = client.GetAsync(url).Result;  
+        string responseContent = response.Content.ReadAsStringAsync().Result; 
+        List<Product> products = JsonSerializer.Deserialize<List<Product>>(responseContent); 
+        Console.WriteLine("-----------------------------------------------------------------");
+        Console.WriteLine("| Название продукта | Цена | Количество на складе |");
+        Console.WriteLine("-----------------------------------------------------------------");
+
+        foreach (var product in products)
         {
-            var url = "http://localhost:5087/store/show"; // Замените на порт вашего сервера
-            var client = new HttpClient();   
-            var response = client.GetAsync(url).Result;  
-            string responseContent = response.Content.ReadAsStringAsync().Result; 
-            List<Product> products = JsonSerializer.Deserialize<List<Product>>(responseContent); 
-            Console.WriteLine("-----------------------------------------------------------------");
-            Console.WriteLine("| Название продукта | Цена | Количество на складе |");
-            Console.WriteLine("-----------------------------------------------------------------");
-
-            foreach (var product in products)
-            {
-                Console.WriteLine($"| {product.name, -18} | {product.price, -5} | {product.stock, -19} |");
-            }
-
-            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine($"| {product.name, -18} | {product.price, -5} | {product.stock, -19} |");
         }
+
+        Console.WriteLine("-----------------------------------------------------------------");
+    }
 
 
     public static void SendProduct()
     {       
+        if(!IsAuthorized)
+        {
+            Console.WriteLine("Вы не авторизованы");
+            return;        
+        }
+    
+        var url = $"{BaseUrl}:{Port}{AddProductMethod}"; // Замените на порт вашего сервера
+        Console.WriteLine("Введите название продукта:");
+        var name = Console.ReadLine();
+        Console.WriteLine("Введите цену продукта:");
+        var price = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите количество на складе:");
+        var stock = int.Parse(Console.ReadLine());
 
+        var product = new Product
+        {
+            Name = name,
+            Price = price,
+            Stock = stock
+        };
 
+        var json = JsonSerializer.Serialize(product);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            if(!IsAuthorized)
-            {
-                Console.WriteLine("Вы не авторизованы");
-                return;        
-            }
-        
-            var url = "http://localhost:5087/store/add"; // Замените на порт вашего сервера
-            Console.WriteLine("Введите название продукта:");
-            var name = Console.ReadLine();
-            Console.WriteLine("Введите цену продукта:");
-            var price = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите количество на складе:");
-            var stock = int.Parse(Console.ReadLine());
-
-            var product = new
-            {
-                Name = name,
-                Price = price,
-                Stock = stock
-            };
-
-            var client = new HttpClient(); 
-            var json = JsonSerializer.Serialize(product);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var response = client.PostAsync(url, content).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(responseContent);
-            }
-            else
-            {
-                Console.WriteLine($"Error: {response.StatusCode}");
-            }
+        var response = client.PostAsync(url, content).Result;
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseContent);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
     }
 
 
     public static void Auth()
-    {       var url = "http://localhost:5087/store/auth"; // Замените на порт вашего сервера
+    {       var url = $"{BaseUrl}:{Port}{AuthMethod}"; // Замените на порт вашего сервера
             var userData = new
             {
                 User = "admin",

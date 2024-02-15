e9aeb7a72366ea5a044be5ddf562cc10fc43b069
Ivanova Daria <ida30062006@gmail.com>
2024-02-15 18:06:26 +0300

    feat les22 prAB

--- Course/Lesson22/PracticeABC/Server/Model/Product.cs
+++ Course/Lesson22/PracticeABC/Server/Model/Product.cs
@@ -2,13 +2,14 @@ namespace PracticeABC;
 using System;
 using System.ComponentModel.DataAnnotations;
 
+
 public class Product
 {
     [Required]
     [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; }
 
-    [Range(0.01, 10000)]
+    [Range(0.01, 100)]
     public double Price { get; set; }
 
     [Range(0, 10000)]

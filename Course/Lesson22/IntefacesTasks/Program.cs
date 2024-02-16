c9259bc1883db73c7492172bbd1b492eb1425d17
Nikita Bykov <nikita.bykov.322@gmail.com>
2024-02-16 19:27:56 +0300

    feat: Lesson22

--- Course/Lesson22/IntefacesTasks/Program.cs
+++ Course/Lesson22/IntefacesTasks/Program.cs
@@ -5,28 +5,27 @@
 // Шуточный интерфейс "Способность танцевать"
 interface IDanceable
 {
- 
+    public void Dance();
 }
 
 // Шуточный интерфейс "Способность петь"
 interface ISingable
 {
- 
+    public void Sing();
 }
-
 
 internal interface ITalkable
 {
-  
+    public void Talk();
 }
 
 internal interface IJokeable
 {
-     
+    public void TellJoke();
 }
 
 // Класс шутника, реализующий ITalkable, IJokeable, IDanceable и ISingable
-class Joker  
+class Joker : ITalkable, IJokeable, IDanceable, ISingable
 {
     public void Talk()
     {
@@ -51,7 +50,7 @@ class Joker
 
 
 // Класс клоуна, реализующий ITalkable и IJokeable
-class Clown  
+class Clown : ITalkable, IJokeable
 {
     public void Talk()
     {
@@ -69,10 +68,18 @@ class Program
     static void Main(string[] args)
     {
         // Создаем экземпляры шутника и клоуна 
-
+        IDanceable danceJoker = new Joker();
+        ISingable singJoker = new Joker();
+        IJokeable tellJokeJoker = new Joker();
+        ITalkable talkJoker = new Joker();
+        ITalkable talkClown = new Clown();
+        IJokeable tellJokeClown0 = new Clown();
         // Используем методы через интерфейсы
-         
-
-        
+        danceJoker.Dance();
+        singJoker.Sing();
+        tellJokeJoker.TellJoke();
+        talkJoker.Talk();
+        talkClown.Talk();
+        tellJokeClown0.TellJoke();
     }
 }

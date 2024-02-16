ad8a473e16161be9a05510c569fffc7bf8405c1c
Satlykovs <satlykovs@gmail.com>
2024-02-16 18:15:10 +0300

    feat: Practice ABC finished Lesson22

--- Course/Lesson22/IntefacesTasks/Program.cs
+++ Course/Lesson22/IntefacesTasks/Program.cs
@@ -5,45 +5,45 @@
 // Шуточный интерфейс "Способность танцевать"
 interface IDanceable
 {
- 
+ public void CanDance();
 }
 
 // Шуточный интерфейс "Способность петь"
 interface ISingable
 {
- 
+    public void CanSing();
 }
 
 
 internal interface ITalkable
 {
-  
+    public void CanTalk();
 }
 
 internal interface IJokeable
 {
-     
+     public void CanJoke();
 }
 
 // Класс шутника, реализующий ITalkable, IJokeable, IDanceable и ISingable
-class Joker  
+class Joker : ITalkable, IJokeable, IDanceable, ISingable
 {
-    public void Talk()
+    public void CanTalk()
     {
         Console.WriteLine("Привет! Я мастер шуток и разговоров.");
     }
 
-    public void TellJoke()
+    public void CanJoke()
     {
         Console.WriteLine("Почему ученые не доверяют атомам? Потому что они составляют все!");
     }
 
-    public void Dance()
+    public void CanDance()
     {
         Console.WriteLine("Я танцую, как будто никто не видит!");
     }
 
-    public void Sing()
+    public void CanSing()
     {
         Console.WriteLine("Ла ла ла! Я певчая сенсация!");
     }
@@ -51,14 +51,14 @@ class Joker
 
 
 // Класс клоуна, реализующий ITalkable и IJokeable
-class Clown  
+class Clown  : ITalkable, IJokeable
 {
-    public void Talk()
+    public void CanTalk()
     {
         Console.WriteLine("Привет! Я здесь, чтобы заставить вас улыбнуться.");
     }
 
-    public void TellJoke()
+    public void CanJoke()
     {
         Console.WriteLine("Почему пугало получило награду? Потому что оно было выдающимся на своем поле!");
     }
@@ -69,9 +69,21 @@ class Program
     static void Main(string[] args)
     {
         // Создаем экземпляры шутника и клоуна 
+        IDanceable jokerDance = new Joker();
+        ITalkable jokerTalk = new Joker();
+        ISingable jokerSing = new Joker();
+        IJokeable jokerJoke = new Joker();
 
+        ITalkable clownTalk = new Clown();
+        IJokeable clownJoke = new Clown();
         // Используем методы через интерфейсы
-         
+        jokerDance.CanDance();
+        jokerTalk.CanTalk();
+        jokerSing.CanSing();
+        jokerJoke.CanJoke();
+
+        clownTalk.CanTalk();
+        clownJoke.CanJoke(); 
 
         
     }

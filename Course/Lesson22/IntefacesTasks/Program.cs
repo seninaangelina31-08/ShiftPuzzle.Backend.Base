d782f072244d495ee60bf4633b26d5a4cea8661b
venimu <ven1mu@yandex.ru>
2024-02-16 18:00:45 +0300

    Lesson 22; Practice ABC; 	modified:   Course/Lesson22/IntefacesTasks/Program.cs 	modified:   Course/Lesson22/PracticeABC/Server/DataBase.db

--- Course/Lesson22/IntefacesTasks/Program.cs
+++ Course/Lesson22/IntefacesTasks/Program.cs
@@ -5,28 +5,28 @@
 // Шуточный интерфейс "Способность танцевать"
 interface IDanceable
 {
- 
+    public void Dance();
 }
 
 // Шуточный интерфейс "Способность петь"
 interface ISingable
 {
- 
+    public void Sing();
 }
 
 
 internal interface ITalkable
 {
-  
+    public void Talk();
 }
 
 internal interface IJokeable
 {
-     
+    public void TellJoke();
 }
 
 // Класс шутника, реализующий ITalkable, IJokeable, IDanceable и ISingable
-class Joker  
+class Joker : ITalkable, IJokeable, IDanceable, ISingable
 {
     public void Talk()
     {
@@ -51,7 +51,7 @@ class Joker
 
 
 // Класс клоуна, реализующий ITalkable и IJokeable
-class Clown  
+class Clown: ITalkable, IJokeable  
 {
     public void Talk()
     {
@@ -69,8 +69,11 @@ class Program
     static void Main(string[] args)
     {
         // Создаем экземпляры шутника и клоуна 
-
+        Joker j = new Joker();
+        Clown c = new Clown();
         // Используем методы через интерфейсы
+        j.Dance();
+        c.Talk();
          
 
         

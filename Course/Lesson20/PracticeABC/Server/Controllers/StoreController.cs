22548dec0e1807c606b3fe59725dcceb114d4de1
DariaN <m.dzino@ya.ru>
2024-02-18 16:09:22 +0300

    feat: L20 PrABC

--- Course/Lesson20/PracticeABC/Server/Controllers/StoreController.cs
+++ Course/Lesson20/PracticeABC/Server/Controllers/StoreController.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
-using System.IO; 
+using System.IO;
 using System.Net.Http;
-using System.Text; 
+using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
@@ -14,107 +14,105 @@ using System.Collections.Generic;
 [ApiController]
 public class StoreController : ControllerBase
 {
-    
+
     private readonly ProductRepository _productRepository;
 
-        public StoreController(ProductRepository productRepository)
-        {
-            _productRepository = productRepository;
-        }
+    public StoreController(ProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
 
-        [HttpPost]
-        [Route("/store/updateprice")]
-        public IActionResult UpdatePrice(string name, double newPrice)
+    [HttpPut]
+    [Route("/store/updateprice")]
+    public IActionResult UpdatePrice(string name, double newPrice)
+    {
+        var product = _productRepository.GetProductByName(name);
+        if (product != null)
         {
-            var product = _productRepository.GetProductByName(name);
-            if (product != null)
-            {
-                product.Price = newPrice;
-                _productRepository.UpdateProduct(product);
-                return Ok($"{name} обновлен с новой ценой: {newPrice}");
-            }
-            else
-            {
-                return NotFound($"Продукт {name} не найден");
-            }
+            product.Price = newPrice;
+            _productRepository.UpdateProduct(product);
+            return Ok($"{name} обновлен с новой ценой: {newPrice}");
         }
-
-        [HttpPost]
-        [Route("/store/updatename")]
-        public IActionResult UpdateName(string currentName, string newName)
+        else
         {
-            var product = _productRepository.GetProductByName(currentName);
-            if (product != null)
-            {
-                product.Name = newName;
-                _productRepository.UpdateProduct(product);
-                return Ok($"Имя продукта изменено с {currentName} на {newName}");
-            }
-            else
-            {
-                return NotFound($"Продукт {currentName} не найден");
-            }
+            return NotFound($"Продукт {name} не найден");
         }
+    }
 
-        [HttpGet]
-        [Route("/store/outofstock")]
-        public IActionResult OutOfStock()
+    [HttpPut]
+    [Route("/store/updatename")]
+    public IActionResult UpdateName(string currentName, string newName)
+    {
+        var product = _productRepository.GetProductByName(currentName);
+        if (product != null)
         {
-            var outOfStockItems = _productRepository.GetAllProducts().Where(p => p.Stock == 0).ToList();
-            if (outOfStockItems.Any())
-            {
-                return Ok(outOfStockItems);
-            }
-            else
-            {
-                return Ok("Все продукты в наличии");
-            }
+            product.Name = newName;
+            _productRepository.UpdateProduct(product);
+            return Ok($"Имя продукта изменено с {currentName} на {newName}");
         }
-
-        [HttpPost]
-        [Route("/store/auth")]
-        public IActionResult Auth([FromBody] UserCredentials user)
+        else
         {
-            if ((user.user == "admin") && (user.pass == "123"))
-            {
-                return Ok($"{user.user} авторизован");
-            }
-            else
-            {
-                return NotFound($"{user.user} не найден");
-            }
+            return NotFound($"Продукт {currentName} не найден");
         }
+    }
 
-        [HttpPost]
-        [Route("/store/add")]
-        public IActionResult Add([FromBody] Product newProduct)
+    [HttpGet]
+    [Route("/store/outofstock")]
+    public IActionResult OutOfStock()
+    {
+        var outOfStockItems = _productRepository.GetAllProducts().Where(p => p.Stock == 0).ToList();
+        if (outOfStockItems.Any())
         {
-            _productRepository.AddProduct(newProduct);
-            return Ok(_productRepository.GetAllProducts());
+            return Ok(outOfStockItems);
         }
-
-        [HttpPost]
-        [Route("/store/delete")]
-        public IActionResult Delete(string name)
+        else
         {
-            var product = _productRepository.GetProductByName(name);
-            if (product != null)
-            {
-                _productRepository.DeleteProduct(name);
-                return Ok($"{name} удален");
-            }
-            else
-            {
-                return NotFound($"{name} не найден");
-            }
+            return Ok("Все продукты в наличии");
         }
+    }
 
-        [HttpGet]
-        [Route("/store/show")]
-        public IActionResult Show()
+    [HttpPost]
+    [Route("/store/auth")]
+    public IActionResult Auth([FromBody] UserCredentials user)
+    {
+        if ((user.user == "admin") && (user.pass == "123"))
         {
-            return Ok(_productRepository.GetAllProducts());
+            return Ok($"{user.user} авторизован");
         }
-      
+        else
+        {
+            return NotFound($"{user.user} не найден");
+        }
+    }
 
+    [HttpPost]
+    [Route("/store/add")]
+    public IActionResult Add([FromBody] Product newProduct)
+    {
+        _productRepository.AddProduct(newProduct);
+        return Ok(_productRepository.GetAllProducts());
+    }
+
+    [HttpDelete]
+    [Route("/store/delete")]
+    public IActionResult Delete(string name)
+    {
+        var product = _productRepository.GetProductByName(name);
+        if (product != null)
+        {
+            _productRepository.DeleteProduct(name);
+            return Ok($"{name} удален");
+        }
+        else
+        {
+            return NotFound($"{name} не найден");
+        }
+    }
+
+    [HttpGet]
+    [Route("/store/show")]
+    public IActionResult Show()
+    {
+        return Ok(_productRepository.GetAllProducts());
+    }
 }

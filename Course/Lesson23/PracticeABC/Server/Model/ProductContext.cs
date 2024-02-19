be999806a4496b4db17710f052e253d4b64896ee
venimu <ven1mu@yandex.ru>
2024-02-19 18:52:26 +0300

    new file:   Course/Lesson23/Lesson23.sln 	new file:   Course/Lesson23/PracticeABC/Server/Model/EFCoreProductRepository.cs 	modified:   Course/Lesson23/PracticeABC/Server/Model/ProductContext.cs 	modified:   Course/Lesson23/PracticeABC/Server/PracticeA.csproj

--- Course/Lesson23/PracticeABC/Server/Model/ProductContext.cs
+++ Course/Lesson23/PracticeABC/Server/Model/ProductContext.cs
@@ -1,5 +1,13 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using PracticeABC;
 
 
-public class Product
+public class ProductContext: DbContext {
+    public DbSet<Product> Products {get; set;}
+
+    public ProductContext(DbContextOptions<ProductContext> options): base(options) {}
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Product>().HasNoKey();
+    }
+}

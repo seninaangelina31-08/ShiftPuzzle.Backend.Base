3a6b08d170fbca388ebba64850f4236d8f919e75
Frain <89194392174pp@gmail.com>
2024-02-19 19:00:28 +0300

    feature(lesson23): practice AB complete

---
+++ Course/Lesson23/PracticeABC/Server/Model/ProductContext.cs
@@ -0,0 +1,14 @@
+namespace PracticeABC;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasNoKey();
+    }
+}

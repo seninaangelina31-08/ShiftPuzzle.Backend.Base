8b9152147c6cb5194b22e8ca64772951407c1052
timurkaff <timalololohka@gmail.com>
2024-02-26 12:52:29 +0300

    feat:(Сделал Practice AB)

--- Course/Lesson24/PracticeABC/PracticeB/Program.cs
+++ Course/Lesson24/PracticeABC/PracticeB/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,28 +8,37 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // Задача 1: Загрузка файла из сети по URL и сохранение его локально
-        
+        // Задача  1: Загрузка файла из сети по URL и сохранение его локально
+        string url = "https://www.file.io/eWYt/download/TDikYDqjKkss";
+        string filePath = "PracticeB.txt";
+        await DownloadFileAsync(url, filePath);
 
-        // Задача 2: Асинхронное чтение и запись файлов
-        
+        // Задача  2: Асинхронное чтение и запись файлов
+        string content = "Пример содержимого файла";
+        await WriteToFileAsync(filePath, content);
+        await ReadFromFileAsync(filePath);
 
-        // Задача 3: Выполнение параллельных HTTP-запросов к нескольким серверам
-        
- 
+        // Задача  3: Выполнение параллельных HTTP-запросов к нескольким серверам
+        List<string> urls = new List<string>
+        {
+            "https://ненаю.com",
+            "https://ненаю.com"
+        };
+        await FetchDataAsync(urls);
     }
 
     static async Task DownloadFileAsync(string url, string filePath)
     {
         using (var httpClient = new HttpClient())
         {
-            //отправка запроса на сервер
-            if ( ) //проверка успешности запроса
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
             {
-                using (var fileStream = new FileStream(filePath, FileMode.Create)) 
+                using (var fileStream = new FileStream(filePath, FileMode.Create))   
                 {
-                    await ; // сохранение файла  c CopyToAsync(fileStream)
+                    await response.Content.CopyToAsync(fileStream);
                 }
+                Console.WriteLine("Файл успешно загружен.");
             }
             else
             {
@@ -43,7 +51,7 @@ class Program
     {
         using (var writer = new StreamWriter(filePath))
         {
-            await  ; // запись в файл асинхронно
+            await writer.WriteAsync(content);
         }
         Console.WriteLine("Файл успешно записан.");
     }
@@ -52,7 +60,8 @@ class Program
     {
         using (var reader = new StreamReader(filePath))
         {
-            
+            string content = await reader.ReadToEndAsync();
+            Console.WriteLine(content);
         }
     }
 
@@ -60,9 +69,20 @@ class Program
     {
         using (var httpClient = new HttpClient())
         {
-             
+            var tasks = urls.Select(url => httpClient.GetAsync(url)).ToArray();
+            var responses = await Task.WhenAll(tasks);
+            foreach (var response in responses)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"URL: {response.RequestMessage.RequestUri}, Content: {content}");
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при запросе URL: {response.RequestMessage.RequestUri}");
+                }
+            }
         }
     }
-
-     
 }

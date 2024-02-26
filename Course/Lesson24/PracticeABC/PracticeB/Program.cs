7bdd9797e5e371aface168b143e13ee94a11aebe
Ivanova Daria <ida30062006@gmail.com>
2024-02-26 16:55:05 +0300

    feat: les24 prB

--- Course/Lesson24/PracticeABC/PracticeB/Program.cs
+++ Course/Lesson24/PracticeABC/PracticeB/Program.cs
@@ -10,12 +10,19 @@ class Program
     static async Task Main(string[] args)
     {
         // Задача 1: Загрузка файла из сети по URL и сохранение его локально
-        
+        string url = "https://www.vladtime.ru/uploads/posts/2018-03/1522438548_evropeyskaya-koshka-dikiy-kot.jpg";
+        string filePath1 = "cat.jpg";
+        DownloadFileAsync(url, filePath1);
 
         // Задача 2: Асинхронное чтение и запись файлов
+        string filePath2 = "writer.txt";
+        await WriteToFileAsync(filePath2, "I`m kringer");
+        await ReadFromFileAsync(filePath2);
         
-
         // Задача 3: Выполнение параллельных HTTP-запросов к нескольким серверам
+
+        List<string> urls = new List<string> { "http://google.com", "https://tilda.cc/ru/", "https://edu.dobro.ru/" };
+        await FetchDataAsync(urls);
         
  
     }
@@ -25,11 +32,12 @@ class Program
         using (var httpClient = new HttpClient())
         {
             //отправка запроса на сервер
-            if ( ) //проверка успешности запроса
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode) //проверка успешности запроса
             {
                 using (var fileStream = new FileStream(filePath, FileMode.Create)) 
                 {
-                    await ; // сохранение файла  c CopyToAsync(fileStream)
+                    await response.Content.CopyToAsync(fileStream); // сохранение файла  c CopyToAsync(fileStream)
                 }
             }
             else
@@ -43,7 +51,7 @@ class Program
     {
         using (var writer = new StreamWriter(filePath))
         {
-            await  ; // запись в файл асинхронно
+            await writer.WriteAsync(content); // запись в файл асинхронно
         }
         Console.WriteLine("Файл успешно записан.");
     }
@@ -52,7 +60,7 @@ class Program
     {
         using (var reader = new StreamReader(filePath))
         {
-            
+            await reader.ReadToEndAsync();
         }
     }
 
@@ -60,7 +68,11 @@ class Program
     {
         using (var httpClient = new HttpClient())
         {
-             
+             foreach (var url in urls)
+            {
+                var response = httpClient.GetAsync(url);
+                Console.WriteLine($"Запрос к {url} выполнен");
+            }
         }
     }
 

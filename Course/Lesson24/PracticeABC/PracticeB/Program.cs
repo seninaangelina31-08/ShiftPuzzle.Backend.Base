1c5dafb62482b862d2f1e3de98a57e79372284ee
DariaN <m.dzino@ya.ru>
2024-02-27 17:14:48 +0300

    feat L24 PrAB

--- Course/Lesson24/PracticeABC/PracticeB/Program.cs
+++ Course/Lesson24/PracticeABC/PracticeB/Program.cs
@@ -10,31 +10,39 @@ class Program
     static async Task Main(string[] args)
     {
         // Задача 1: Загрузка файла из сети по URL и сохранение его локально
-        
+        string url = "https://emojiisland.com/cdn/shop/products/Emoji_Icon_-_Clown_emoji_large.png";
+        string localFilePath = "clown.png";
+        await DownloadFileAsync(url, localFilePath);
 
         // Задача 2: Асинхронное чтение и запись файлов
-        
+        string filePath = "input.txt";
+        await WriteToFileAsync(filePath, "Привет, мир!");
+        await ReadFromFileAsync(filePath);
 
         // Задача 3: Выполнение параллельных HTTP-запросов к нескольким серверам
-        
- 
+        List<string> urls = new List<string> { "http://google.com", "http://yandex.ru", "http://yahoo.com" };
+        await FetchDataAsync(urls);
+    
     }
 
     static async Task DownloadFileAsync(string url, string filePath)
     {
         using (var httpClient = new HttpClient())
         {
-            //отправка запроса на сервер
-            if ( ) //проверка успешности запроса
+            // Отправка запроса на сервер
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
             {
-                using (var fileStream = new FileStream(filePath, FileMode.Create)) 
+                using (var file = new FileStream(filePath, FileMode.Create))
                 {
-                    await ; // сохранение файла  c CopyToAsync(fileStream)
+                    // Сохранение файла
+                    await response.Content.CopyToAsync(file);
                 }
             }
             else
             {
-                Console.WriteLine("Ошибка загрузки файла.");
+                Console.WriteLine($"Ошибка: {response.StatusCode}");
             }
         }
     }
@@ -43,16 +51,21 @@ class Program
     {
         using (var writer = new StreamWriter(filePath))
         {
-            await  ; // запись в файл асинхронно
+            await writer.WriteAsync(content); // запись в файл асинхронно
         }
         Console.WriteLine("Файл успешно записан.");
     }
+
 
     static async Task ReadFromFileAsync(string filePath)
     {
         using (var reader = new StreamReader(filePath))
         {
-            
+            string line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                Console.WriteLine(line); // Выведем в консоль
+            }
         }
     }
 
@@ -60,9 +73,20 @@ class Program
     {
         using (var httpClient = new HttpClient())
         {
-             
+            foreach (var url in urls)
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    string firstLine = responseData.Split('\n').FirstOrDefault();
+                    Console.WriteLine(firstLine); // Выводим первую строку в консоль
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка из {url}. Ошибка: {response.StatusCode}");
+                }
+            }
         }
-    }
-
-     
+    } 
 }

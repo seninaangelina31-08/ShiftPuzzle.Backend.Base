7c74bce4a34230cd64817e28540231428afb6369
Валерий Манойлов <manojlovv@inbox.ru>
2024-02-29 18:50:24 +0300

    Complete all Practices in Lesson26

---
+++ Course/Lesson26/PracticeABC/PracticeAB/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+Пратика А:
+Система уведомлений:
+Создайте простую систему уведомлений, где пользователь может подписываться на различные события (например, "новое сообщение", "новый заказ" и т. д.) и получать уведомления при их возникновении.
+
+ 
+Пратика Б:
+
+Система обработки асинхронных событий в предыдщуем примере:
+Создайте систему обработки асинхронных событий, где различные задачи выполняются параллельно. Реализуйте механизм подписки на события с возможностью асинхронного выполнения обработчиков событий.
+*/
+
+// система уведомлений  
+using System;   
+using System.Collections.Generic;
+
+
+
+public  class NotificationSystem
+{
+    public event Action <string> OnNewMessage;
+    public event Action <string> OnNewOrder; 
+    public event Action <string, string> OnDeliveredOrder; 
+        
+    public NotificationSystem()
+    { 
+
+    }
+// данная обертка нужна для того чтобы вызвать событие, 
+//т.к. напрямую вызвать событие нельзя изза того что  фукнция мейн в статическом классе
+    public void NewMessage(string message)
+    {
+        OnNewMessage?.Invoke(message);
+    }
+    public void NewOrder(string orderInfo) 
+    {
+        OnNewOrder?.Invoke(orderInfo);
+    }
+
+    public void NewDeliveredOrder(string order, string dateTime)
+    {
+        OnDeliveredOrder?.Invoke(order, dateTime);
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        NotificationSystem notificationSystem = new NotificationSystem();
+        notificationSystem.OnNewMessage += TestNewMsg;
+        notificationSystem.OnNewOrder += TestNewOrder;
+        notificationSystem.OnDeliveredOrder += TestDeliveredOrder;
+
+        Console.WriteLine("Input message: ");
+        string messageForUser = Console.ReadLine();
+        Console.WriteLine("Input product info: ");
+        string orderInfo = Console.ReadLine();
+        string order = orderInfo;
+        string dateTime = "28.02.24 17:00";
+
+        notificationSystem.NewMessage(messageForUser);
+        notificationSystem.NewOrder(orderInfo);
+        notificationSystem.NewDeliveredOrder(order, dateTime);
+    }
+    
+    public static async void TestNewMsg(string message)
+    {
+       await TestNewMsgAsync(message);
+    }
+    public static async void TestNewOrder(string orderInfo)
+    {
+        await TestNewOrderAsync(orderInfo);
+    }
+
+    public static async void TestDeliveredOrder(string order, string dateTime)
+    {
+        await TestOrderDeliveredAsync(order, dateTime);
+    }
+
+
+
+    public static async Task TestNewMsgAsync(string message)
+    {
+        Console.WriteLine($"New message: {message}");
+    }
+
+    public static async Task TestNewOrderAsync(string orderInfo)
+    {
+        Console.WriteLine($"New order: {orderInfo}");
+    }
+
+    public static async Task TestOrderDeliveredAsync(string order, string dateTime)
+    {
+        Console.WriteLine($"Order '{order}' delivered async. At time {dateTime}");
+    }
+   
+}   
---
+++ Course/Lesson26/PracticeABC/PracticeC/Server/Model/EFCoreProductRepository.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeABC
+{
+
+    public class EFCoreProductRepository : IProductRepository
+    {
+
+        private event Action <Product> OnProductAdded;
+        private event Action <Product> OnProductUpdated;
+        private event Action <string> OnProductDeleted;
+
+        private readonly ProductContext _context;
+
+        public EFCoreProductRepository(ProductContext context)
+        {
+            _context = context;  
+            OnProductAdded += SendNotificationToStatDepartmentAdd;
+            OnProductUpdated += SendNotificationToStatDepartmentUpdate;
+            OnProductDeleted += SendNotificationToStatDepartmentDelete;
+        }
+
+        private void SendNotificationToStatDepartmentAdd(Product product)
+        {
+           Console.WriteLine("Отправляю отчет в отдел статистики...");
+           Console.WriteLine($"Добавлен новый продукт: {product.Name}");    
+        }
+
+        private void SendNotificationToStatDepartmentUpdate(Product product)
+        {
+           Console.WriteLine("Отправляю отчет в отдел статистики...");
+           Console.WriteLine($"Обновлен продукт: {product.Name}. Новая цена: {product.Price}");    
+        }
+
+        private void SendNotificationToStatDepartmentDelete(string name)
+        {
+           Console.WriteLine("Отправляю отчет в отдел статистики...");
+           Console.WriteLine($"Удален продукт: {name}");    
+        }
+ 
+
+        public List<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetProductByName(string name)
+        {
+            return _context.Products.FirstOrDefault(p => p.Name == name);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();  
+
+            OnProductAdded?.Invoke(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+
+            OnProductUpdated?.Invoke(product);
+        }
+
+        public void DeleteProduct(string name)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Name == name);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+
+                OnProductDeleted?.Invoke(name);
+            }
+        }
+    }
+}

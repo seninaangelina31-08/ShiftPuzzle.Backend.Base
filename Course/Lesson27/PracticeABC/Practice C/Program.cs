fe2b282674594817c85d6d914477e4aa243b7458
venimu <ven1mu@yandex.ru>
2024-03-04 18:47:15 +0300

    Lesson 27; Practice C; 	modified:   Course/Lesson27/PracticeABC/Practice C/Program.cs

--- Course/Lesson27/PracticeABC/Practice C/Program.cs
+++ Course/Lesson27/PracticeABC/Practice C/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using namespace PracticeABC;
+using System;
+using System.Threading.Tasks;
+
+class Program {
+
+
+
+    static async Task ExtremeReadAsync(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            var content = reader.ReadToEndAsync();
+            Console.WriteLine(content);
+        }
+    }
+
+    
+    public static async void main() {
+        Task tasks = new Task[] {
+            Task.Run(ExtremeReadAsync("task.txt")),
+            Task.Run(() => {Console.WriteLine("Данные изменены")}),
+            Task.Run(() => {Console.WriteLine("Данные перезаписанны")})
+        };
+        await Task.WhenAll(tasks);
+    }
+
+}

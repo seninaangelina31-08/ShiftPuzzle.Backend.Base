1543276eaf22ee6f7ffe9a6bac70ff8b1ed09e99
Ivanova Daria <ida30062006@gmail.com>
2024-03-05 22:54:51 +0300

    feat: practiceC les27

---
+++ Course/Lesson27/PracticeABC/PracticeC/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PracticeABC;
+using System;
+using System.IO;
+
+class Programm
+{
+    static async Task Main(string[] args){
+        string path =  @"1.txt";
+        string newPath = @"2.txt";
+        Task[] tasks = new Task[]
+        {
+            Task.Run(() => CopyFile(path, newPath)),
+            Task.Run(() => Console.WriteLine("The file was changed")),
+            Task.Run(() => Console.WriteLine("The information was rewrite"))
+        };
+        await Task.WhenAll(tasks);
+    }
+
+    static async Task CopyFile(string path, string newPath)
+    {
+        FileInfo fileInf = new FileInfo(path);
+        if (fileInf.Exists)
+        {
+            fileInf.CopyTo(newPath, true);      
+   
+        }
+    }
+
+}

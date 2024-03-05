56a2544dc0b57bce76a70471ffac679da1fdf7fc
DariaN <m.dzino@ya.ru>
2024-03-05 18:58:04 +0300

    feat L27 PrC

--- Course/Lesson27/PracticeABC/Practice C/Program.cs
+++ Course/Lesson27/PracticeABC/Practice C/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PracticeC
+{
+    class Program
+    {
+        public static async Task BackUp(string filePath = "t.txt", string backupFilePath = "backup.txt")
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string content = await reader.ReadToEndAsync(); 
+                
+                using (var writer = new StreamWriter(backupFilePath))
+                {
+                    await writer.WriteAsync(content);  
+                    Console.WriteLine("Резервное копирование выполнено");
+                }
+            }
+        }
+
+
+        public static async Task ChangeData()
+        {
+            Console.WriteLine("Данные изменены");
+        }
+
+        public static async Task RewriteData()
+        {
+            Console.WriteLine("Данные перезаписаны");
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            Task[] tasks = new Task[]
+            {
+                Task.Run(() => BackUp()),
+                Task.Run(() => ChangeData()),
+                Task.Run(() => RewriteData())
+            };
+            await Task.WhenAll(tasks);
+        }
+    }
+}

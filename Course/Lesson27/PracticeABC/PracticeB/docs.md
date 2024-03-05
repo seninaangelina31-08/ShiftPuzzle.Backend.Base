bf216cf7a9be942926938d3b7b1bd988b6eb7a56
MisterCat100 <mistercat0618@gmail.com>
2024-03-05 23:40:50 +0300

    feat(Lesson27): completed Practices A-B-C.

---
+++ Course/Lesson27/PracticeABC/PracticeB/docs.md
@@ -0,0 +1,3 @@
+# Описание проекта "Task-tracker"
+
+[Подробное описание проекта](https://docs.google.com/document/d/1BHQLjr5am5KNdDwy-c_0TEPWOTTTMhrl1pfUE9qUbQ0/edit?usp=sharing)
---
+++ Course/Lesson27/PracticeABC/PracticeC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+
+
+static async Task MakeCopyFileAsync(string path)
+{
+    string[] lines = await File.ReadAllLinesAsync(path);
+    await File.WriteAllLinesAsync($"{path}_backup", lines);
+}
+
+static async Task ChangeInfoAsync()
+{
+    Console.WriteLine("Изменяю данные....");
+    await Task.Delay(3000);
+    Console.WriteLine("Данные изменены");
+}
+
+static async Task ChangeExtraInfoAsync()
+{
+    Console.WriteLine("Изменяю данные внутри....");
+    await Task.Delay(3000);
+    Console.WriteLine("Данные внутри - изменены");
+}
+
+
+
+
+await Task.Run(async () => await MakeCopyFileAsync("text.txt"));
+
+await Task.Run(ChangeInfoAsync);
+
+await Task.Run(ChangeExtraInfoAsync);
+

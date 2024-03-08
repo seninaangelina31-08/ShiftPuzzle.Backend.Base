5f112c8e6cb9aa560e043c031420fb826f710373
IvanSobolev <i@bezlikiy.ru>
2024-03-08 19:39:41 +0300

    feat lesson22 practiceAB

--- Course/Lesson22/PracticeABC/Server/Model/Product.cs
+++ Course/Lesson22/PracticeABC/Server/Model/Product.cs
@@ -8,7 +8,7 @@ public class Product
     [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; }
 
-    [Range(0.01, 10000)]
+    [Range(0.01, 5000)]
     public double Price { get; set; }
 
     [Range(0, 10000)]

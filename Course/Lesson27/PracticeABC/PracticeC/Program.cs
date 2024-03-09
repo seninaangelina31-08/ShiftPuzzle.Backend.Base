fa58dd903affd368c5b39a0e9e0deba9df637812
Shadik23 <kurichjev@yandex.ru>
2024-03-09 18:36:09 +0300

    feat(Lesson27): PracticeABC

---
+++ Course/Lesson27/PracticeABC/PracticeC/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace PracticeABC;
+
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourceFilePath = "file/source.txt";
+        string backupFilePath = "file/backup.txt";
+        string tempFilePath = "file/temp.txt";
+
+        Thread thread1 = new Thread(() =>
+        {
+            File.Copy(sourceFilePath, backupFilePath, true);
+            Console.WriteLine("Резервное копирование завершено.");
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            string content = File.ReadAllText(sourceFilePath);
+            content = content.Replace("oldText", "newText");
+            File.WriteAllText(tempFilePath, content);
+            Console.WriteLine("Данные изменены.");
+        });
+
+        Thread thread3 = new Thread(() =>
+        {
+            string newContent = File.ReadAllText(tempFilePath);
+            File.WriteAllText(sourceFilePath, newContent);
+            Console.WriteLine("Данные перезаписаны.");
+        });
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+
+        Console.WriteLine("Все потоки завершили работу.");
+    }
+}

83783992b7ac6f952c985dbdc4e47de4cf4e9815
Юрий <zaretskiy.y.a@gmail.com>
2024-03-09 13:21:52 +0300

    feat lesson 28 BC

--- Course/Lesson28/EasyTaskTrackerAPI/Program.cs
+++ Course/Lesson28/EasyTaskTrackerAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+ 
+builder.Services.AddSingleton<ITaskManager>(provider =>
+{
+    var optionsBuilder = new DbContextOptionsBuilder<TaskContext>();
+    optionsBuilder.UseSqlite("Data Source=TaskDataBase.db"); 
+    var taskContext = new TaskContext(optionsBuilder.Options);
+    taskContext.Database.EnsureCreated(); 
+    ITaskRepository taskRepository = new TaskRepository(taskContext);
+    ITaskManager taskManager = new TaskManager(taskRepository);
+
+    return taskManager;
+});
+
 
 var app = builder.Build();
 

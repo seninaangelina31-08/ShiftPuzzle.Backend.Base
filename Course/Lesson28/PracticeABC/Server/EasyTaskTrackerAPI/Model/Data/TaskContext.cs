91df4d0211f9ea8cca57ff0f9b6e17f332b93d95
LokiMATI <r.matigorov@yandex.ru>
2024-03-10 21:18:43 +0300

    feat(Lesson28): Practice complited.

---
+++ Course/Lesson28/PracticeABC/Server/EasyTaskTrackerAPI/Model/Data/TaskContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+namespace EasyTaskTrackerAPI;
+
+public class TaskContext : DbContext
+{
+    public TaskContext(DbContextOptions<TaskContext> options) : base(options){}
+    public DbSet<TrackerTask> TrackerTasks { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TrackerTask>().HasKey(t => t.ID);
+    }
+}
---
+++ Course/Lesson28/PracticeABC/Server/EasyTaskTrackerAPI/Model/Interfaces/ITaskManager.cs
@@ -0,0 +1,8 @@
+namespace EasyTaskTrackerAPI;
+public interface ITaskManager
+{
+    List<TrackerTask> GetAllTasks();
+    TrackerTask GetTaskById(int taskId);
+    void AddTask(TrackerTask task);
+    void DeleteTask(TrackerTask taskId);
+}

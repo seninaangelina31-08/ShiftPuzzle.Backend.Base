72a9dcba202522c534958fe4f3763f035daea692
Satlykovs <satlykovs@gmail.com>
2024-03-11 18:48:31 +0300

    feat: Practice A finished Lesson29

--- Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
+++ Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
@@ -1,4 +1,6 @@
 public class TrackerTask
 {  
     public int ID { get; set; } 
+
+
 }
--- Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Implementation/TaskRepository.cs
+++ Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Implementation/TaskRepository.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 public class TaskRepository : ITaskRepository
 {
     private readonly TaskContext _context;

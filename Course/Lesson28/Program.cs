7f2abab9d222fa0c6e14159e6da8aa40aba71659
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-03-12 17:40:02 +0300

    Feat(Lesson 28): B,C

--- Course/Lesson28/Program.cs
+++ Course/Lesson28/Program.cs
@@ -1,9 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ITaskManager>(provider =>
+{
+    var optionsBuilder = new DbContextOptionsBuilder<TaskContext>();
+    optionsBuilder.UseSqlite("Data Source=TaskDataBase.db"); 
+
+    var taskContext = new TaskContext(optionsBuilder.Options);
+    taskContext.Database.EnsureCreated(); 
+
+    ITaskRepository taskRepository = new TaskRepository(taskContext);
+    ITaskManager taskManager = new TaskManager(taskRepository);
+
+    return taskManager;
+});
 
 var app = builder.Build();
 
@@ -16,29 +30,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

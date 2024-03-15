59e7b22dc5f765fae1034bd81e7a283b8305c7e8
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-03-15 23:37:55 +0300

    Feat(Lesson 27): С

--- Course/Lesson27/PracticeABC/Practice C/Program.cs
+++ Course/Lesson27/PracticeABC/Practice C/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string filePath = "mytextfile.txt";
+        Task task1 = Task.Run(async () => await ReverseCopy(filePath));
+        Task task2 = Task.Run(async () => await ModifyData(filePath));
+        Task task3 = Task.Run(async () => await RewriteData(filePath));
+
+        await Task.WhenAll(task1, task2, task3);
+    }
+
+    public static async Task ReverseCopy(string filePath)
+    {
+        Console.WriteLine($"Файл {filePath} скопирован");
+    }
+
+    public static async Task ModifyData(string filePath)
+    {
+        Console.WriteLine($"Данные в файле {filePath} изменены");
+    }
+    public static async Task RewriteData(string filePath)
+    {
+        Console.WriteLine($"Данные в файле {filePath} перезаписаны");
+    }
+}

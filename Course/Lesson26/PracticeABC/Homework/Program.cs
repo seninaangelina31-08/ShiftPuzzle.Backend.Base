0569264c5de95a5e16f6b39cef3c5fede7d33827
LokiMATI <r.matigorov@yandex.ru>
2024-03-15 16:29:17 +0300

    feat(Lesson26): Homework complited.

---
+++ Course/Lesson26/PracticeABC/Homework/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main()
+    {
+        NotificationSystem notificationSystem = new NotificationSystem();
+        PizzaDelivery orderDelivery = new PizzaDelivery();
+        notificationSystem.OnNewOrder += orderDelivery.OrderAdd;
+        notificationSystem.OnOrderComplited += orderDelivery.OrderFinished;
+
+        PizzaOrder order = new PizzaOrder("Пиперони", 150);
+        notificationSystem.NewOrder(order);
+        notificationSystem.OrderComplited(order);
+    }
+}
+
+public class PizzaOrder
+{
+    public string PizzaName;
+    public int PizzaPrice;
+    
+    public PizzaOrder(string _PizzaName, int _PizzaPrice)
+    {
+        this.PizzaName = _PizzaName;
+        this.PizzaPrice = _PizzaPrice;
+    }
+}
+
+public class PizzaDelivery
+{
+    public void OrderAdd(PizzaOrder order)
+    {
+        Console.WriteLine($"Заказ {order.PizzaName} принят.");
+    }
+
+    public void OrderFinished(PizzaOrder order)
+    {
+        Console.WriteLine($"Заказ {order.PizzaName} доставлен. Вы получили {order.PizzaPrice} рублей.");
+    }
+}
+
+public  class NotificationSystem
+{
+    public event Action <PizzaOrder> OnOrderComplited;
+    public event Action <PizzaOrder> OnNewOrder; 
+
+    public NotificationSystem(){}
+    public void NewOrder(PizzaOrder order) 
+    {
+        OnNewOrder?.Invoke(order);
+    }
+    public void OrderComplited(PizzaOrder order)
+    {
+        OnOrderComplited?.Invoke(order);
+    }
+}

95c703764faa28b6b0afd92a5b123c1cf4d5905b
DariaN <m.dzino@ya.ru>
2024-03-17 17:24:09 +0300

    fix L29

--- Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
+++ Course/Lesson29/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
@@ -1,4 +1,8 @@
 public class TrackerTask
 {  
-    public int ID { get; set; } 
+    public int ID { get; set; }
+    public string? Name { get; internal set; }
+    public string? Description { get; internal set; }
+    public bool IsComplete { get; set; }
+    public DateTime? DueDate { get; set; }
 }

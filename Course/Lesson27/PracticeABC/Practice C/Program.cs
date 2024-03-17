6099ba5c8ee3e1cfb8d73fb4d8b57be39d39a8a4
lisyonok14 <olesya.nikulina.2007@mail.ru>
2024-03-17 13:11:32 +0300

    feat: less27 practABC done

--- Course/Lesson27/PracticeABC/Practice C/Program.cs
+++ Course/Lesson27/PracticeABC/Practice C/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PracticeC;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string filepath = "text";
+        Task[] tasks = new Task[]
+        {
+            Task.Run(() => CopyFileAsync(filepath)),
+            Task.Run(() => ChangeDataAsync(filepath)),
+            Task.Run(() => ReWriteDataAsync(filepath))
+        };
+        await Task.WhenAll(tasks);
+    }
+    
+    static async Task CopyFileAsync(string filepath)
+    {
+        string filePathCopy = $"{filepath}Copy.txt";
+        string filePath = $"{filepath}.txt";
+        using (var writer = new StreamWriter(filePathCopy))
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string line = await reader.ReadToEndAsync();
+                await writer.WriteAsync(line);
+            }
+        }
+    }
+
+    static async Task ChangeDataAsync(string filepath)
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("Данные изменены");
+    }
+
+    static async Task ReWriteDataAsync(string filepath)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Данные перезаписаны");
+    }
+}

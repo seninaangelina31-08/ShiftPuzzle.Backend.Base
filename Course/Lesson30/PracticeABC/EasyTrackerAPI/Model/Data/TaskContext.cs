f699ef0fd463fb053437591c17fa6f574917be0e
Shadik23 <kurichjev@yandex.ru>
2024-03-18 19:58:06 +0300

    feat(Lesson30): Practice ABC

--- Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TaskContext.cs
+++ Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TaskContext.cs
@@ -8,9 +8,13 @@ public class TaskContext : DbContext
     }
 
     public DbSet<TrackerTask> TrackerTasks { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<TrackerTask>().HasKey(t => t.ID);   
+        modelBuilder.Entity<TrackerTask>().HasKey(t => t.ID);
+        modelBuilder.Entity<User>().HasKey(u => u.ID);
+
+        modelBuilder.Entity<TrackerTask>().HasOne(t => t.AssignedUser).WithMany(u => u.Tasks);  
     }
 }
---
+++ Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/user.cs
@@ -0,0 +1,9 @@
+public class User
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public List<TrackerTask> Tasks { get; set; }
+
+}

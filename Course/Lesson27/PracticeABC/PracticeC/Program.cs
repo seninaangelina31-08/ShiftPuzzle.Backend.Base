270d2074612e54de0b385bf9c7ff9a006ca45975
DABer1805 <DABer1805@yandex.ru>
2024-03-20 02:28:13 +0300

    feat: finish practice ABC lesson 27

---
+++ Course/Lesson27/PracticeABC/PracticeC/Program.cs
@@ -0,0 +1,46 @@
+﻿﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PracticeC
+{
+    class Program
+    {
+        public static async Task BackUp(string filePath="file.txt", string backupFilePath="backup.txt")
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string data = await reader.ReadToEndAsync(); 
+                
+                using (var writer = new StreamWriter(backupFilePath))
+                {
+                    await writer.WriteAsync(data);  
+                    Console.WriteLine("Резервное копирование выполнено");
+                }
+            }
+        }
+
+
+        public static async Task ChangeData()
+        {
+            Console.WriteLine("Данные изменены");
+        }
+
+        public static async Task RewriteData()
+        {
+            Console.WriteLine("Данные перезаписаны");
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            Task[] tasks = new Task[]
+            {
+                Task.Run(() => BackUp()),
+                Task.Run(() => ChangeData()),
+                Task.Run(() => RewriteData())
+            };
+            await Task.WhenAll(tasks);
+        }
+    }
+}

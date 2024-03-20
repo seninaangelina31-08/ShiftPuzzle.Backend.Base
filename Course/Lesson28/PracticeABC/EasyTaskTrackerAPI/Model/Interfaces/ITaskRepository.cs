0788c00b07edb304592619d372636a44a68e6ce5
DABer1805 <DABer1805@yandex.ru>
2024-03-20 12:02:07 +0300

    feat: add finish practice ABC lesson 28

---
+++ Course/Lesson28/PracticeABC/EasyTaskTrackerAPI/Model/Interfaces/ITaskRepository.cs
@@ -0,0 +1,9 @@
+namespace EasyTaskTrackerAPI;
+
+public interface ITaskRepository
+{
+    List<TrackerTask> GetAllTasks();
+    TrackerTask GetTaskById(int taskId);
+    void AddTask(TrackerTask task);
+    void DeleteTask(TrackerTask taskId);
+}

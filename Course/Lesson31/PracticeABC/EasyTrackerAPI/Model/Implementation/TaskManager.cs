a0caf941adaabb93d99b953e07c0b3bef21bb306
IvanSobolev <i@bezlikiy.ru>
2024-03-28 18:44:26 +0300

    feat: Lesson 31 practiceA

--- Course/Lesson31/PracticeABC/EasyTrackerAPI/Model/Implementation/TaskManager.cs
+++ Course/Lesson31/PracticeABC/EasyTrackerAPI/Model/Implementation/TaskManager.cs
@@ -26,4 +26,18 @@ public class TaskManager : ITaskManager
     { 
         return _taskRepository.GetTaskById(taskId);
     }
+    
+    public void CompleteTask(int id)
+    {
+        var task = this.GetAllTasks().FirstOrDefault(task => task.ID == id);
+        if (task != null)
+        {
+            task.IsComplete = true;
+            Console.WriteLine($"Задача с ID = {id} завершена");
+        }
+        else
+        {
+            Console.WriteLine($"Задачи с ID = {id} нет");
+        }
+    }
 }

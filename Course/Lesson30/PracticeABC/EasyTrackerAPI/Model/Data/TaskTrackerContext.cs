99473a3b00abf24b23d05b3c8c3f5b870912315f
IvanSobolev <i@bezlikiy.ru>
2024-03-28 18:35:16 +0300

    feat: lesson 30 practiceABC

---
+++ Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TaskTrackerContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class TaskContext : DbContext
+{
+    public TaskContext(DbContextOptions<TaskContext> options) : base(options){}
+    public DbSet<TrackerTask> TrackerTasks { get; set; }
+    public DbSet<User> Users {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TrackerTask>().HasKey(t => t.ID);
+        modelBuilder.Entity<User>().HasKey(u => u.ID);
+
+        modelBuilder.Entity<TrackerTask>().HasOne(t => t.AssignedUser).WithMany(u => u.Tasks);
+    }
+}
--- Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
+++ Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
@@ -1,6 +1,11 @@
+using System;
+
 public class TrackerTask
 {  
     public int ID { get; set; }
-    public string? Name { get; internal set; }
-    public string? Description { get; internal set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsComplete {get; set;}
+    public DateTime? DueDate {get; set;}
+    public User? AssignedUser {get; set;}
 }

ba737959319507fb1b0939ef48f231028e324d64
Maxon <maxn2703@gmail.com>
2024-04-04 18:22:15 +0300

    feat: Lesson 27 Practise AC

--- Course/Lesson27/PracticeABC/Practice C/Program.cs
+++ Course/Lesson27/PracticeABC/Practice C/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PracticeABC;
+using System;
+using System.IO;
+class Program
+{   
+    public static async Task Backup(string filePath, string backupFilePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            using (var writer = new StreamWriter(backupFilePath))
+            {
+                await writer.WriteAsync(await reader.ReadToEndAsync());
+                Console.WriteLine("Резервное копирование выполнено");
+            }
+        }
+    }
+    static async Task Main(string[] args)
+    {
+        Task[] tasks = new Task[]
+        {
+            Task.Run(() => Backup("test.txt", "backup_test.txt")),
+            Task.Run(() => Console.WriteLine("Данные изменены")),
+            Task.Run(() => Console.WriteLine("Данные перезаписаны изнутри"))
+        };
+        await Task.WhenAll(tasks);
+    }
+}

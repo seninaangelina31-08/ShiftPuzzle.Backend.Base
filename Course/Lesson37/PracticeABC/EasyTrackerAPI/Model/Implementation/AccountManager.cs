e6b4adf95b684a18ebb49b61e559b1017196bb89
Frain <89194392174pp@gmail.com>
2024-04-15 18:54:59 +0300

    feature(lesson37): practice A complete

--- Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Implementation/AccountManager.cs
+++ Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Implementation/AccountManager.cs
@@ -19,9 +19,31 @@ public class AccountManager : IAccountManager
 
         if(_context.Users.Any(u => u.Name == account.Name))
         {
-            Console.WriteLine("Account with name " + account.Name + " already exists."); 
-            return;
+            User u = _context.Users.FirstOrDefault(u => u.Name == account.Name);
+            if (u.Password == account.Password)
+            {
+                Console.WriteLine("Account with name " + account.Name + " already exists."); 
+                return;
+            }
         } 
+
+        string[] domens = new string[2]{"ru", "com"};
+        
+        if (account.Name.Contains("@") && account.Name.Contains(".") && 
+        (account.Name.Length < 50) && 
+        (account.Name.Length > 5) &&
+        domens.Any(d => account.Name.Split(".").Last().Contains(d)))
+        {
+            account.Email = account.Name;
+            account.Name = account.Email.Split("@").First();
+            Console.WriteLine($"Account {account.Name} is verifided sucesfully");
+        }
+        else 
+        {
+            account.Email = account.Name;
+            Console.WriteLine($"Account {account.Name} is verifided sucesfully");
+        }
+
         account.ID = _context.Users.Count() + 1;    
         _context.Users.Add(account); 
         _context.SaveChanges();

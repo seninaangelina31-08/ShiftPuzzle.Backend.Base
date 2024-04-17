f6b2aaa984f7700fd13f1949d6740a28f0dca0a0
lisyonok14 <olesya.nikulina.2007@mail.ru>
2024-04-17 20:19:08 +0300

    feat: less36 prABC done

--- Course/Lesson36/PracticeABC/EasyTrackerAPI/Program.cs
+++ Course/Lesson36/PracticeABC/EasyTrackerAPI/Program.cs
@@ -23,6 +23,16 @@ builder.Services.AddSingleton<ITaskManager>(provider =>
     return taskManager;
 });
 
+builder.Services.AddSingleton<IAccountManager>(provider =>
+{
+    var optionsBuilder = new DbContextOptionsBuilder<AccountContext>();
+    optionsBuilder.UseSqlite("Data Source=TaskDataBase.db"); 
+    var accountContext = new AccountContext(optionsBuilder.Options);
+    accountContext.Database.EnsureCreated();
+    IAccountManager accountManager = new AccountManager(accountContext);
+    
+    return accountManager;
+});
 
 var app = builder.Build();
 

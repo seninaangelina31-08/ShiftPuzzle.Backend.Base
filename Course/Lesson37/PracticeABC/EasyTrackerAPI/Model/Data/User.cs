dc05baf85a89bb3184b449517f31579bb4a9f444
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-04-19 23:50:32 +0300

    Feat(Lesson 37): ABC

--- Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Data/User.cs
+++ Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Data/User.cs
@@ -18,5 +18,6 @@ public class User
     public long ID { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
-    public string? Password { get; set; }       
+    public string? Password { get; set; }  
+    public bool IsVerified { get; set;}     
 }
